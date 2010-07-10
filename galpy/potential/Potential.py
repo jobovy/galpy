324530f150b72bec592602d154286e6ec0139d41
Jo Bovy <bovy@ias.edu>
2010-07-10 20:21:45 +0000

    galpy: move pygd

---
+++ galpy/potential/Potential.py
@@ -0,0 +1,294 @@
+###############################################################################
+#   Potential.py: top-level class for a full (R,z) potential
+#
+#   Evaluate by calling the instance: Phi(z;R)= Pot(z,R)
+#
+#   API for Potentials:
+#      function _evaluate(self,R,z) returns Phi(z;R)
+#      function _Rforce(self,R,z) return K_R
+#      function _zforce(self,R,z) return K_z
+###############################################################################
+import os, os.path
+import cPickle as pickle
+import numpy as nu
+import bovy_plot as plot
+class Potential:
+    """Top-level class for a potential"""
+    def __init__(self,amp=1.):
+        """
+        NAME:
+           __init__
+        PURPOSE:
+        INPUT:
+           amp - amplitude to be applied when evaluating the potential and its forces
+        OUTPUT:
+        HISTORY:
+        """
+        self._amp= amp
+        return None
+
+    def __call__(self,R,z):
+        """
+        NAME:
+           __call__
+        PURPOSE:
+           evaluate the (R,z)
+        INPUT:
+           R - Cylindrical Galactocentric radius
+           z - vertical height
+        OUTPUT:
+           Phi(z;R)
+        HISTORY:
+           2010-04-16 - Written - Bovy (NYU)
+        """
+        try:
+            return self._amp*self._evaluate(R,z)
+        except AttributeError:
+            raise PotentialError("'_evaluate' function not implemented for this potential")
+
+    def Rforce(self,R,z):
+        """
+        NAME:
+           Rforce
+        PURPOSE:
+           evaluate radial force K_R  (R,z)
+        INPUT:
+           R - Cylindrical Galactocentric radius
+           z - vertical height
+        OUTPUT:
+           K_R (R,z)
+        HISTORY:
+           2010-04-16 - Written - Bovy (NYU)
+        DOCTEST:
+        """
+        try:
+            return self._amp*self._Rforce(R,z)
+        except AttributeError:
+            raise PotentialError("'_Rforce' function not implemented for this potential")
+        
+        
+    def zforce(self,R,z):
+        """
+        NAME:
+           zforce
+        PURPOSE:
+           evaluate the vertical force K_R  (R,z)
+        INPUT:
+           R - Cylindrical Galactocentric radius
+           z - vertical height
+        OUTPUT:
+           K_z (R,z)
+        HISTORY:
+           2010-04-16 - Written - Bovy (NYU)
+        DOCTEST:
+        """
+        try:
+            return self._amp*self._zforce(R,z)
+        except AttributeError:
+            raise PotentialError("'_zforce' function not implemented for this potential")
+
+
+    def _phiforce(self,R,z,phi):
+        """
+        NAME:
+           _phiforce
+        PURPOSE:
+           evaluate the azimuthal force K_R  (R,z,phi)
+        INPUT:
+           R - Cylindrical Galactocentric radius
+           z - vertical height
+           phi - azimuth (rad)
+        OUTPUT:
+           K_phi (R,z,phi)
+        HISTORY:
+           2010-07-10 - Written - Bovy (NYU)
+        DOCTEST:
+        """
+        return 0.
+
+    def plot(self,rmin=0.,rmax=1.5,nrs=21,zmin=-0.5,zmax=0.5,nzs=21,
+             ncontours=21,savefilename=None):
+        """
+        NAME:
+           plot
+        PURPOSE:
+           plot the potential
+        INPUT:
+           rmin - minimum R
+           rmax - maximum R
+           nrs - grid in R
+           zmin - minimum z
+           zmax - maximum z
+           nzs - grid in z
+           ncontours - number of contours
+           savefilename - save to or restore from this savefile (pickle)
+        OUTPUT:
+           plot to output device
+        HISTORY:
+           2010-07-09 - Written - Bovy (NYU)
+        """
+        if not savefilename == None and os.path.exists(savefilename):
+            print "Restoring savefile "+savefilename+" ..."
+            savefile= open(savefilename,'rb')
+            potRz= pickle.load(savefile)
+            Rs= pickle.load(savefile)
+            zs= pickle.load(savefile)
+            savefile.close()
+        else:
+            Rs= nu.linspace(rmin,rmax,nrs)
+            zs= nu.linspace(zmin,zmax,nzs)
+            potRz= nu.zeros((nrs,nzs))
+            for ii in range(nrs):
+                for jj in range(nzs):
+                    potRz[ii,jj]= self._evaluate(Rs[ii],zs[jj])
+            if not savefilename == None:
+                print "Writing savefile "+savefilename+" ..."
+                savefile= open(savefilename,'wb')
+                pickle.dump(potRz,savefile)
+                pickle.dump(Rs,savefile)
+                pickle.dump(zs,savefile)
+                savefile.close()
+        return plot.bovy_dens2d(potRz.T,origin='lower',cmap='gist_gray',contours=True,
+                                xlabel=r"$R/R_0$",ylabel=r"$z/R_0$",
+                                xrange=[rmin,rmax],
+                                yrange=[zmin,zmax],
+                                aspect=.75*(rmax-rmin)/(zmax-zmin),
+                                cntrls='-',
+                                levels=nu.linspace(nu.nanmin(potRz),nu.nanmax(potRz),
+                                                   ncontours))
+        
+
+
+class PotentialError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def evaluatePotentials(R,z,Pot):
+    """
+    NAME:
+       evaluatePotentials
+    PURPOSE:
+       convenience function to evaluate a possible sum of potentials
+    INPUT:
+       R - cylindrical Galactocentric distance
+       z - distance above the plane
+       Pot - potential or list of potentials
+    OUTPUT:
+       Phi(R,z)
+    HISTORY:
+       2010-04-16 - Written - Bovy (NYU)
+    """
+    if isinstance(Pot,list):
+        sum= 0.
+        for pot in Pot:
+            sum+= pot(R,z)
+        return sum
+    elif isinstance(Pot,Potential):
+        return Pot(R,z)
+    else:
+        raise PotentialError("Input to 'evaluatePotentials' is neither a Potential-instance or a list of such instances")
+
+def evaluateRforces(R,z,Pot):
+    """
+    NAME:
+       evaluateRforce
+    PURPOSE:
+       convenience function to evaluate a possible sum of potentials
+    INPUT:
+       R - cylindrical Galactocentric distance
+       z - distance above the plane
+       Pot - a potential or list of potentials
+    OUTPUT:
+       K_R(R,z)
+    HISTORY:
+       2010-04-16 - Written - Bovy (NYU)
+    """
+    if isinstance(Pot,list):
+        sum= 0.
+        for pot in Pot:
+            sum+= pot.Rforce(R,z)
+        return sum
+    elif isinstance(Pot,Potential):
+        return Pot.Rforce(R,z)
+    else:
+        raise PotentialError("Input to 'evaluateRforces' is neither a Potential-instance or a list of such instances")
+
+def evaluatezforces(R,z,Pot):
+    """
+    NAME:
+       evaluatezforces
+    PURPOSE:
+       convenience function to evaluate a possible sum of potentials
+    INPUT:
+       R - cylindrical Galactocentric distance
+       z - distance above the plane
+       Pot - a potential or list of potentials
+    OUTPUT:
+       K_z(R,z)
+    HISTORY:
+       2010-04-16 - Written - Bovy (NYU)
+    """
+    if isinstance(Pot,list):
+        sum= 0.
+        for pot in Pot:
+            sum+= pot.zforce(R,z)
+        return sum
+    elif isinstance(Pot,Potential):
+        return Pot.zforce(R,z)
+    else:
+        raise PotentialError("Input to 'evaluatezforces' is neither a Potential-instance or a list of such instances")
+
+def plotPotentials(Pot,rmin=0.,rmax=1.5,nrs=21,zmin=-0.5,zmax=0.5,nzs=21,
+                   ncontours=21,savefilename=None):
+        """
+        NAME:
+           plotPotentials
+        PURPOSE:
+           plot a set of potentials
+        INPUT:
+           rmin - minimum R
+           rmax - maximum R
+           nrs - grid in R
+           zmin - minimum z
+           zmax - maximum z
+           nzs - grid in z
+           ncontours - number of contours
+           savefilename - save to or restore from this savefile (pickle)
+        OUTPUT:
+           plot to output device
+        HISTORY:
+           2010-07-09 - Written - Bovy (NYU)
+        """
+        if not savefilename == None and os.path.exists(savefilename):
+            print "Restoring savefile "+savefilename+" ..."
+            savefile= open(savefilename,'rb')
+            potRz= pickle.load(savefile)
+            Rs= pickle.load(savefile)
+            zs= pickle.load(savefile)
+            savefile.close()
+        else:
+            Rs= nu.linspace(rmin,rmax,nrs)
+            zs= nu.linspace(zmin,zmax,nzs)
+            potRz= nu.zeros((nrs,nzs))
+            for ii in range(nrs):
+                for jj in range(nzs):
+                    potRz[ii,jj]= evaluatePotentials(Rs[ii],zs[jj],Pot)
+            if not savefilename == None:
+                print "Writing savefile "+savefilename+" ..."
+                savefile= open(savefilename,'wb')
+                pickle.dump(potRz,savefile)
+                pickle.dump(Rs,savefile)
+                pickle.dump(zs,savefile)
+                savefile.close()
+        return plot.bovy_dens2d(potRz.T,origin='lower',cmap='gist_gray',contours=True,
+                                xlabel=r"$R/R_0$",ylabel=r"$z/R_0$",
+                                xrange=[rmin,rmax],
+                                yrange=[zmin,zmax],
+                                aspect=.75*(rmax-rmin)/(zmax-zmin),
+                                cntrls='-',
+                                levels=nu.linspace(nu.nanmin(potRz),nu.nanmax(potRz),
+                                                   ncontours))
+        
+
