0c1dfd1d1aab1090fae99eb30b7d918ebcded19c
Jo Bovy <bovy@ias.edu>
2010-07-11 23:04:05 +0000

    galpy: df.py

---
+++ galpy/df.py
@@ -0,0 +1,12 @@
+from galpy.df_src import diskdf
+from galpy.df_src import surfaceSigmaProfile
+
+#
+# Classes
+#
+shudf= diskdf.shudf
+dehnendf= diskdf.dehnendf
+DFcorrection= diskdf.DFcorrection
+diskdf= diskdf.diskdf
+expSurfaceSigmaProfile= surfaceSigmaProfile.expSurfaceSigmaProfile
+surfaceSigmaProfile= surfaceSigmaProfile.surfaceSigmaProfile
