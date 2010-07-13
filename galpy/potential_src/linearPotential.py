10c271c2c072b4f244c4f4a02c0769ae30ef3732
Jo Bovy <bovy@ias.edu>
2010-07-13 22:02:30 +0000

    evaluateForces

--- galpy/potential_src/linearPotential.py
+++ galpy/potential_src/linearPotential.py
@@ -41,3 +41,27 @@ class linearPotential:
             return self._amp*self._force(x)
         except AttributeError:
             raise PotentialError("'_force' function not implemented for this potential")
+
+def evaluateForces(x,Pot):
+    """
+    NAME:
+       evaluateForces
+    PURPOSE:
+       evaluate the forces due to a list of potentials
+    INPUT:
+       x - evaluate forces at this position
+       Pot - (list of) linearPotential instance(s)
+    OUTPUT:
+       force(x)
+    HISTORY:
+       2010-07-13 - Written - Bovy (NYU)
+    """
+    if isinstance(Pot,list):
+        sum= 0.
+        for pot in Pot:
+            sum+= pot.force(x)
+        return sum
+    elif isinstance(Pot,linearPotential):
+        return Pot.force(x)
+    else:
+        raise PotentialError("Input to 'evaluateForces' is neither a linearPotential-instance or a list of such instances")
