545746c463bf623ea6bbcb05078163b94e02bf97
Jo Bovy <bovy@ias.edu>
2010-07-13 00:37:45 +0000

    galpy: placeholder eval()

--- galpy/galpy/df_src/Edf.py
+++ galpy/galpy/df_src/Edf.py
@@ -25,7 +25,7 @@ class Edf:
         HISTORY:
            2010-07-12 - Written - Bovy (NYU)
         """
-        if isinstance(call_in,'Orbit') or isinstance(call_in,'planarOrbitTop'):
+        if isinstance(call_in,Orbit) or isinstance(call_in,planarOrbitTop):
             if not t == None:
                 E= call_in.E(t)
             else:
@@ -33,3 +33,16 @@ class Edf:
             return self.eval(E)
         else:
             return self.eval(call_in)
+
+    def eval(self,E):
+        """
+        NAME:
+           eval
+        PURPOSE:
+           evaluate the DF
+        INPUT:
+        OUTPUT:
+        HISTORY:
+           2010-07-12 - Written - Bovy (NYU)
+        """
+        raise AttributeError("eval() function of this DF is not implemented")
