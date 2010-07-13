079cd1249f48962dffd7307e943095275efcf4f5
Jo Bovy <bovy@ias.edu>
2010-07-13 00:10:38 +0000

    galpy: start planarOrbit

--- galpy/galpy/orbit.py
+++ galpy/galpy/orbit.py
@@ -1,9 +1,13 @@
 from galpy.orbit_src import Orbit
 from galpy.orbit_src import RZOrbit
+from galpy.orbit_src import planarOrbit
 
 #
 # Classes
 #
 Orbit= Orbit.Orbit
 RZOrbit= RZOrbit.RZOrbit
+planarOrbitTop= planarOrbit.planarOrbitTop
+planarROrbit= planarOrbit.planarROrbit
+planarOrbit= planarOrbit.planarOrbit
 
