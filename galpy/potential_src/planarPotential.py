5d93c1fbc12b021f2a8edde2804fd176341d045f
Jo Bovy <bovy@ias.edu>
2010-07-14 02:39:33 +0000

    reformatting/start on plotRotcurve

--- galpy/potential_src/planarPotential.py
+++ galpy/potential_src/planarPotential.py
@@ -301,123 +301,165 @@ def evaluateplanarphiforces(*args):
         raise PotentialError("Input to 'evaluatephiforces' is neither a Potential-instance or a list of such instances")
 
 def plotplanarPotentials(Pot,*args,**kwargs):
-        """
-        NAME:
-           plotplanarPotentials
-        PURPOSE:
-           plot a planar potential
-        INPUT:
-           Rrange - range
-           xrange, yrange - if relevant
-           grid, gridx, gridy - number of points to plot
-           savefilename - save to or restore from this savefile (pickle)
-           ncontours - number of contours to plot (if applicable)
-           +bovy_plot(*args,**kwargs) or bovy_dens2d(**kwargs)
-        OUTPUT:
-           plot to output device
-        HISTORY:
-           2010-07-13 - Written - Bovy (NYU)
-        """
-        if kwargs.has_key('Rrange'):
-            Rrange= kwargs['Rrange']
-            kwargs.pop('Rrange')
-        else:
-            Rrange= [0.01,5.]
-        if kwargs.has_key('xrange'):
-            xrange= kwargs['xrange']
-            kwargs.pop('xrange')
-        else:
-            xrange= [-5.,5.]
-        if kwargs.has_key('yrange'):
-            yrange= kwargs['yrange']
-            kwargs.pop('yrange')
-        else:
-            yrange= [-5.,5.]
-        if kwargs.has_key('grid'):
-            grid= kwargs['grid']
-            kwargs.pop('grid')
-        else:
-            grid= 1001
-        if kwargs.has_key('gridx'):
-            gridx= kwargs['gridx']
-            kwargs.pop('gridx')
-        else:
-            gridx= 1001
-        if kwargs.has_key('gridy'):
-            gridy= kwargs['gridy']
-            kwargs.pop('gridy')
-        else:
-            gridy= gridx
-        if kwargs.has_key('savefilename'):
-            savefilename= kwargs['savefilename']
-            kwargs.pop('savefilename')
-        else:
-            savefilename= None
-        nonAxi= isNonAxi(Pot)
-        if not savefilename == None and os.path.exists(savefilename):
-            print "Restoring savefile "+savefilename+" ..."
-            savefile= open(savefilename,'rb')
-            potR= pickle.load(savefile)
-            Rs= pickle.load(savefile)
-            if nonAxi:
-                xs= pickle.load(savefile)
-                ys= pickle.load(savefile)
-            else:
-                Rs= pickle.load(savefile)
-            savefile.close()
-        else:
-            if nonAxi:
-                xs= nu.linspace(xrange[0],xrange[1],gridx)
-                ys= nu.linspace(yrange[0],yrange[1],gridy)
-                potR= nu.zeros((gridx,gridy))
-                for ii in range(gridx):
-                    for jj in range(gridy):
-                        thisR= nu.sqrt(xs[ii]**2.+ys[jj]**2.)
-                        if xs[ii] >= 0.:
-                            thisphi= nu.arcsin(ys[jj]/thisR)
-                        else:
-                            thisphi= -nu.arcsin(ys[jj]/thisR)+nu.pi
-                        potR[ii,jj]= evaluateplanarPotentials(thisR,thisphi,Pot)
-            else:
-                Rs= nu.linspace(Rrange[0],Rrange[1],grid)
-                potR= nu.zeros(grid)
-                for ii in range(grid):
-                    potR[ii]= evaluateplanarPotentials(Rs[ii],Pot)
-            if not savefilename == None:
-                print "Writing savefile "+savefilename+" ..."
-                savefile= open(savefilename,'wb')
-                pickle.dump(potR,savefile)
-                if nonAxi:
-                    pickle.dump(xs,savefile)
-                    pickle.dump(ys,savefile)
-                else:
-                    pickle.dump(Rs,savefile)
-                savefile.close()
+    """
+    NAME:
+       plotplanarPotentials
+    PURPOSE:
+       plot a planar potential
+    INPUT:
+       Rrange - range
+       xrange, yrange - if relevant
+       grid, gridx, gridy - number of points to plot
+       savefilename - save to or restore from this savefile (pickle)
+       ncontours - number of contours to plot (if applicable)
+       +bovy_plot(*args,**kwargs) or bovy_dens2d(**kwargs)
+    OUTPUT:
+       plot to output device
+    HISTORY:
+       2010-07-13 - Written - Bovy (NYU)
+    """
+    if kwargs.has_key('Rrange'):
+        Rrange= kwargs['Rrange']
+        kwargs.pop('Rrange')
+    else:
+        Rrange= [0.01,5.]
+    if kwargs.has_key('xrange'):
+        xrange= kwargs['xrange']
+        kwargs.pop('xrange')
+    else:
+        xrange= [-5.,5.]
+    if kwargs.has_key('yrange'):
+        yrange= kwargs['yrange']
+        kwargs.pop('yrange')
+    else:
+        yrange= [-5.,5.]
+    if kwargs.has_key('grid'):
+        grid= kwargs['grid']
+        kwargs.pop('grid')
+    else:
+        grid= 1001
+    if kwargs.has_key('gridx'):
+        gridx= kwargs['gridx']
+        kwargs.pop('gridx')
+    else:
+        gridx= 1001
+    if kwargs.has_key('gridy'):
+        gridy= kwargs['gridy']
+        kwargs.pop('gridy')
+    else:
+        gridy= gridx
+    if kwargs.has_key('savefilename'):
+        savefilename= kwargs['savefilename']
+        kwargs.pop('savefilename')
+    else:
+        savefilename= None
+    nonAxi= isNonAxi(Pot)
+    if not savefilename == None and os.path.exists(savefilename):
+        print "Restoring savefile "+savefilename+" ..."
+        savefile= open(savefilename,'rb')
+        potR= pickle.load(savefile)
+        Rs= pickle.load(savefile)
         if nonAxi:
-            if not kwargs.has_key('origin'):
-                kwargs['origin']= 'lower'
-            if not kwargs.has_key('cmap'):
-                kwargs['cmap']= 'gist_yarg'
-            if not kwargs.has_key('contours'):
-                kwargs['contours']= True
-            if not kwargs.has_key('xlabel'):
-                kwargs['xlabel']= r"$R/R_0$"
-            if not kwargs.has_key('ylabel'):
-                kwargs['ylabel']= "$z/R_0$",
-            if not kwargs.has_key('aspect'):
-                kwargs['aspect']= 1.
-            if not kwargs.has_key('cntrls'):
-                kwargs['cntrls']= '-'
-            if kwargs.has_key('ncontours'):
-                ncontours= kwargs['ncontours']
-                kwargs.pop('ncontours')
-            if not kwargs.has_key('levels'):
-                kwargs['levels']= nu.linspace(nu.nanmin(potR),nu.nanmax(potR),ncontours)
-            return plot.bovy_dens2d(potR.T,
+            xs= pickle.load(savefile)
+            ys= pickle.load(savefile)
+        else:
+            Rs= pickle.load(savefile)
+            savefile.close()
+    else:
+        if nonAxi:
+            xs= nu.linspace(xrange[0],xrange[1],gridx)
+            ys= nu.linspace(yrange[0],yrange[1],gridy)
+            potR= nu.zeros((gridx,gridy))
+            for ii in range(gridx):
+                for jj in range(gridy):
+                    thisR= nu.sqrt(xs[ii]**2.+ys[jj]**2.)
+                    if xs[ii] >= 0.:
+                        thisphi= nu.arcsin(ys[jj]/thisR)
+                    else:
+                        thisphi= -nu.arcsin(ys[jj]/thisR)+nu.pi
+                    potR[ii,jj]= evaluateplanarPotentials(thisR,thisphi,Pot)
+        else:
+            Rs= nu.linspace(Rrange[0],Rrange[1],grid)
+            potR= nu.zeros(grid)
+            for ii in range(grid):
+                potR[ii]= evaluateplanarPotentials(Rs[ii],Pot)
+        if not savefilename == None:
+            print "Writing savefile "+savefilename+" ..."
+            savefile= open(savefilename,'wb')
+            pickle.dump(potR,savefile)
+            if nonAxi:
+                pickle.dump(xs,savefile)
+                pickle.dump(ys,savefile)
+            else:
+                pickle.dump(Rs,savefile)
+                savefile.close()
+    if nonAxi:
+        if not kwargs.has_key('origin'):
+            kwargs['origin']= 'lower'
+        if not kwargs.has_key('cmap'):
+            kwargs['cmap']= 'gist_yarg'
+        if not kwargs.has_key('contours'):
+            kwargs['contours']= True
+        if not kwargs.has_key('xlabel'):
+            kwargs['xlabel']= r"$R/R_0$"
+        if not kwargs.has_key('ylabel'):
+            kwargs['ylabel']= "$z/R_0$",
+        if not kwargs.has_key('aspect'):
+            kwargs['aspect']= 1.
+        if not kwargs.has_key('cntrls'):
+            kwargs['cntrls']= '-'
+        if kwargs.has_key('ncontours'):
+            ncontours= kwargs['ncontours']
+            kwargs.pop('ncontours')
+        if not kwargs.has_key('levels'):
+            kwargs['levels']= nu.linspace(nu.nanmin(potR),nu.nanmax(potR),ncontours)
+        return plot.bovy_dens2d(potR.T,
                                 xrange=xrange,
                                 yrange=yrange,**kwargs)
-        else:
-            return plot.bovy_plot(Rs,potR,*args,
+    else:
+        return plot.bovy_plot(Rs,potR,*args,
                               xlabel=r"$R/R_0$",ylabel=r"$\Phi(R)$",
                               xrange=Rrange,**kwargs)
+    
+def plotRotcurve(Pot,*args,**kwargs):
+rmin=0.,rmax=1.5,nrs=21,
+                 savefilename=None,*args,**kwargs):
+    """
+    NAME:
+       plotRotcurve
+    PURPOSE:
+       plot the rotation curve for this potential (in the z=0 plane for
+       non-spherical potentials)
+    INPUT:
+       Pot - Potential or list of Potential instances
+       rmin - minimum R
+       rmax - maximum R
+       nrs - grid in R
+       savefilename - save to or restore from this savefile (pickle)
+       +matploltib.plot args and kwargs
+    OUTPUT:
+       plot to output device
+    HISTORY:
+       2010-07-10 - Written - Bovy (NYU)
+    """
+    if not savefilename == None and os.path.exists(savefilename):
+        print "Restoring savefile "+savefilename+" ..."
+        savefile= open(savefilename,'rb')
+        rotcurve= pickle.load(savefile)
+        Rs= pickle.load(savefile)
+        savefile.close()
+    else:
+        Rs= nu.linspace(rmin,rmax,nrs)
+        rotcurve= nu.zeros(nrs)
+        for ii in range(nrs):
+            rotcurve[ii]= nu.sqrt(Rs[ii]*-evaluateRforces(Rs[ii],0.,Pot))
+        if not savefilename == None:
+            print "Writing savefile "+savefilename+" ..."
+            savefile= open(savefilename,'wb')
+            pickle.dump(rotcurve,savefile)
+            pickle.dump(Rs,savefile)
+            savefile.close()
+    return plot.bovy_plot(Rs,rotcurve,*args,
+                          xlabel=r"$R/R_0$",ylabel=r"$v_c(R)$",
+                          xrange=[rmin,rmax],**kwargs)
 
