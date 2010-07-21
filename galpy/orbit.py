19cc964ce97f3e05c8f5ed0df715d03cce3b5a36
Jo Bovy <bovy@ias.edu>
2010-07-21 01:48:23 +0000

    add linearOrbit to top-level orbit-product

--- galpy/orbit.py
+++ galpy/orbit.py
@@ -1,6 +1,7 @@
 from galpy.orbit_src import Orbit
 from galpy.orbit_src import RZOrbit
 from galpy.orbit_src import planarOrbit
+from galpy.orbit_src import linearOrbit
 
 #
 # Classes
@@ -10,4 +11,5 @@ RZOrbit= RZOrbit.RZOrbit
 planarOrbitTop= planarOrbit.planarOrbitTop
 planarROrbit= planarOrbit.planarROrbit
 planarOrbit= planarOrbit.planarOrbit
+linearOrbit= linearOrbit.linearOrbit
 
