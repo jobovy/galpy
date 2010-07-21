a8408fab4aee05de51e06f2eab39371b1dcbd8b0
Jo Bovy <bovy@ias.edu>
2010-07-21 17:59:34 +0000

    coordinate vs time plotting for orbits

--- galpy/orbit_src/Orbit.py
+++ galpy/orbit_src/Orbit.py
@@ -111,3 +111,124 @@ class Orbit:
         """
         return self._orb(t)
 
+    def plotRt(self,*args,**kwargs):
+        """
+        NAME:
+           plotRt
+        PURPOSE:
+           plot R(t) along the orbit
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-10 - Written - Bovy (NYU)
+        """
+        self._orb.plotRt(*args,**kwargs)
+
+    def plotzt(self,*args,**kwargs):
+        """
+        NAME:
+           plotzt
+        PURPOSE:
+           plot z(t) along the orbit
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-10 - Written - Bovy (NYU)
+        """
+        self._orb.plotzt(*args,**kwargs)
+
+    def plotvRt(self,*args,**kwargs):
+        """
+        NAME:
+           plotvRt
+        PURPOSE:
+           plot vR(t) along the orbit
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-10 - Written - Bovy (NYU)
+        """
+        self._orb.plotvRt(*args,**kwargs)
+
+    def plotvTt(self,*args,**kwargs):
+        """
+        NAME:
+           plotvTt
+        PURPOSE:
+           plot vT(t) along the orbit
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-10 - Written - Bovy (NYU)
+        """
+        self._orb.plotvTt(*args,**kwargs)
+
+    def plotphit(self,*args,**kwargs):
+        """
+        NAME:
+           plotphit
+        PURPOSE:
+           plot \phi(t) along the orbit
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-10 - Written - Bovy (NYU)
+        """
+        self._orb.plotphit(*args,**kwargs)
+
+    def plotvzt(self,*args,**kwargs):
+        """
+        NAME:
+           plotvzt
+        PURPOSE:
+           plot vz(t) along the orbit
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-10 - Written - Bovy (NYU)
+        """
+        self._orb.plotvzt(*args,**kwargs)
+
+    def plotxt(self,*args,**kwargs):
+        """
+        NAME:
+           plotxt
+        PURPOSE:
+           plot a one-dimensional orbit position
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-21 - Written - Bovy (NYU)
+        """
+        self._orb.plotxt(*args,**kwargs)
+
+    def plotvxt(self,*args,**kwargs):
+        """
+        NAME:
+           plotvxt
+        PURPOSE:
+           plot a one-dimensional orbit velocity
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-21 - Written - Bovy (NYU)
+        """
+        self._orb.plotvxt(*args,**kwargs)
+
+
--- galpy/orbit_src/linearOrbit.py
+++ galpy/orbit_src/linearOrbit.py
@@ -74,6 +74,39 @@ class linearOrbit(OrbitTop):
         plot.bovy_plot(nu.array(self.t),nu.array(self.E)/self.E[0],
                        *args,**kwargs)
 
+    def plotxt(self,*args,**kwargs):
+        """
+        NAME:
+           plotxt
+        PURPOSE:
+           plot a one-dimensional orbit position
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-21 - Written - Bovy (NYU)
+        """
+        plot.bovy_plot(nu.array(self.t),self.orbit[:,0],
+                       *args,**kwargs)
+
+    def plotvxt(self,*args,**kwargs):
+        """
+        NAME:
+           plotvxt
+        PURPOSE:
+           plot a one-dimensional orbit velocity
+        INPUT:
+           bovy_plot.bovy_plot inputs
+        OUTPUT:
+           figure to output device
+        HISTORY:
+           2010-07-21 - Written - Bovy (NYU)
+        """
+        plot.bovy_plot(nu.array(self.t),self.orbit[:,1],
+                       *args,**kwargs)
+
+
 def _integrateLinearOrbit(vxvv,pot,t):
     """
     NAME:
