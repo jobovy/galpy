9b8091e86cfc865a91b091d9898827f9a45019d0
Jo Bovy <bovy@ias.edu>
2010-07-21 02:20:09 +0000

    removed linear-, planar-, and RZOrbits, added Orbit

--- galpy/orbit.py
+++ galpy/orbit.py
@@ -1,15 +1,6 @@
 from galpy.orbit_src import Orbit
-from galpy.orbit_src import RZOrbit
-from galpy.orbit_src import planarOrbit
-from galpy.orbit_src import linearOrbit
 
 #
 # Classes
 #
 Orbit= Orbit.Orbit
-RZOrbit= RZOrbit.RZOrbit
-planarOrbitTop= planarOrbit.planarOrbitTop
-planarROrbit= planarOrbit.planarROrbit
-planarOrbit= planarOrbit.planarOrbit
-linearOrbit= linearOrbit.linearOrbit
-
