12e002f09ca780e52be149e8997bcbb42579717d
Jo Bovy <bovy@ias.edu>
2010-07-22 03:50:35 +0000

    interpRZPotential now works

--- galpy/potential_src/interpRZPotential.py
+++ galpy/potential_src/interpRZPotential.py
@@ -1,6 +1,7 @@
 import numpy as nu
 from scipy import interpolate
 from Potential import Potential
+_DEBUG= True
 class interpRZPotential(Potential):
     """Class that interpolates a given potential on a grid for fast orbit integration"""
     def __init__(self,RZPot,rgrid=(0.01,2.,101),zgrid=(-0.2,0.2,101),
@@ -26,31 +27,38 @@ class interpRZPotential(Potential):
         if logR:
             self._rgrid= nu.exp(self._rgrid)
         self._zgrid= nu.linspace(*zgrid)
-        Rforce= nu.zeros((len(self._rgrid),len(self._zgrid)))
-        zforce= nu.zeros((len(self._rgrid),len(self._zgrid)))
-        print "Computing forces on grid"
+        R= nu.zeros(len(self._rgrid)*len(self._zgrid))
+        z= nu.zeros(len(self._rgrid)*len(self._zgrid))
+        Rforce= nu.zeros(len(self._rgrid)*len(self._zgrid))
+        zforce= nu.zeros(len(self._rgrid)*len(self._zgrid))
+        if _DEBUG:
+            print "Computing forces on grid ..."
         for ii in range(len(self._rgrid)):
             for jj in range(len(self._zgrid)):
-                Rforce[ii,jj]= RZPot.Rforce(self._rgrid[ii],self._zgrid[jj])
-                zforce[ii,jj]= RZPot.zforce(self._rgrid[ii],self._zgrid[jj])
-        self._interpRforce= interpolate.bisplrep(self._rgrid,self._zgrid,
-                                                 Rforce,s=0)
-        self._interpzforce= interpolate.bisplrep(self._rgrid,self._zgrid,
-                                                 zforce,s=0)
+                Rforce[ii*len(self._zgrid)+jj]= RZPot.Rforce(self._rgrid[ii],
+                                                             self._zgrid[jj])
+                zforce[ii*len(self._zgrid)+jj]= RZPot.zforce(self._rgrid[ii],
+                                                             self._zgrid[jj])
+                R[ii*len(self._zgrid)+jj]= self._rgrid[ii]
+                z[ii*len(self._zgrid)+jj]= self._zgrid[jj]
+        if _DEBUG:
+            print "Interpolating ..."
+        self._interpRforce= interpolate.interp2d(R,z,Rforce,bounds_error=True)
+        self._interpzforce= interpolate.interp2d(R,z,zforce,bounds_error=True)
                                                  
 
-    def _Rforce(R,z):
+    def _Rforce(self,R,z):
         if R < self._rgrid[0] or R > self._rgrid[-1] \
                 or z < self._zgrid[0] or z > self._zgrid[-1]:
             print "Current position out of range of interpolation, consider interpolating on a larger range"
-            return self._RZPot.Rforce(R,z)
+            return self._origPot.Rforce(R,z)
         else:
-            return interpolate.bisplev(R,z,self._interpRforce)
+            return self._interpRforce(R,z)
 
-    def _zforce(R,z):
+    def _zforce(self,R,z):
         if R < self._rgrid[0] or R > self._rgrid[-1] \
                 or z < self._zgrid[0] or z > self._zgrid[-1]:
             print "Current position out of range of interpolation, consider interpolating on a larger range"
-            return self._RZPot.zforce(R,z)
+            return self._origPot.zforce(R,z)
         else:
-            return interpolate.bisplev(R,z,self._interpzforce)
+            return self._interpzforce(R,z)
