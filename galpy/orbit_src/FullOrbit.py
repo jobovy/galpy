7808eb75a485dea782f1da9eef6218a2bb6aa174
Jo Bovy <bovy@ias.edu>
2010-11-24 16:48:40 +0000

    axis labels in plotEz

--- galpy/orbit_src/FullOrbit.py
+++ galpy/orbit_src/FullOrbit.py
@@ -210,6 +210,9 @@ class FullOrbit(OrbitTop):
         HISTORY:
            2010-07-10 - Written - Bovy (NYU)
         """
+        labeldict= {'t':r'$t$','R':r'$R$','vR':r'$v_R$','vT':r'$v_T$',
+                    'z':r'$z$','vz':r'$v_z$','phi':r'$\phi$',
+                    'x':r'$x$','y':r'$y$','vx':r'$v_x$','vy':r'$v_y$'}
         if not kwargs.has_key('pot'):
             try:
                 pot= self._pot
@@ -228,6 +231,10 @@ class FullOrbit(OrbitTop):
                    evaluatePotentials(self.orbit[ii,0],0.,pot,
                                       phi=self.orbit[ii,5])+
                   self.orbit[ii,4]**2./2. for ii in range(len(self.t))]
+        if not kwargs.has_key('xlabel'):
+            kwargs['xlabel']= labeldict[d1]
+        if not kwargs.has_key('ylabel'):
+            kwargs['ylabel']= r'$E_z$'
         if d1 == 't':
             plot.bovy_plot(nu.array(self.t),nu.array(self.Ezs)/self.Ezs[0],
                            *args,**kwargs)
