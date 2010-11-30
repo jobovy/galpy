356a19bdf1d09d8bd253022e1fd46c934b726f19
Jo Bovy <bovy@ias.edu>
2010-11-30 21:08:26 +0000

    add actionAnglePower to top-level

--- galpy/actionAngle.py
+++ galpy/actionAngle.py
@@ -1,8 +1,10 @@
 from galpy.actionAngle_src import actionAngle
 from galpy.actionAngle_src import actionAngleFlat
+from galpy.actionAngle_src import actionAnglePower
 
 #
 # Classes
 #
 actionAngle= actionAngle.actionAngle
 actionAngleFlat= actionAngleFlat.actionAngleFlat
+actionAnglePower= actionAnglePower.actionAnglePower
