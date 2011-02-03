71509cd0c7710407ed58126f33bdbbe5644545cf
Jo Bovy <bovy@ias.edu>
2011-02-03 01:13:04 +0000

    fix leapfrog integrator, estimate step size, seems to work well enough

--- galpy/util/bovy_symplecticode.py
+++ galpy/util/bovy_symplecticode.py
@@ -1,6 +1,7 @@
 #Symplectic ODE integrators
 #Follows scipy.integrate.odeint inputs
 import numpy as nu
+from numpy import linalg
 def leapfrog(func,yo,t,args=(),rtol=None,atol=None):
     """
     NAME:
@@ -22,13 +23,17 @@ def leapfrog(func,yo,t,args=(),rtol=None,atol=None):
     if rtol is None and atol is None:
         rtol= 1.49012e-8
         atol= 0.
-    dt= t[1]-t[0] #FOR NOW BOVY
-    ndt= 1
     #Initialize
     qo= yo[0:len(yo)/2]
     po= yo[len(yo)/2:len(yo)]
     out= nu.zeros((len(yo),len(t)))
     out[:,0]= yo
+    #Estimate necessary step size
+    dt= t[1]-t[0] #assumes that the steps are equally spaced
+    init_dt= dt
+    dt= _leapfrog_estimate_step(func,qo,po,dt,t[0],args,rtol,atol)
+    ndt= int(init_dt/dt)
+    print dt, ndt
     #Integrate
     to= t[0]
     for ii in range(1,len(t)):
@@ -37,20 +42,46 @@ def leapfrog(func,yo,t,args=(),rtol=None,atol=None):
             #drift
             q12= leapfrog_leapq(qo,po,dt/2.)
             #kick
-            force= func(q12,to+dt/2.,*args)
-            p1= leapfrog_leapp(po,dt,force)
+            force= func(q12,*args,t=to+dt/2)
+            po= leapfrog_leapp(po,dt,force)
             #drift
-            q1= leapfrog_leapq(qo,p1,dt/2.)
+            qo= leapfrog_leapq(q12,po,dt/2.)
             #Get ready for next
-            qo= q1
-            po= p1
             to+= dt
         out[0:len(yo)/2,ii]= qo
         out[len(yo)/2:len(yo),ii]= po
-    return out
+    return out.T
 
 def leapfrog_leapq(q,p,dt):
     return q+dt*p
 
 def leapfrog_leapp(p,dt,force):
     return p+dt*force
+
+def _leapfrog_estimate_step(func,qo,po,dt,to,args,rtol,atol):
+    scale= atol+rtol*nu.array([nu.fabs(qo),nu.fabs(po)]).flatten()
+    print scale
+    err= 2.
+    dt*= 2.
+    while err > 1.:
+        #Do one leapfrog step with step dt and one with dt/2.
+        #dt
+        q12= leapfrog_leapq(qo,po,dt/2.)
+        force= func(q12,*args,t=to+dt/2)
+        p11= leapfrog_leapp(po,dt,force)
+        q11= leapfrog_leapq(q12,p11,dt/2.)
+        #dt/2.
+        q12= leapfrog_leapq(qo,po,dt/4.)
+        force= func(q12,*args,t=to+dt/4)
+        ptmp= leapfrog_leapp(po,dt/2.,force)
+        qtmp= leapfrog_leapq(q12,ptmp,dt/2.)#Take full step combining two half
+        force= func(qtmp,*args,t=to+3.*dt/4)
+        p12= leapfrog_leapp(ptmp,dt/2.,force)
+        q12= leapfrog_leapq(qtmp,p12,dt/4.)
+        #Norm
+        delta= nu.array([nu.fabs(q11-q12),nu.fabs(p11-p12)]).flatten()
+        err= nu.sqrt(nu.mean((delta/scale)**2.))
+        print err
+        dt/= 2.
+    return dt
+
