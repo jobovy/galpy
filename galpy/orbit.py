31e052c5b0de4153a79cd9f2b826e525fafe0093
Jo Bovy <bovy@odin.sns.ias.edu>
2011-09-30 15:38:04 -0400

    put the default boundary conditions at the top level

--- galpy/orbit.py
+++ galpy/orbit.py
@@ -1,6 +1,18 @@
 from galpy.orbit_src import Orbit
 
 #
+# Functions
+#
+# Boundary conditions
+zEqZeroBC= Orbit._zEqZeroBC
+vzEqZeroBC= Orbit._vzEqZeroBC
+REqZeroBC= Orbit._REqZeroBC
+vREqZeroBC= Orbit._vREqZeroBC
+vTEqZeroBC= Orbit._vTEqZeroBC
+phiEqZeroBC= Orbit._phiEqZeroBC
+
+#
 # Classes
 #
 Orbit= Orbit.Orbit
+
