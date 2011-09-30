456844da2ab9d25ce38452403dfab43b331e2de1
Jo Bovy <bovy@odin.sns.ias.edu>
2011-09-30 15:40:49 -0400

    move defaults up

--- galpy/orbit.py
+++ galpy/orbit.py
@@ -7,8 +7,10 @@ from galpy.orbit_src import Orbit
 zEqZeroBC= Orbit._zEqZeroBC
 vzEqZeroBC= Orbit._vzEqZeroBC
 REqZeroBC= Orbit._REqZeroBC
+REqOneBC= Orbit._REqOneBC
 vREqZeroBC= Orbit._vREqZeroBC
 vTEqZeroBC= Orbit._vTEqZeroBC
+vTEqOneBC= Orbit._vTEqOneBC
 phiEqZeroBC= Orbit._phiEqZeroBC
 
 #
