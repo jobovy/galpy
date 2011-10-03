150769649b5071b3292254dc5c4c361af0e8f85c
Jo Bovy <bovy@odin.sns.ias.edu>
2011-10-03 16:35:59 -0400

    start on h file for potentials

---
+++ galpy/potential_src/potential_c_ext/galpy_potentials.h
@@ -0,0 +1,11 @@
+/* C implementations of galpy potentials */
+/*
+  Function declarations
+*/
+//LogarithmicHaloPotential
+double LogarithmicHaloPotentialRforce(double ,double , double ,
+				      int , double *, 
+				      double * );
+double LogarithmicHaloPotentialzforce(double,double,double,
+				      int, double, 
+				      double *);
