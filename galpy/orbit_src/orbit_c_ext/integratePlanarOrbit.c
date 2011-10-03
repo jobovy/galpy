bd887dd86f3c8bb1bb0befe6571f390041c71c13
Jo Bovy <bovy@odin.sns.ias.edu>
2011-10-03 16:46:04 -0400

    write integration function

--- galpy/orbit_src/orbit_c_ext/integratePlanarOrbit.c
+++ galpy/orbit_src/orbit_c_ext/integratePlanarOrbit.c
@@ -2,9 +2,18 @@
   Wrappers around the C integration code for planar Orbits
 */
 #include <stdbool.h>
+#include <math.h>
 #include <bovy_symplecticode.h>
 //Potentials
 #include <galpy_potentials.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+/*
+  Function Declarations
+*/
+void evalPlanarRectForce(int, double, double *, double *,
+			 int, struct leapFuncArg *);
 void integratePlanarOrbit(int dim,
 			  double *yo,
 			  int nt, 
@@ -21,6 +30,7 @@ void integratePlanarOrbit(int dim,
   bool lp= (bool) lp;
   if ( lp ) npot++;
   struct leapFuncArg * leapFuncArgs= (struct leapFuncArg *) malloc ( npot * sizeof (struct  leapFuncArg) );
+  //LogarithmicHaloPotential
   if ( lp ){
     leapFuncArgs->Rforce= &LogarithmicHaloPotentialRforce;
     leapFuncArgs->zforce= &LogarithmicHaloPotentialzforce;
@@ -30,6 +40,21 @@ void integratePlanarOrbit(int dim,
       *(leapFuncArgs->args)++= *lpargs++;
     leapFuncArgs->args-= leapFuncArgs->nargs;
     lpargs-= leapFuncArgs->nargs;
-  
+  }
+  //Integrate
+  leapfrog(evalPlanarRectForce,dim,yo,nt,t,npot,leapFuncArgs,rtol,atol,result);
+  //Done!
 }
+
+void evalPlanarRectForce(int dim, double t, double *q, double *a,
+			 int nargs, struct leapFuncArg * leapFuncArgs){
+  //q is rectangular so calculate R and phi
+  x= *q;
+  y= *(q+1);
+  R= sqrt(x*x+y*y);
+  phi= acos(x/R);
+  sinphi= y/R;
+  cosphi= x/R;
+  if ( y < 0. ) phi= phi+2.*M_PI;
+  //Calculate the forces
 }
