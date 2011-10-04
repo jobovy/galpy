25ae5d0a403d8e53bde4ff20ceaf74588ff19b38
Jo Bovy <bovy@odin.sns.ias.edu>
2011-10-04 16:56:46 -0400

    add time and bar

--- galpy/potential_src/potential_c_ext/galpy_potentials.h
+++ galpy/potential_src/potential_c_ext/galpy_potentials.h
@@ -6,9 +6,12 @@
 double ZeroPlanarForce(double, double,int, double *);
 double ZeroForce(double,double,double,int, double *);
 //LogarithmicHaloPotential
-double LogarithmicHaloPotentialRforce(double ,double , double ,
+double LogarithmicHaloPotentialRforce(double ,double , double, double,
 				      int , double *);
-double LogarithmicHaloPotentialPlanarRforce(double ,double ,
+double LogarithmicHaloPotentialPlanarRforce(double ,double, double,
 					    int , double *);
-double LogarithmicHaloPotentialzforce(double,double,double,
+double LogarithmicHaloPotentialzforce(double,double,double,double,
 				      int, double *);
+//DehnenBarPotential
+double DehnenBarPotentialRforce(double,double,double,int,double *);
+double DehnenBarPotentialphiforce(double,double,double,int,double *);
