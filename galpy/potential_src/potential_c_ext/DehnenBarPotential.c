7a686c83ff507ce2c7f120e2752bf12ae332a8cc
Jo Bovy <bovy@odin.sns.ias.edu>
2011-10-04 17:09:18 -0400

    implement bar potential

--- galpy/potential_src/potential_c_ext/DehnenBarPotential.c
+++ galpy/potential_src/potential_c_ext/DehnenBarPotential.c
@@ -1,17 +1,55 @@
+#include <math.h>
 #include <galpy_potentials.h>
 //DehnenBarPotential
 //
+inline double calcSmooth(double tform, double tsteady){
+  double smooth, xi;
+  if ( t < tform )
+    smooth= 0.;
+  else if ( t < tsteady ) {
+    deltat= t-tform;
+    xi= 2.*deltat/(tsteady-tform)-1.;
+    smooth= (3./16.*pow(xi,5.)-5./8.*pow(xi,3.)+15./16.*xi+.5);
+  }
+  else
+    smooth= 1.;
+  return smooth;
+}
 double DehnenBarPotentialRforce(double R,double phi,double t,
 				      int nargs, double *args){
+  //declare
+  double smooth;
   //Get args
   double amp= *args++;
+  double tform= *args++;
+  double tsteady= *args++;
+  double rb= *args++;
+  double af= *args++;
+  double omegab= *args++;
+  double barphi= *args++;
   //Calculate Rforce
-  return 0.;
+  smooth= calc_smooth(tform,tsteady);
+  if (R <= rb )
+    return -3.*amp*af*smooth*cos(2.*(phi-omegab*t-barphi))*pow(R/rb,3.)/R;
+  else
+    return -3.*amp*af*smooth*cos(2.*(phi-omegab*t-barphi))*pow(rb/R,3.)/R;   
 }
 double DehnenBarPotentialphiforce(double R,double phi,double t,
 				      int nargs, double *args){
+  //declare
+  double smooth;
   //Get args
   double amp= *args++;
+  double tform= *args++;
+  double tsteady= *args++;
+  double rb= *args++;
+  double af= *args++;
+  double omegab= *args++;
+  double barphi= *args++;
   //Calculate phiforce
-  return 0.;
+  smooth= calc_smooth(tform,tsteady);
+  if (R <= rb )
+    return 2.*amp*af*smooth*sin(2.*(phi-omegab*t-barphi))*(pow(R/rb,3.)-2.);
+  else
+    return 2.*amp*af*smooth*sin(2.*(phi-omegab*t-barphi))*pow(rb/R,3.);
 }
