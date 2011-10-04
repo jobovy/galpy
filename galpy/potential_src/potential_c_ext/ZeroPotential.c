83c5615f04026fe5480b50891a6892ce11a31dc4
Jo Bovy <jo.bovy@gmail.com>
2011-10-03 22:09:58 -0400

    fix zeroForce

--- galpy/potential_src/potential_c_ext/ZeroPotential.c
+++ galpy/potential_src/potential_c_ext/ZeroPotential.c
@@ -2,9 +2,9 @@
 //Dummy zero forces
 double zeroPlanarForce(double R, double phi,
 		       int nargs, double *args){
-  return 0.
-    }
+  return 0.;
+}
 double zeroForce(double R, double Z, double phi,
 		 int nargs, double *args){
-  return 0.
-    }
+  return 0.;
+}
