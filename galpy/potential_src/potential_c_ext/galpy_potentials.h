28d8dbe8aa5dc155a8141edc40303e060c5bba1b
Jo Bovy <jo.bovy@gmail.com>
2011-10-03 21:32:00 -0400

    add dummy zero forces

--- galpy/potential_src/potential_c_ext/galpy_potentials.h
+++ galpy/potential_src/potential_c_ext/galpy_potentials.h
@@ -2,10 +2,16 @@
 /*
   Function declarations
 */
+//ZeroForce
+double zeroPlanarForce(double, double,int, double);
+double zeroForce(double,int, double);
 //LogarithmicHaloPotential
 double LogarithmicHaloPotentialRforce(double ,double , double ,
 				      int , double *, 
 				      double * );
+double LogarithmicHaloPotentialPlanarRforce(double ,double ,
+					    int , double *, 
+					    double * );
 double LogarithmicHaloPotentialzforce(double,double,double,
 				      int, double, 
 				      double *);
