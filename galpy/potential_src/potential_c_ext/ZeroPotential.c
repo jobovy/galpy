1d11400b4ad00794dea9cf66c7c73483435b79ad
Jo Bovy <jo.bovy@gmail.com>
2011-10-03 21:31:44 -0400

    add dummy zero forces

---
+++ galpy/potential_src/potential_c_ext/ZeroPotential.c
@@ -0,0 +1,10 @@
+#include <galpy_potentials.h>
+//Dummy zero forces
+double zeroPlanarForce(double R, double phi,
+		       int nargs, double *args){
+  return 0.
+    }
+double zeroForce(double R, double Z, double phi,
+		 int nargs, double *args){
+  return 0.
+    }
