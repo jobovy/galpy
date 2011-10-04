dc1a69b5c2bb44d76d3202b00a34a9fd8478bf42
Jo Bovy <jo.bovy@gmail.com>
2011-10-03 21:26:37 -0400

    add Planar part, change output again

--- galpy/potential_src/potential_c_ext/LogarithmicHaloPotential.c
+++ galpy/potential_src/potential_c_ext/LogarithmicHaloPotential.c
@@ -1,24 +1,27 @@
 //LogarithmicHaloPotential
 //2 arguments: q and c2
 double LogarithmicHaloPotentialRforce(double R,double Z, double phi,
-				      int nargs, double *args, 
-				      double * result){
+				      int nargs, double *args){
   //Get args
-  double q= *args;
-  args++;
-  double c= *args;
+  double q= *args++;
+  double c= *args--;
   //Calculate Rforce
   double zq= z/q;
-  *result= -R/(R*R+zq*zq+c);
+  return -R/(R*R+zq*zq+c);
+}
+double LogarithmicHaloPotentialPlanarRforce(double R,double phi,
+					    int nargs, double *args){
+  //Get args
+  double c= *args;
+  //Calculate Rforce
+  return -R/(R*R+c);
 }
 double LogarithmicHaloPotentialzforce(double R,double z,double phi,
-				      int nargs, double *args, 
-				      double * result){
+				      int nargs, double *args){
   //Get args
-  double q= *args;
-  args++;
-  double c= *args;
+  double q= *args++;
+  double c= *args--;
   //Calculate zforce
   double zq= z/q;
-  *result= -z/q/q/(R*R+zq*zq+c);
+  return -z/q/q/(R*R+zq*zq+c);
 }
