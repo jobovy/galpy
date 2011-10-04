6bfff9808208c10ec8b6c0884436b59cf851dcda
Jo Bovy <jo.bovy@gmail.com>
2011-10-03 22:25:40 -0400

    remove dim from arguments, get zeroForce more in line

--- galpy/potential_src/potential_c_ext/ZeroPotential.c
+++ galpy/potential_src/potential_c_ext/ZeroPotential.c
@@ -1,10 +1,10 @@
 #include <galpy_potentials.h>
 //Dummy zero forces
-double zeroPlanarForce(double R, double phi,
+double ZeroPlanarForce(double R, double phi,
 		       int nargs, double *args){
   return 0.;
 }
-double zeroForce(double R, double Z, double phi,
+double ZeroForce(double R, double Z, double phi,
 		 int nargs, double *args){
   return 0.;
 }
