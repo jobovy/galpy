a864ba379cdeb39dbd507e32222bae1927cf2ecc
Jo Bovy <bovy@odin.sns.ias.edu>
2011-10-04 16:44:10 -0400

    fix arguments to LogPotential

--- galpy/potential_src/potential_c_ext/LogarithmicHaloPotential.c
+++ galpy/potential_src/potential_c_ext/LogarithmicHaloPotential.c
@@ -5,25 +5,28 @@
 double LogarithmicHaloPotentialRforce(double R,double Z, double phi,
 				      int nargs, double *args){
   //Get args
+  double amp= *args++;
   double q= *args++;
   double c= *args--;
   //Calculate Rforce
   double zq= Z/q;
-  return -R/(R*R+zq*zq+c);
+  return - amp * R/(R*R+zq*zq+c);
 }
 double LogarithmicHaloPotentialPlanarRforce(double R,double phi,
 					    int nargs, double *args){
   //Get args
+  double amp= *args++;
   double c= *args;
   //Calculate Rforce
-  return -R/(R*R+c);
+  return -amp * R/(R*R+c);
 }
 double LogarithmicHaloPotentialzforce(double R,double z,double phi,
 				      int nargs, double *args){
   //Get args
+  double amp= *args++;
   double q= *args++;
   double c= *args--;
   //Calculate zforce
   double zq= z/q;
-  return -z/q/q/(R*R+zq*zq+c);
+  return -amp * z/q/q/(R*R+zq*zq+c);
 }
