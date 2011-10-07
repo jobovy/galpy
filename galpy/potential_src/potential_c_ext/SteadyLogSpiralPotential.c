15a0070f59159f6a7ef88792d7cc20344a4fc9a5
Jo Bovy <bovy@odin.sns.ias.edu>
2011-10-07 12:14:03 -0400

    no smoothing if nan

--- galpy/potential_src/potential_c_ext/SteadyLogSpiralPotential.c
+++ galpy/potential_src/potential_c_ext/SteadyLogSpiralPotential.c
@@ -2,6 +2,23 @@
 #include <galpy_potentials.h>
 //SteadyLogSpiralPotential
 //
+inline double dehnenSpiralSmooth(double t,double tform, double tsteady){
+  double smooth, xi,deltat;
+  //Slightly different smoothing
+  if ( ! isnan(tform) )
+    if ( t < tform )
+      smooth= 0.;
+    else if ( t < tsteady ) {
+      deltat= t-tform;
+      xi= 2.*deltat/(tsteady-tform)-1.;
+      smooth= (3./16.*pow(xi,5.)-5./8.*pow(xi,3.)+15./16.*xi+.5);
+    }
+    else
+      smooth= 1.;
+  else
+    smooth=1.;
+  return smooth;
+}
 double SteadyLogSpiralPotentialRforce(double R,double phi,double t,
 				      int nargs, double *args){
   //declare
@@ -16,7 +33,7 @@ double SteadyLogSpiralPotentialRforce(double R,double phi,double t,
   double omegas= *args++;
   double gamma= *args++;
   //Calculate Rforce
-  smooth= dehnenSmooth(t,tform,tsteady);
+  smooth= dehnenSpiralSmooth(t,tform,tsteady);
   return amp * smooth * A / R * sin(alpha * log(R) + m * (phi-omegas*t-gamma));
 }
 double SteadyLogSpiralPotentialphiforce(double R,double phi,double t,
@@ -33,7 +50,7 @@ double SteadyLogSpiralPotentialphiforce(double R,double phi,double t,
   double omegas= *args++;
   double gamma= *args++;
   //Calculate Rforce
-  smooth= dehnenSmooth(t,tform,tsteady);
+  smooth= dehnenSpiralSmooth(t,tform,tsteady);
   return amp * smooth * A / alpha * m * 
     sin(alpha * log(R) + m * (phi-omegas*t-gamma));
 }
