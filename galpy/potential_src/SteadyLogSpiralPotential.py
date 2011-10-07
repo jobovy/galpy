91bbbbaaf06f0e9e131ce4b021529dfe6f6a5cca
Jo Bovy <bovy@odin.sns.ias.edu>
2011-10-07 12:13:48 -0400

    tsteady None initialization for C extension

--- galpy/potential_src/SteadyLogSpiralPotential.py
+++ galpy/potential_src/SteadyLogSpiralPotential.py
@@ -74,7 +74,7 @@ class SteadyLogSpiralPotential(planarPotential):
         if not tsteady is None:
             self._tsteady= self._tform+tsteady*self._ts
         else:
-            if self._tform is None: pass
+            if self._tform is None: self._tsteady= None
             else: self._tsteady= self._tform+2.*self._ts
         self.hasC= True
 
