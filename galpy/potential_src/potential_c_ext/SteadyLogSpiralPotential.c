9c3450cca898f3e708e1f91a8a3ed5940f890c7f
Jo Bovy <jo.bovy@gmail.com>
2011-10-16 15:10:39 -0400

    fix C implementation of spirals

--- galpy/potential_src/potential_c_ext/SteadyLogSpiralPotential.c
+++ galpy/potential_src/potential_c_ext/SteadyLogSpiralPotential.c
@@ -34,7 +34,7 @@ double SteadyLogSpiralPotentialRforce(double R,double phi,double t,
   double gamma= *args++;
   //Calculate Rforce
   smooth= dehnenSpiralSmooth(t,tform,tsteady);
-  return amp * smooth * A / R * sin(alpha * log(R) + m * (phi-omegas*t-gamma));
+  return amp * smooth * A / R * sin(alpha * log(R) - m * (phi-omegas*t-gamma));
 }
 double SteadyLogSpiralPotentialphiforce(double R,double phi,double t,
 				      int nargs, double *args){
@@ -51,6 +51,6 @@ double SteadyLogSpiralPotentialphiforce(double R,double phi,double t,
   double gamma= *args++;
   //Calculate Rforce
   smooth= dehnenSpiralSmooth(t,tform,tsteady);
-  return amp * smooth * A / alpha * m * 
-    sin(alpha * log(R) + m * (phi-omegas*t-gamma));
+  return -amp * smooth * A / alpha * m * 
+    sin(alpha * log(R) - m * (phi-omegas*t-gamma));
 }
