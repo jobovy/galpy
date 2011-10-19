fbb9aeda4a7b9ac7a1bc73b024c7194c1d3930ff
Jo Bovy <jo.bovy@gmail.com>
2011-10-19 16:13:26 -0400

    add C implementation of EllipticalDiskPotential

---
+++ galpy/potential_src/potential_c_ext/EllipticalDiskPotential.c
@@ -0,0 +1,53 @@
+#include <math.h>
+#include <galpy_potentials.h>
+//EllipticalDiskPotential
+//
+inline double EllipticalDiskSmooth(double t,double tform, double tsteady){
+  double smooth, xi,deltat;
+  //Slightly different smoothing
+  if ( ! isnan(tform) )
+    if ( t < tform )
+      smooth= 0.;
+    else if ( t < tsteady ) {
+      deltat= t-tform;
+      xi= 2.*deltat/(tsteady-tform)-1.;
+      smooth= (3./16.*pow(xi,5.)-5./8.*pow(xi,3.)+15./16.*xi+.5);
+    }
+    else
+      smooth= 1.;
+  else
+    smooth=1.;
+  return smooth;
+}
+double EllipticalDiskPotentialRforce(double R,double phi,double t,
+				     int nargs, double *args){
+  //declare
+  double smooth;
+  //Get args
+  double amp= *args++;
+  double tform= *args++;
+  double tsteady= *args++;
+  double twophio= *args++;
+  double p= *args++;
+  double phib= *args++;
+  //Calculate Rforce
+  smooth= EllipticalDiskSmooth(t,tform,tsteady);
+  return -amp * smooth * p * twophio / 2. * pow(R,p-1.) 
+    * cos( 2. * (phi - phib));
+}
+double EllipticalDiskPotentialphiforce(double R,double phi,double t,
+				       int nargs, double *args){
+  //declare
+  double smooth;
+  //Get args
+  double amp= *args++;
+  double tform= *args++;
+  double tsteady= *args++;
+  double twophio= *args++;
+  double p= *args++;
+  double phib= *args++;
+  //Calculate Rforce
+  smooth= EllipticalDiskSmooth(t,tform,tsteady);
+  return amp * smooth * twophio * pow(R,p) 
+    * sin( 2. * (phi-phib));
+}
