9e170e06ba1089e14f4787bc083276c981e9b44a
Jo Bovy <bovy@odin.sns.ias.edu>
2011-10-19 18:14:51 -0400

    implememt dxdv integration in C; compiles

--- galpy/orbit_src/orbit_c_ext/integratePlanarOrbit.c
+++ galpy/orbit_src/orbit_c_ext/integratePlanarOrbit.c
@@ -19,27 +19,25 @@ void evalPlanarRectForce(double, double *, double *,
 			 int, struct leapFuncArg *);
 void evalPlanarRectDeriv(double, double *, double *,
 			 int, struct leapFuncArg *);
+void evalPlanarRectDeriv_dxdv(double, double *, double *,
+			      int, struct leapFuncArg *);
 double calcPlanarRforce(double, double, double, 
 			int, struct leapFuncArg *);
 double calcPlanarphiforce(double, double, double, 
 			int, struct leapFuncArg *);
+double calcPlanarR2deriv(double, double, double, 
+			 int, struct leapFuncArg *);
+double calcPlanarphi2deriv(double, double, double, 
+			   int, struct leapFuncArg *);
+double calcPlanarRphideriv(double, double, double, 
+			   int, struct leapFuncArg *);
 /*
   Actual functions
 */
-void integratePlanarOrbit(double *yo,
-			  int nt, 
-			  double *t,
-			  int npot,
-			  int * pot_type,
-			  double * pot_args,
-			  double rtol,
-			  double atol,
-			  double *result,
-			  int odeint_type){
-  //Set up the forces, first count
-  int ii, jj;
-  int dim;
-  struct leapFuncArg * leapFuncArgs= (struct leapFuncArg *) malloc ( npot * sizeof (struct leapFuncArg) );
+inline void parse_leapFuncArgs(int npot,struct leapFuncArg * leapFuncArgs,
+			       int * pot_type,
+			       double * pot_args){
+  int ii,jj;
   for (ii=0; ii < npot; ii++){
     switch ( *pot_type++ ) {
     case 0: //LogarithmicHaloPotential, 2 arguments
@@ -77,6 +75,22 @@ void integratePlanarOrbit(double *yo,
     leapFuncArgs++;
   }
   leapFuncArgs-= npot;
+}
+void integratePlanarOrbit(double *yo,
+			  int nt, 
+			  double *t,
+			  int npot,
+			  int * pot_type,
+			  double * pot_args,
+			  double rtol,
+			  double atol,
+			  double *result,
+			  int odeint_type){
+  //Set up the forces, first count
+  int ii;
+  int dim;
+  struct leapFuncArg * leapFuncArgs= (struct leapFuncArg *) malloc ( npot * sizeof (struct leapFuncArg) );
+  parse_leapFuncArgs(npot,leapFuncArgs,pot_type,pot_args);
   //Integrate
   void (*odeint_func)(void (*func)(double, double *, double *,
 			   int, struct leapFuncArg *),
@@ -117,6 +131,75 @@ void integratePlanarOrbit(double *yo,
   case 5: //DOPR54
     odeint_func= &bovy_dopr54;
     odeint_deriv_func= &evalPlanarRectDeriv;
+    dim= 4;
+    break;
+  }
+  odeint_func(odeint_deriv_func,dim,yo,nt,t,npot,leapFuncArgs,rtol,atol,result);
+  //Free allocated memory
+  for (ii=0; ii < npot; ii++) {
+    free(leapFuncArgs->args);
+    leapFuncArgs++;
+  }
+  leapFuncArgs-= npot;
+  free(leapFuncArgs);
+  //Done!
+}
+
+void integratePlanarOrbit_dxdv(double *yo,
+			       int nt, 
+			       double *t,
+			       int npot,
+			       int * pot_type,
+			       double * pot_args,
+			       double rtol,
+			       double atol,
+			       double *result,
+			       int odeint_type){
+  //Set up the forces, first count
+  int ii;
+  int dim;
+  struct leapFuncArg * leapFuncArgs= (struct leapFuncArg *) malloc ( npot * sizeof (struct leapFuncArg) );
+  parse_leapFuncArgs(npot,leapFuncArgs,pot_type,pot_args);
+  //Integrate
+  void (*odeint_func)(void (*func)(double, double *, double *,
+			   int, struct leapFuncArg *),
+		      int,
+		      double *,
+		      int, double *,
+		      int, struct leapFuncArg *,
+		      double, double,
+		      double *);
+  void (*odeint_deriv_func)(double, double *, double *,
+			    int,struct leapFuncArg *);
+  switch ( odeint_type ) {
+  case 0: //leapfrog
+    odeint_func= &leapfrog;
+    odeint_deriv_func= &evalPlanarRectForce;
+    dim= 2;
+    break;
+  case 1: //RK4
+    odeint_func= &bovy_rk4;
+    odeint_deriv_func= &evalPlanarRectDeriv_dxdv;
+    dim= 4;
+    break;
+  case 2: //RK6
+    odeint_func= &bovy_rk6;
+    odeint_deriv_func= &evalPlanarRectDeriv_dxdv;
+    dim= 4;
+    break;
+  case 3: //symplec4
+    odeint_func= &symplec4;
+    odeint_deriv_func= &evalPlanarRectForce;
+    dim= 2;
+    break;
+  case 4: //symplec6
+    odeint_func= &symplec6;
+    odeint_deriv_func= &evalPlanarRectForce;
+    dim= 2;
+    break;
+  case 5: //DOPR54
+    odeint_func= &bovy_dopr54;
+    odeint_deriv_func= &evalPlanarRectDeriv_dxdv;
     dim= 4;
     break;
   }
@@ -195,4 +278,98 @@ double calcPlanarphiforce(double R, double phi, double t,
   }
   leapFuncArgs-= nargs;
   return phiforce;
+}
+
+void evalPlanarRectDeriv_dxdv(double t, double *q, double *a,
+			      int nargs, struct leapFuncArg * leapFuncArgs){
+  double sinphi, cosphi, x, y, phi,R,Rforce,phiforce;
+  double R2deriv, phi2deriv, Rphideriv, dFxdx, dFxdy, dFydx, dFydy;
+  //first two derivatives are just the velocities
+  *a++= *(q+2);
+  *a++= *(q+3);
+  //Rest is force
+  //q is rectangular so calculate R and phi
+  x= *q;
+  y= *(q+1);
+  R= sqrt(x*x+y*y);
+  phi= acos(x/R);
+  sinphi= y/R;
+  cosphi= x/R;
+  if ( y < 0. ) phi= 2.*M_PI-phi;
+  //Calculate the forces
+  Rforce= calcPlanarRforce(R,phi,t,nargs,leapFuncArgs);
+  phiforce= calcPlanarphiforce(R,phi,t,nargs,leapFuncArgs);
+  *a++= cosphi*Rforce-1./R*sinphi*phiforce;
+  *a++= sinphi*Rforce+1./R*cosphi*phiforce;
+  //dx derivatives are just dv
+  *a++= *(q+6);
+  *a++= *(q+7);
+  //for the dv derivatives we need also R2deriv, phi2deriv, and Rphideriv
+  R2deriv= calcPlanarR2deriv(R,phi,t,nargs,leapFuncArgs);
+  phi2deriv= calcPlanarphi2deriv(R,phi,t,nargs,leapFuncArgs);
+  Rphideriv= calcPlanarRphideriv(R,phi,t,nargs,leapFuncArgs);
+  //..and dFxdx, dFxdy, dFydx, dFydy
+  dFxdx= -cosphi*cosphi*R2deriv
+    +2.*cosphi*sinphi/R/R*phiforce
+    +sinphi*sinphi/R*Rforce
+    +2.*sinphi*cosphi/R*Rphideriv
+    +sinphi*sinphi/R/R*phi2deriv;
+  dFxdy= -sinphi*cosphi*R2deriv
+    +(sinphi*sinphi-cosphi*cosphi)/R/R*phiforce
+    -cosphi*sinphi/R*Rforce
+    -(cosphi*cosphi-sinphi*sinphi)/R*Rphideriv
+    +cosphi*sinphi/R/R*phi2deriv;
+  dFydx= -cosphi*sinphi*R2deriv
+    +(sinphi*sinphi-cosphi*cosphi)/R/R*phiforce
+    +(sinphi*sinphi-cosphi*cosphi)/R*Rphideriv
+    -sinphi*cosphi/R*Rforce
+    +sinphi*cosphi/R/R*phi2deriv;
+  dFydy= -sinphi*sinphi*R2deriv
+    -2.*sinphi*cosphi/R/R*phiforce
+    -2.*sinphi*cosphi/R*Rphideriv
+    +cosphi*cosphi/R*Rforce
+    -cosphi*cosphi/R/R*phi2deriv;
+  *a++= dFxdx * *(q+4) + dFxdy * *(q+5);
+  *a= dFydx * *(q+4) + dFydy * *(q+5);
+}
+
+double calcPlanarR2deriv(double R, double phi, double t, 
+			 int nargs, struct leapFuncArg * leapFuncArgs){
+  int ii;
+  double R2deriv= 0.;
+  for (ii=0; ii < nargs; ii++){
+    R2deriv+= leapFuncArgs->planarR2deriv(R,phi,t,
+					  leapFuncArgs->nargs,
+					  leapFuncArgs->args);
+    leapFuncArgs++;
+  }
+  leapFuncArgs-= nargs;
+  return R2deriv;
+}
+
+double calcPlanarphi2deriv(double R, double phi, double t, 
+			 int nargs, struct leapFuncArg * leapFuncArgs){
+  int ii;
+  double phi2deriv= 0.;
+  for (ii=0; ii < nargs; ii++){
+    phi2deriv+= leapFuncArgs->planarphi2deriv(R,phi,t,
+					  leapFuncArgs->nargs,
+					  leapFuncArgs->args);
+    leapFuncArgs++;
+  }
+  leapFuncArgs-= nargs;
+  return phi2deriv;
+}
+double calcPlanarRphideriv(double R, double phi, double t, 
+			 int nargs, struct leapFuncArg * leapFuncArgs){
+  int ii;
+  double Rphideriv= 0.;
+  for (ii=0; ii < nargs; ii++){
+    Rphideriv+= leapFuncArgs->planarRphideriv(R,phi,t,
+					  leapFuncArgs->nargs,
+					  leapFuncArgs->args);
+    leapFuncArgs++;
+  }
+  leapFuncArgs-= nargs;
+  return Rphideriv;
 }
