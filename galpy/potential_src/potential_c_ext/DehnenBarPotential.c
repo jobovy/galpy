4c914e8097daaf26bb02c828eb8be512b0a64389
Jo Bovy <jo.bovy@gmail.com>
2011-10-19 22:30:00 -0400

    second derivatives in DehnenBarPotential

--- galpy/potential_src/potential_c_ext/DehnenBarPotential.c
+++ galpy/potential_src/potential_c_ext/DehnenBarPotential.c
@@ -53,3 +53,57 @@ double DehnenBarPotentialphiforce(double R,double phi,double t,
   else
     return -2.*amp*af*smooth*sin(2.*(phi-omegab*t-barphi))*pow(rb/R,3.);
 }
+double DehnenBarPotentialR2deriv(double R,double phi,double t,
+				 int nargs, double *args){
+  //declare
+  double smooth;
+  //Get args
+  double amp= *args++;
+  double tform= *args++;
+  double tsteady= *args++;
+  double rb= *args++;
+  double af= *args++;
+  double omegab= *args++;
+  double barphi= *args++;
+  smooth= dehnenSmooth(t,tform,tsteady);
+  if (R <= rb )
+    return 6.*amp*af*smooth*cos(2.*(phi-omegab*t-barphi))*pow(R/rb,3.)/R/R;
+  else
+    return -12.*amp*af*smooth*cos(2.*(phi-omegab*t-barphi))*pow(rb/R,3.)/R/R;
+}
+double DehnenBarPotentialphi2deriv(double R,double phi,double t,
+				   int nargs, double *args){
+  //declare
+  double smooth;
+  //Get args
+  double amp= *args++;
+  double tform= *args++;
+  double tsteady= *args++;
+  double rb= *args++;
+  double af= *args++;
+  double omegab= *args++;
+  double barphi= *args++;
+  smooth= dehnenSmooth(t,tform,tsteady);
+  if (R <= rb )
+    return -4.*amp*af*smooth*cos(2.*(phi-omegab*t-barphi))*(pow(R/rb,3.)-2.);
+  else
+    return 4.*amp*af*smooth*cos(2.*(phi-omegab*t-barphi))*pow(rb/R,3.);
+}
+double DehnenBarPotentialRphideriv(double R,double phi,double t,
+				   int nargs, double *args){
+  //declare
+  double smooth;
+  //Get args
+  double amp= *args++;
+  double tform= *args++;
+  double tsteady= *args++;
+  double rb= *args++;
+  double af= *args++;
+  double omegab= *args++;
+  double barphi= *args++;
+  smooth= dehnenSmooth(t,tform,tsteady);
+  if (R <= rb )
+    return -6.*amp*af*smooth*sin(2.*(phi-omegab*t-barphi))*pow(R/rb,3.)/R;
+  else
+    return -6.*amp*af*smooth*sin(2.*(phi-omegab*t-barphi))*pow(rb/R,3.)/R;
+}
