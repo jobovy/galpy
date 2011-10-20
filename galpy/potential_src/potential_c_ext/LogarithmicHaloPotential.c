e4caf48b1880058b195b5c0d96d01de04b587a53
Jo Bovy <jo.bovy@gmail.com>
2011-10-19 22:29:41 -0400

    second derivatives in LogarithmicHaloPotential

--- galpy/potential_src/potential_c_ext/LogarithmicHaloPotential.c
+++ galpy/potential_src/potential_c_ext/LogarithmicHaloPotential.c
@@ -13,7 +13,7 @@ double LogarithmicHaloPotentialRforce(double R,double Z, double phi,
   return - amp * R/(R*R+zq*zq+c);
 }
 double LogarithmicHaloPotentialPlanarRforce(double R,double phi,
-				      double t,
+					    double t,
 					    int nargs, double *args){
   //Get args
   double amp= *args++;
@@ -31,4 +31,13 @@ double LogarithmicHaloPotentialzforce(double R,double z,double phi,
   //Calculate zforce
   double zq= z/q;
   return -amp * z/q/q/(R*R+zq*zq+c);
+}
+double LogarithmicHaloPotentialPlanarR2deriv(double R,double phi,
+					     double t,
+					     int nargs, double *args){
+  //Get args
+  double amp= *args++;
+  double c= *args;
+  //Calculate Rforce
+  return amp * (1.- 2.*R*R/(R*R+c))/(R*R+c);
 }
