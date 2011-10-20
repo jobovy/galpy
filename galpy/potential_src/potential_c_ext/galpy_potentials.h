f3673697cc5601114d47dd422ec9ebf0b1b0b97e
Jo Bovy <jo.bovy@gmail.com>
2011-10-19 22:30:17 -0400

    second derivatives in LogarithmicHaloPotential and DehnenBarPotential

--- galpy/potential_src/potential_c_ext/galpy_potentials.h
+++ galpy/potential_src/potential_c_ext/galpy_potentials.h
@@ -12,9 +12,14 @@ double LogarithmicHaloPotentialPlanarRforce(double ,double, double,
 					    int , double *);
 double LogarithmicHaloPotentialzforce(double,double,double,double,
 				      int, double *);
+double LogarithmicHaloPotentialPlanarR2deriv(double ,double, double,
+					     int , double *);
 //DehnenBarPotential
 double DehnenBarPotentialRforce(double,double,double,int,double *);
 double DehnenBarPotentialphiforce(double,double,double,int,double *);
+double DehnenBarPotentialR2deriv(double,double,double,int,double *);
+double DehnenBarPotentialphi2deriv(double,double,double,int,double *);
+double DehnenBarPotentialRphideriv(double,double,double,int,double *);
 //TransientLogSpiralPotential
 double TransientLogSpiralPotentialRforce(double,double,double,int,double *);
 double TransientLogSpiralPotentialphiforce(double,double,double,int,double *);
