44d01aa506a784e8f04cd88c122d33c50fcf2ff9
Jo Bovy <jo.bovy@gmail.com>
2011-11-02 16:52:54 -0400

    Add C implementation of PowerSphericalPotential

--- galpy/orbit_src/integratePlanarOrbit.py
+++ galpy/orbit_src/integratePlanarOrbit.py
@@ -78,6 +78,13 @@ def _parse_pot(pot):
             else:
                 pot_args.extend([p._amp,p._tform,p._tsteady,
                                  p._mphio,p._p,p._phib])
+        elif isinstance(p,potential.PowerSphericalPotential):
+            pot_type.append(7)
+            pot_args.extend([p._amp,p.alpha])
+        elif isinstance(p,potential_src.planarPotential.planarPotentialFromRZPotential) \
+                 and isinstance(p._RZPot,potential.PowerSphericalPotential):
+            pot_type.append(7)
+            pot_args.extend([p._RZPot._amp,p._RZPot.alpha])
     pot_type= nu.array(pot_type,dtype=nu.int32,order='C')
     pot_args= nu.array(pot_args,dtype=nu.float64,order='C')
     return (npot,pot_type,pot_args)
