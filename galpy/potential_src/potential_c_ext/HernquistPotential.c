84fead855cf37a50addf905be8a0bfdfe6ea4518
Jo Bovy <jo.bovy@gmail.com>
2011-11-02 22:42:49 -0400

    add C implementation of Hernquist potential

---
+++ galpy/potential_src/potential_c_ext/HernquistPotential.c
@@ -0,0 +1,42 @@
+#include <math.h>
+#include <galpy_potentials.h>
+//HernquistPotential
+//2 arguments: amp, a
+double HernquistPotentialRforce(double R,double Z, double phi,
+				double t,
+				int nargs, double *args){
+  //Get args
+  double amp= *args++;
+  double a= *args;
+  //Calculate Rforce
+  double sqrtRz= pow(R*R+Z*Z,0.5);
+  return - amp * R / a / sqrtRz * pow(1. + sqrtRz / a , -2. );
+}
+double HernquistPotentialPlanarRforce(double R,double phi,
+					    double t,
+					    int nargs, double *args){
+  //Get args
+  double amp= *args++;
+  double a= *args;
+  //Calculate Rforce
+  return - amp / a * pow(1. + R / a , -2. );
+}
+double HernquistPotentialzforce(double R,double Z,double phi,
+				      double t,
+				      int nargs, double *args){
+  //Get args
+  double amp= *args++;
+  double a= *args;
+  //Calculate Rforce
+  double sqrtRz= pow(R*R+Z*Z,0.5);
+  return - amp * Z / a / sqrtRz * pow(1. + sqrtRz / a , -2. );
+}
+double HernquistPotentialPlanarR2deriv(double R,double phi,
+					     double t,
+					     int nargs, double *args){
+  //Get args
+  double amp= *args++;
+  double a= *args;
+  //Calculate R2deriv
+  return -2. * amp / a / a * pow(1. + R / a, -3. );
+}
