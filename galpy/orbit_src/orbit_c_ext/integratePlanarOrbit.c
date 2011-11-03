b35d87de954243e4f9a6cbe3ab5d3fcc33efe6c2
Jo Bovy <jo.bovy@gmail.com>
2011-11-02 22:43:02 -0400

    add C implementation of Hernquist potential

--- galpy/orbit_src/orbit_c_ext/integratePlanarOrbit.c
+++ galpy/orbit_src/orbit_c_ext/integratePlanarOrbit.c
@@ -98,6 +98,14 @@ inline void parse_leapFuncArgs(int npot,struct leapFuncArg * leapFuncArgs,
       leapFuncArgs->planarRphideriv= &ZeroPlanarForce;
       leapFuncArgs->nargs= 2;
       break;
+    case 8: //HernquistPotential, 2 arguments
+      leapFuncArgs->planarRforce= &HernquistPotentialPlanarRforce;
+      leapFuncArgs->planarphiforce= &ZeroPlanarForce;
+      leapFuncArgs->planarR2deriv= &HernquistPotentialPlanarR2deriv;
+      leapFuncArgs->planarphi2deriv= &ZeroPlanarForce;
+      leapFuncArgs->planarRphideriv= &ZeroPlanarForce;
+      leapFuncArgs->nargs= 2;
+      break;
     }
     leapFuncArgs->args= (double *) malloc( leapFuncArgs->nargs * sizeof(double));
     for (jj=0; jj < leapFuncArgs->nargs; jj++){
