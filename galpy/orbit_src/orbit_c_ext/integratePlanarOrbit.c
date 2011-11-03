8d8079cb58641bd9134b53d88edab33f7c562b1b
Jo Bovy <jo.bovy@gmail.com>
2011-11-02 23:09:16 -0400

    added C implementation of JaffePotential

--- galpy/orbit_src/orbit_c_ext/integratePlanarOrbit.c
+++ galpy/orbit_src/orbit_c_ext/integratePlanarOrbit.c
@@ -114,6 +114,14 @@ inline void parse_leapFuncArgs(int npot,struct leapFuncArg * leapFuncArgs,
       leapFuncArgs->planarRphideriv= &ZeroPlanarForce;
       leapFuncArgs->nargs= 2;
       break;
+    case 10: //JaffePotential, 2 arguments
+      leapFuncArgs->planarRforce= &JaffePotentialPlanarRforce;
+      leapFuncArgs->planarphiforce= &ZeroPlanarForce;
+      leapFuncArgs->planarR2deriv= &JaffePotentialPlanarR2deriv;
+      leapFuncArgs->planarphi2deriv= &ZeroPlanarForce;
+      leapFuncArgs->planarRphideriv= &ZeroPlanarForce;
+      leapFuncArgs->nargs= 2;
+      break;
     }
     leapFuncArgs->args= (double *) malloc( leapFuncArgs->nargs * sizeof(double));
     for (jj=0; jj < leapFuncArgs->nargs; jj++){
