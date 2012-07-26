ee4719de59363a9c8a360dffd8d62f0c37b7f967
Jo Bovy <bovy@ias.edu>
2012-07-26 12:30:00 +0200

    add wrapper class for adiabatic approximation

--- galpy/actionAngle.py
+++ galpy/actionAngle.py
@@ -2,6 +2,7 @@ from galpy.actionAngle_src import actionAngle
 from galpy.actionAngle_src import actionAngleFlat
 from galpy.actionAngle_src import actionAnglePower
 from galpy.actionAngle_src import actionAngleAxi
+from galpy.actionAngle_src import actionAngleAdiabatic
 
 #
 # Classes
@@ -10,3 +11,4 @@ actionAngle= actionAngle.actionAngle
 actionAngleFlat= actionAngleFlat.actionAngleFlat
 actionAnglePower= actionAnglePower.actionAnglePower
 actionAngleAxi= actionAngleAxi.actionAngleAxi
+actionAngleAdiabatic= actionAngleAdiabatic.actionAngleAdiabatic
