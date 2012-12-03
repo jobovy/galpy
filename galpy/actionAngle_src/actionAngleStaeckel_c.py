2cd238ca8d2e83bf86145c4c0a6db9338074ed90
Jo Bovy <bovy@ias.edu>
2012-12-02 21:32:21 -0500

    start on c extension

---
+++ galpy/actionAngle_src/actionAngleStaeckel_c.py
@@ -0,0 +1,98 @@
+import os
+import ctypes
+import ctypes.util
+import numpy
+from numpy.ctypeslib import ndpointer
+from galpy import potential, potential_src
+from galpy.orbit_src.integrateFullOrbit import _parse_pot
+#Find and load the library
+_lib = None
+_libname = ctypes.util.find_library('galpy_actionAngle_c')
+if _libname:
+    _lib = ctypes.CDLL(_libname)
+if _lib is None:
+    import sys
+for path in sys.path:
+    try:
+        _lib = ctypes.CDLL(os.path.join(path,'galpy_actionAngle_c.so'))
+    except OSError:
+        _lib = None
+    else:
+        break
+if _lib is None:
+    raise IOError('galpy actionAngle_c module not found')
+
+def actionAngleStaeckel_c(pot,delta,R,vR,vT,z,vz):
+    """
+    NAME:
+       actionAngleStaeckel_c
+    PURPOSE:
+       Use C to calculate actions using the Staeckel approximation
+    INPUT:
+       pot - Potential or list of such instances
+       delta - focal length of prolate spheroidal coordinates
+       R, vR, vT, z, vz - coordinates (arrays)
+    OUTPUT:
+       (jr,jz,err)
+       jr,jz : array, shape (len(R))
+       err - non-zero if error occured
+    HISTORY:
+       2012-12-01 - Written - Bovy (IAS)
+    """
+    #Parse the potential
+    npot, pot_type, pot_args= _parse_pot(pot)
+
+    #Set up result arrays
+    jr= numpy.empty(len(R))
+    jz= numpy.empty(len(R))
+    err= ctypes.c_int(0)
+
+    #Set up the C code
+    ndarrayFlags= ('C_CONTIGUOUS','WRITEABLE')
+    integrationFunc= _lib.actionAngleStaeckel_actions
+    integrationFunc.argtypes= [ndpointer(dtype=numpy.float64,flags=ndarrayFlags),
+                               ctypes.c_int,                             
+                               ndpointer(dtype=numpy.float64,flags=ndarrayFlags),
+                               ctypes.c_int,
+                               ndpointer(dtype=numpy.int32,flags=ndarrayFlags),
+                               ndpointer(dtype=numpy.float64,flags=ndarrayFlags),
+                               ctypes.c_double,
+                               ctypes.c_double,
+                               ndpointer(dtype=numpy.float64,flags=ndarrayFlags),
+                               ctypes.POINTER(ctypes.c_int),
+                               ctypes.c_int]
+
+    #Array requirements, first store old order
+    f_cont= [R.flags['F_CONTIGUOUS'],
+             vR.flags['F_CONTIGUOUS'],
+             vT.flags['F_CONTIGUOUS'],
+             z.flags['F_CONTIGUOUS'],
+             vz.flags['F_CONTIGUOUS']]
+    R= numpy.require(R,dtype=numpy.float64,requirements=['C','W'])
+    vR= numpy.require(vR,dtype=numpy.float64,requirements=['C','W'])
+    vT= numpy.require(vT,dtype=numpy.float64,requirements=['C','W'])
+    z= numpy.require(z,dtype=numpy.float64,requirements=['C','W'])
+    vz= numpy.require(vz,dtype=numpy.float64,requirements=['C','W'])
+    jr= numpy.require(jr,dtype=numpy.float64,requirements=['C','W'])
+    jz= numpy.require(jz,dtype=numpy.float64,requirements=['C','W'])
+
+    #Run the C code
+    integrationFunc(yo,
+                    ctypes.c_int(len(t)),
+                    t,
+                    ctypes.c_int(npot),
+                    pot_type,
+                    pot_args,
+                    ctypes.c_double(rtol),ctypes.c_double(atol),
+                    result,
+                    ctypes.byref(err),
+                    ctypes.c_int(int_method_c))
+
+    #Reset input arrays
+    if f_cont[0]: R= numpy.asfortranarray(R)
+    if f_cont[1]: vR= numpy.asfortranarray(vR)
+    if f_cont[2]: vT= numpy.asfortranarray(vT)
+    if f_cont[3]: z= numpy.asfortranarray(z)
+    if f_cont[4]: vz= numpy.asfortranarray(vz)
+
+    return (jr,jz,err.value)
