e516bc2c96fe1506c37d1d1a258facb0d195856d
Jo Bovy <bovy@ias.edu>
2012-12-04 16:42:22 -0500

    add C potential implementations of remaining potentials

--- galpy/potential_src/potential_c_ext/PowerSphericalPotential.c
+++ galpy/potential_src/potential_c_ext/PowerSphericalPotential.c
@@ -2,6 +2,18 @@
 #include <galpy_potentials.h>
 //PowerSphericalPotential
 //2  arguments: amp, alpha
+double PowerSphericalPotentialEval(double R,double Z, double phi,
+				   double t,
+				   int nargs, double *args){
+  //Get args
+  double amp= *args++;
+  double alpha= *args;
+  //Calculate Rforce
+  if ( alpha == 2. )
+    return 0.5 * amp * log ( R*R+Z*Z);
+  else
+    return - amp * pow(R*R+Z*Z,1.-0.5*alpha) / (alpha - 2.);
+}
 double PowerSphericalPotentialRforce(double R,double Z, double phi,
 				      double t,
 				      int nargs, double *args){
