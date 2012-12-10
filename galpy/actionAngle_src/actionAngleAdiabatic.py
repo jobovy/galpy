9ea21ad1eca473970fbb5afa3d80ed1e84034618
Jo Bovy <bovy@ias.edu>
2012-12-10 15:50:31 -0500

    use C to calculate Adiabatic apprimxation

--- galpy/actionAngle_src/actionAngleAdiabatic.py
+++ galpy/actionAngle_src/actionAngleAdiabatic.py
@@ -10,6 +10,7 @@
 #             __call__: returns (jr,lz,jz)
 #
 ###############################################################################
+import warnings
 import math as m
 import numpy as nu
 from actionAngleAxi import actionAngleAxi
@@ -67,20 +68,65 @@ class actionAngleAdiabatic():
         HISTORY:
            2012-07-26 - Written - Bovy (IAS@MPIA)
         """
-        #Set up the actionAngleAxi object
-        meta= actionAngle(*args)
-        if isinstance(self._pot,list):
-            thispot= [p.toPlanar() for p in self._pot]
+        if self._c or (ext_loaded and kwargs.has_key('c') and kwargs['c']):
+            #print "BOVY: CHECK THAT POTENTIALS HAVE C IMPLEMENTATIONS"
+            if len(args) == 5: #R,vR.vT, z, vz
+                R,vR,vT, z, vz= args
+            elif len(args) == 6: #R,vR.vT, z, vz, phi
+                R,vR,vT, z, vz, phi= args
+            else:
+                meta= actionAngle(*args)
+                R= meta._R
+                vR= meta._vR
+                vT= meta._vT
+                z= meta._z
+                vz= meta._vz
+            if isinstance(R,float):
+                R= nu.array([R])
+                vR= nu.array([vR])
+                vT= nu.array([vT])
+                z= nu.array([z])
+                vz= nu.array([vz])
+            Lz= R*vT
+            jr, jz, err= actionAngleAdiabatic_c.actionAngleAdiabatic_c(\
+                self._pot,self._gamma,R,vR,vT,z,vz)
+            if err == 0:
+                return (jr,Lz,jz)
+            else:
+                raise RuntimeError("C-code for calculation actions failed; try with c=False")
         else:
-            thispot= self._pot.toPlanar()
-        if isinstance(self._pot,list):
-            thisverticalpot= [p.toVertical(meta._R) for p in self._pot]
-        else:
-            thisverticalpot= self._pot.toVertical(meta._R)
-        aAAxi= actionAngleAxi(*args,pot=thispot,
-                               verticalPot=thisverticalpot,
-                               gamma=self._gamma)
-        return (aAAxi.JR(**kwargs),aAAxi._R*aAAxi._vT,aAAxi.Jz(**kwargs))
+            if (len(args) == 5 or len(args) == 6) \
+                    and isinstance(args[0],nu.ndarray):
+                ojr= nu.zeros((len(args[0])))
+                olz= nu.zeros((len(args[0])))
+                ojz= nu.zeros((len(args[0])))
+                for ii in range(len(args[0])):
+                    if len(args) == 5:
+                        targs= (args[0][ii],args[1][ii],args[2][ii],
+                                args[3][ii],args[4][ii])
+                    elif len(args) == 6:
+                        targs= (args[0][ii],args[1][ii],args[2][ii],
+                                args[3][ii],args[4][ii],args[5][ii])
+                    tjr,tlz,tjz= self(*targs,**copy.copy(kwargs))
+                    ojr[ii]= tjr[0]
+                    ojz[ii]= tjz[0]
+                    olz[ii]= tlz
+                return (ojr,olz,ojz)
+            else:
+                #Set up the actionAngleAxi object
+                meta= actionAngle(*args)
+                if isinstance(self._pot,list):
+                    thispot= [p.toPlanar() for p in self._pot]
+                else:
+                    thispot= self._pot.toPlanar()
+                if isinstance(self._pot,list):
+                    thisverticalpot= [p.toVertical(meta._R) for p in self._pot]
+                else:
+                    thisverticalpot= self._pot.toVertical(meta._R)
+                aAAxi= actionAngleAxi(*args,pot=thispot,
+                                       verticalPot=thisverticalpot,
+                                       gamma=self._gamma)
+                return (aAAxi.JR(**kwargs),aAAxi._R*aAAxi._vT,aAAxi.Jz(**kwargs))
 
     def JR(self,*args,**kwargs):
         """
