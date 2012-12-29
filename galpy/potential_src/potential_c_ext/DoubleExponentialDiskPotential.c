fdd17401aa0f871a516aaf09b4e219ad3fe957cb
Jo Bovy <bovy@ias.edu>
2012-12-29 10:30:26 -0500

    add zforce

--- galpy/potential_src/potential_c_ext/DoubleExponentialDiskPotential.c
+++ galpy/potential_src/potential_c_ext/DoubleExponentialDiskPotential.c
@@ -37,3 +37,42 @@ double DoubleExponentialDiskPotentialEval(double R,double z, double phi,
   }
     return - amp * 2 * M_PI * alpha * out;
 }
+double MiyamotoNagaiPotentialRforce(double R,double z, double phi,
+				    double t,
+				    int nargs, double *args){
+}
+double MiyamotoNagaiPotentialzforce(double R,double z,double phi,
+				    double t,
+				    int nargs, double *args){
+  //Get args
+  double amp= *args++;
+  double alpha= *args++;
+  double beta= *args++;
+  double kmaxFac= *args++;
+  double kmax= kmaxFac * beta;
+  int nzeros= (int) *args++;
+  int glorder= (int) *args++;
+  double * glx= args;
+  double * glw= args + glorder;
+  double * j0zeros= args + 2 * glorder;
+  double * dj0zeros= args + 2 * glorder + nzeros + 1;
+  //Calculate potential
+  double out= 0.;
+  double k;
+  int ii, jj;
+  if ( R < 1. ) kmax= kmax/R;
+  for (ii=0; ii < ( nzeros + 1 ); ii++) {
+    for (jj=0; jj < glorder; jj++) {
+      k= 0.5 * ( *(glx+jj) + 1. ) * *(dj0zeros+ii+1) + *(j0zeros+ii);
+      out+= *(glw+jj) * *(dj0zeros+ii+1) * k * gsl_sf_bessel_J0(k*R) 
+	* pow(alpha * alpha + k * k,-1.5) 
+	* (exp(-k * fabs(z) ) - exp(-beta * fabs(z) ))
+	/ (beta * beta - k * k);
+    }
+    if ( k > kmax ) break;
+  }
+  if ( z > 0. )
+    return - amp * 2 * M_PI * alpha * beta * out;
+  else
+    return amp * 2 * M_PI * alpha * beta * out;
+}
