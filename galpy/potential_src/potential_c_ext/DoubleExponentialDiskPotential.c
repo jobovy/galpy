519ffdad1f68bb9b02102acab8449e7a8c32cc4f
Jo Bovy <bovy@ias.edu>
2012-12-28 22:34:37 -0500

    add C potential implementation of DoubleExponentialDisk

---
+++ galpy/potential_src/potential_c_ext/DoubleExponentialDiskPotential.c
@@ -0,0 +1,39 @@
+#include <math.h>
+#include <gsl/gsl_sf_bessel.h>
+#include <galpy_potentials.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+//Double exponential disk potential
+double DoubleExponentialDiskPotentialEval(double R,double z, double phi,
+				  double t,
+				  int nargs, double *args){
+  //Get args
+  double amp= *args++;
+  double alpha= *args++;
+  double beta= *args++;
+  double kmaxFac= *args++;
+  double kmax= kmaxFac * beta;
+  int nzeros= (int) *args++;
+  int glorder= (int) *args++;
+  double * glx= args;
+  double * glw= args + glorder;
+  double * j0zeros= args + 2 * glorder;
+  double * dj0zeros= args + 2 * glorder + nzeros + 1;
+  //Calculate potential
+  double out= 0.;
+  double k;
+  int ii, jj;
+  if ( R < 1. ) kmax= kmax/R;
+  for (ii=0; ii < ( nzeros + 1 ); ii++) {
+    for (jj=0; jj < glorder; jj++) {
+      k= 0.5 * ( *(glx+jj) + 1. ) * *(dj0zeros+ii+1) + *(j0zeros+ii);
+      out+= *(glw+jj) * *(dj0zeros+ii+1) * gsl_sf_bessel_J0(k*R) 
+	* pow(alpha * alpha + k * k,-1.5) 
+	* (beta * exp(-k * fabs(z) ) - k * exp(-beta * fabs(z) ))
+	/ (beta * beta - k * k);
+    }
+    if ( k > kmax ) break;
+  }
+    return - amp * 2 * M_PI * alpha * out;
+}
