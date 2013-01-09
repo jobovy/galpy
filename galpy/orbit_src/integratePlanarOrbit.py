7622afbefe1975d5afcbeccdf5ed905fe0965861
Jo Bovy <bovy@ias.edu>
2013-01-09 01:09:04 -0500

    fix bug in sending PowerSpherical potential to C

--- galpy/orbit_src/integratePlanarOrbit.py
+++ galpy/orbit_src/integratePlanarOrbit.py
@@ -80,7 +80,7 @@ def _parse_pot(pot):
                                  p._mphio,p._p,p._phib])
         elif isinstance(p,potential.PowerSphericalPotential):
             pot_type.append(7)
-            pot_args.extend([p._amp,p.aalpha])
+            pot_args.extend([p._amp,p.alpha])
         elif isinstance(p,potential_src.planarPotential.planarPotentialFromRZPotential) \
                  and isinstance(p._RZPot,potential.PowerSphericalPotential):
             pot_type.append(7)
