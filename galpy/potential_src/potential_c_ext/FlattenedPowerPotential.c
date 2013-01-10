e810e92e3b469dea75566b64d714e3249aebb7ce
Jo Bovy <bovy@ias.edu>
2013-01-10 10:45:34 -0700

    add forces in C

--- galpy/potential_src/potential_c_ext/FlattenedPowerPotential.c
+++ galpy/potential_src/potential_c_ext/FlattenedPowerPotential.c
@@ -19,3 +19,69 @@ double FlattenedPowerPotentialEval(double R,double Z, double phi,
     return - amp * pow(m2,-0.5 * alpha) / alpha;
   }
 }
+double FlattenedPowerPotentialRforce(double R,double Z, double phi,
+				     double t,
+				     int nargs, double *args){
+  //Get args
+  double amp= *args;
+  double alpha= *(args+1);
+  double q2= *(args+2);
+  double core2= *(args+3);
+  double m2;
+  //Calculate potential
+  if ( alpha == 0. ) 
+    return - amp * R/(R*R+Z*Z/q2+core2);
+  else {
+    m2= core2+R*R+Z*Z/q2;
+    return - amp * pow(m2,-0.5 * alpha-1.) * R;
+  }
+}
+double FlattenedPowerPotentialPlanarRforce(double R,double Z, double phi,
+					   double t,
+					   int nargs, double *args){
+  //Get args
+  double amp= *args;
+  double alpha= *(args+1);
+  double core2= *(args+2);
+  double m2;
+  //Calculate potential
+  if ( alpha == 0. ) 
+    return - amp * R/(R*R+core2);
+  else {
+    m2= core2+R*R;
+    return - amp * pow(m2,-0.5 * alpha - 1.) * R;
+  }
+}
+double FlattenedPowerPotentialzforce(double R,double Z, double phi,
+				     double t,
+				     int nargs, double *args){
+  //Get args
+  double amp= *args;
+  double alpha= *(args+1);
+  double q2= *(args+2);
+  double core2= *(args+3);
+  double m2;
+  //Calculate potential
+  if ( alpha == 0. ) 
+    return -amp * Z/q2/(R*R+Z*Z/q2+core2);
+  else {
+    m2= core2+R*R+Z*Z/q2;
+    return - amp * pow(m2,-0.5 * alpha - 1.) * Z / q2;
+  }
+}
+double FlattenedPowerPotentialPlanarR2deriv(double R,double Z, double phi,
+					    double t,
+					    int nargs, double *args){
+  //Get args
+  double amp= *args;
+  double alpha= *(args+1);
+  double core2= *(args+2);
+  double m2;
+  //Calculate potential
+  if ( alpha == 0. ) 
+    return amp * (1.- 2.*R*R/(R*R+core2))/(R*R+core2);
+  else {
+    m2= core2+R*R;
+    return - amp * pow(m2,-0.5 * alpha - 1.) * ( (alpha + 1.) * R*R/m2 -1.);
+  }
+}
