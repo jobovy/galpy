12b816486269459940ff93f676753c01175f84eb
Jo Bovy <bovy@ias.edu>
2013-01-24 15:31:16 -0500

    calculate a potential in C

---
+++ galpy/potential_src/interppotential_c_ext/interppotential_calc_potential.c
@@ -0,0 +1,36 @@
+/*
+  C code for calculating a potential and its forces on a grid
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+//Potentials
+#include <galpy_potentials.h>
+#include <actionAngle.h>
+#include <cubic_bspline_2d_coeffs.h>
+/*
+  MAIN FUNCTIONS
+*/
+void calc_potential(int nR,
+		    double *R,
+		    int nz,
+		    double *z,
+		    int npot,
+		    int * pot_type,
+		    double * pot_args,
+		    double *out,
+		    int * err){
+  int ii, jj;
+  double * row= (double *) malloc ( nR * ( sizeof ( double ) ) );
+  //Set up the potentials
+  struct actionAngleArg * actionAngleArgs= (struct actionAngleArg *) malloc ( npot * sizeof (struct actionAngleArg) );
+  parse_actionAngleArgs(npot,actionAngleArgs,pot_type,pot_args);
+  //Run through the grid and calculate
+  for (jj=0; jj < nz; jj++){
+    for (ii=0; ii < nR; ii++){
+      *(row+ii)= evaluatePotentials(*(R+ii),*(z+jj),npot,actionAngleArgs);
+    }
+    put_row(out,jj,row,nR); 
+  }
+}
