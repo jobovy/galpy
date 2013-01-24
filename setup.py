161f45c0dbf10a7363adbab6409b904f5ceadd75
Jo Bovy <bovy@ias.edu>
2013-01-24 15:30:27 -0500

    add interppotential extension

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ ext_modules=[orbit_int_c]
 #actionAngle C extension
 actionAngle_c_src= glob.glob('galpy/actionAngle_src/actionAngle_c_ext/*.c')
 actionAngle_c_src.extend(glob.glob('galpy/potential_src/potential_c_ext/*.c'))
+#actionAngle_c_src.extend(glob.glob('galpy/util/interp_2d/*.c'))
 
 #Installation of this extension using the GSL may (silently) fail, if the GSL
 #is built for the wrong architecture, on Mac you can install the GSL correctly
@@ -45,10 +46,27 @@ actionAngle_c= Extension('galpy_actionAngle_c',
                          sources=actionAngle_c_src,
                          libraries=['m','gsl','gslcblas','gomp'],
                          include_dirs=['galpy/actionAngle_src/actionAngle_c_ext',
+#                                       'galpy/util/interp_2d',
                                        'galpy/potential_src/potential_c_ext'],
                          extra_compile_args=["-fopenmp"])
 if float(gsl_version[0]) >= 1. and float(gsl_version[1]) > 14.:
     ext_modules.append(actionAngle_c)
+    
+#interppotential C extension
+interppotential_c_src= glob.glob('galpy/potential_src/potential_c_ext/*.c')
+interppotential_c_src.extend(glob.glob('galpy/potential_src/interppotential_c_ext/*.c'))
+interppotential_c_src.append('galpy/actionAngle_src/actionAngle_c_ext/actionAngle.c')
+interppotential_c_src.append('galpy/util/interp_2d/cubic_bspline_2d_coeffs.c')
+
+interppotential_c= Extension('galpy_interppotential_c',
+                         sources=interppotential_c_src,
+                         libraries=['m','gsl','gslcblas'],
+                         include_dirs=['galpy/potential_src/potential_c_ext',
+                                       'galpy/util/interp_2d',
+                                       'galpy/actionAngle_src/actionAngle_c_ext',
+                                       'galpy/potential_src/interppotential_c_ext'])
+if float(gsl_version[0]) >= 1. and float(gsl_version[1]) > 14.:
+    ext_modules.append(interppotential_c)
 
 setup(name='galpy',
       version='1.',
