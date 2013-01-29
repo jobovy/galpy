0044ec5b6aa05c091b1256423e761625710709b0
Jo Bovy <bovy@ias.edu>
2013-01-29 13:43:12 -0500

    add header file with definition of parser in preparation for interpolated forces

---
+++ galpy/orbit_src/orbit_c_ext/integrateFullOrbit.h
@@ -0,0 +1,5 @@
+#ifndef __INTEGRATEFULLORBIT_H__
+#define __INTEGRATEFULLORBIT_H__
+#include <galpy_potentials.h>
+void parse_leapFuncArgs_Full(int, struct potentialArg *,int *,double *);
+#endif /* integrateFullOrbit.h */
