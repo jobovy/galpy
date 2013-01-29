9418cf61849ff047603f1dcd37f8eb51940ec567
Jo Bovy <bovy@ias.edu>
2013-01-29 14:17:29 -0500

    add force evaluations to header file

--- galpy/orbit_src/orbit_c_ext/integrateFullOrbit.h
+++ galpy/orbit_src/orbit_c_ext/integrateFullOrbit.h
@@ -2,4 +2,6 @@
 #define __INTEGRATEFULLORBIT_H__
 #include <galpy_potentials.h>
 void parse_leapFuncArgs_Full(int, struct potentialArg *,int *,double *);
+double calcRforce(double,double,double,double,int,struct potentialArg *);
+double calczforce(double,double,double,double,int,struct potentialArg *);
 #endif /* integrateFullOrbit.h */
