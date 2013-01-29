cf61c63ad95db5fa85bf69c343b0c6698c5e04bc
Jo Bovy <bovy@ias.edu>
2013-01-29 14:18:16 -0500

    calculate R and z forces on a grid for interpolation in C

--- galpy/potential_src/interppotential_c_ext/interppotential_calc_potential.c
+++ galpy/potential_src/interppotential_c_ext/interppotential_calc_potential.c
@@ -62,15 +62,54 @@ void calc_rforce(int nR,
   //Run through the grid and calculate
   for (ii=0; ii < nR; ii++){
     for (jj=0; jj < nz; jj++){
-      *(row+jj)= evaluatePotentials(*(R+ii),*(z+jj),npot,potentialArgs);
+      *(row+jj)= calcRforce(*(R+ii),*(z+jj),0.,0.,npot,potentialArgs);
     }
     put_row(out,ii ,row,nz); 
   }
   for (ii=0; ii < npot; ii++) {
-    if ( (potentialArgs+ii)->i2d )
-      interp_2d_free((potentialArgs+ii)->i2d) ;
-    if ((potentialArgs+ii)->acc )
-      gsl_interp_accel_free ((potentialArgs+ii)->acc);
+    if ( (potentialArgs+ii)->i2drforce )
+      interp_2d_free((potentialArgs+ii)->i2drforce ) ;
+    if ((potentialArgs+ii)->accrforce )
+      gsl_interp_accel_free ((potentialArgs+ii)->accrforce );
+    if ( (potentialArgs+ii)->i2dzforce )
+      interp_2d_free((potentialArgs+ii)->i2dzforce ) ;
+    if ((potentialArgs+ii)->acczforce )
+      gsl_interp_accel_free ((potentialArgs+ii)->acczforce );
+    free((potentialArgs+ii)->args);
+  }
+  free(potentialArgs);
+  free(row);
+}
+void calc_zforce(int nR,
+		 double *R,
+		 int nz,
+		 double *z,
+		 int npot,
+		 int * pot_type,
+		 double * pot_args,
+		 double *out,
+		 int * err){
+  int ii, jj;
+  double * row= (double *) malloc ( nz * ( sizeof ( double ) ) );
+  //Set up the potentials
+  struct potentialArg * potentialArgs= (struct potentialArg *) malloc ( npot * sizeof (struct potentialArg) );
+  parse_leapFuncArgs_Full(npot,potentialArgs,pot_type,pot_args);
+  //Run through the grid and calculate
+  for (ii=0; ii < nR; ii++){
+    for (jj=0; jj < nz; jj++){
+      *(row+jj)= calczforce(*(R+ii),*(z+jj),0.,0.,npot,potentialArgs);
+    }
+    put_row(out,ii ,row,nz); 
+  }
+  for (ii=0; ii < npot; ii++) {
+    if ( (potentialArgs+ii)->i2drforce )
+      interp_2d_free((potentialArgs+ii)->i2drforce ) ;
+    if ((potentialArgs+ii)->accrforce )
+      gsl_interp_accel_free ((potentialArgs+ii)->accrforce );
+    if ( (potentialArgs+ii)->i2dzforce )
+      interp_2d_free((potentialArgs+ii)->i2dzforce ) ;
+    if ((potentialArgs+ii)->acczforce )
+      gsl_interp_accel_free ((potentialArgs+ii)->acczforce );
     free((potentialArgs+ii)->args);
   }
   free(potentialArgs);
