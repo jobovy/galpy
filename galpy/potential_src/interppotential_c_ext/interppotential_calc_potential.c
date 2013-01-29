54fffed68a07f271cb5cf758f447ae04cf5d4caa
Jo Bovy <bovy@ias.edu>
2013-01-29 13:42:34 -0500

    switch to potentialArgs and better freeing of allocated memory

--- galpy/potential_src/interppotential_c_ext/interppotential_calc_potential.c
+++ galpy/potential_src/interppotential_c_ext/interppotential_calc_potential.c
@@ -8,6 +8,7 @@
 //Potentials
 #include <galpy_potentials.h>
 #include <actionAngle.h>
+#include <integrateFullOrbit.h>
 #include <interp_2d.h>
 #include <cubic_bspline_2d_coeffs.h>
 /*
@@ -25,20 +26,54 @@ void calc_potential(int nR,
   int ii, jj;
   double * row= (double *) malloc ( nz * ( sizeof ( double ) ) );
   //Set up the potentials
-  struct potentialArg * actionAngleArgs= (struct potentialArg *) malloc ( npot * sizeof (struct potentialArg) );
-  parse_actionAngleArgs(npot,actionAngleArgs,pot_type,pot_args);
+  struct potentialArg * potentialArgs= (struct potentialArg *) malloc ( npot * sizeof (struct potentialArg) );
+  parse_actionAngleArgs(npot,potentialArgs,pot_type,pot_args);
   //Run through the grid and calculate
   for (ii=0; ii < nR; ii++){
     for (jj=0; jj < nz; jj++){
-      *(row+jj)= evaluatePotentials(*(R+ii),*(z+jj),npot,actionAngleArgs);
+      *(row+jj)= evaluatePotentials(*(R+ii),*(z+jj),npot,potentialArgs);
     }
     put_row(out,ii ,row,nz); 
   }
-  if ( actionAngleArgs->i2d )
-    interp_2d_free(actionAngleArgs->i2d) ;
-  if (actionAngleArgs->acc )
-    gsl_interp_accel_free (actionAngleArgs->acc);
-  free(actionAngleArgs);
+  for (ii=0; ii < npot; ii++) {
+    if ( (potentialArgs+ii)->i2d )
+      interp_2d_free((potentialArgs+ii)->i2d) ;
+    if ((potentialArgs+ii)->acc )
+      gsl_interp_accel_free ((potentialArgs+ii)->acc);
+    free((potentialArgs+ii)->args);
+  }
+  free(potentialArgs);
+  free(row);
+}
+void calc_rforce(int nR,
+		 double *R,
+		 int nz,
+		 double *z,
+		 int npot,
+		 int * pot_type,
+		 double * pot_args,
+		 double *out,
+		 int * err){
+  int ii, jj;
+  double * row= (double *) malloc ( nz * ( sizeof ( double ) ) );
+  //Set up the potentials
+  struct potentialArg * potentialArgs= (struct potentialArg *) malloc ( npot * sizeof (struct potentialArg) );
+  parse_leapFuncArgs_Full(npot,potentialArgs,pot_type,pot_args);
+  //Run through the grid and calculate
+  for (ii=0; ii < nR; ii++){
+    for (jj=0; jj < nz; jj++){
+      *(row+jj)= evaluatePotentials(*(R+ii),*(z+jj),npot,potentialArgs);
+    }
+    put_row(out,ii ,row,nz); 
+  }
+  for (ii=0; ii < npot; ii++) {
+    if ( (potentialArgs+ii)->i2d )
+      interp_2d_free((potentialArgs+ii)->i2d) ;
+    if ((potentialArgs+ii)->acc )
+      gsl_interp_accel_free ((potentialArgs+ii)->acc);
+    free((potentialArgs+ii)->args);
+  }
+  free(potentialArgs);
   free(row);
 }
 void eval_potential(int nR,
@@ -51,15 +86,18 @@ void eval_potential(int nR,
 		    int * err){
   int ii;
   //Set up the potentials
-  struct potentialArg * actionAngleArgs= (struct potentialArg *) malloc ( npot * sizeof (struct potentialArg) );
-  parse_actionAngleArgs(npot,actionAngleArgs,pot_type,pot_args);
+  struct potentialArg * potentialArgs= (struct potentialArg *) malloc ( npot * sizeof (struct potentialArg) );
+  parse_actionAngleArgs(npot,potentialArgs,pot_type,pot_args);
   //Run through and evaluate
   for (ii=0; ii < nR; ii++){
-    *(out+ii)= evaluatePotentials(*(R+ii),*(z+ii),npot,actionAngleArgs);
+    *(out+ii)= evaluatePotentials(*(R+ii),*(z+ii),npot,potentialArgs);
   }
-  if ( actionAngleArgs->i2d )
-    interp_2d_free(actionAngleArgs->i2d) ;
-  if (actionAngleArgs->acc )
-    gsl_interp_accel_free (actionAngleArgs->acc);
-  free(actionAngleArgs);
+  for (ii=0; ii < npot; ii++) {
+    if ( (potentialArgs+ii)->i2d )
+      interp_2d_free((potentialArgs+ii)->i2d) ;
+    if ((potentialArgs+ii)->acc )
+      gsl_interp_accel_free ((potentialArgs+ii)->acc);
+    free((potentialArgs+ii)->args);
+  }
+  free(potentialArgs);
 }
