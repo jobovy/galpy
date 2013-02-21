97ddb7e5dc3ca4dacabf63c360859f09d9bb3228
Jo Bovy <bovy@ias.edu>
2013-02-21 12:24:33 -0500

    fix bug

--- galpy/df_src/quasiisothermaldf.py
+++ galpy/df_src/quasiisothermaldf.py
@@ -423,7 +423,7 @@ class quasiisothermaldf:
             elif not _jr is None and not _rg is None:
                 logqeval, jr, lz, jz, rg, kappa, nu, Omega= self((_jr,_lz,_jz),
                                            rg=_rg,kappa=_kappa,nu=_nu,
-                                           Omega=_Omega
+                                           Omega=_Omega,
                                            log=True,
                                            _return_actions=True,
                                                                  _return_freqs=True)
