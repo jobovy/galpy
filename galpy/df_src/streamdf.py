60870f4393be6046d2bca4c0bdc847fc5bca38d2
Jo Bovy <bovy@ias.edu>
2013-09-17 18:10:20 -0400

    first full implementation of aA stream DF

--- galpy/df_src/streamdf.py
+++ galpy/df_src/streamdf.py
@@ -36,7 +36,7 @@ class streamdf:
         self._progenitor= progenitor
         self._ts= ts
         if not self._ts is None: self._nts= len(self._ts)
-        if True:
+        if self._aA is None: #direct integration, so we need the progenitor orbit in rectangular coordinates
             if not isinstance(self._progenitor,Orbit):
                 raise IOError('progenitor= kwargs needs to be an Orbit instance')
             if not hasattr(self._progenitor,'orbit'): #not integrated yet
@@ -56,7 +56,6 @@ class streamdf:
                                                self._progenitor_z**2.)
             self._progenitor_rap= numpy.amax(self._progenitor_R**2.+
                                              self._progenitor_z**2.)
-        if self._aA is None: #direct integration, so we need the progenitor orbit in rectangular coordinates
             tX, tY, tZ= bovy_coords.cyl_to_rect(self._progenitor_R,
                                                 self._progenitor_phi,
                                                 self._progenitor_z)
@@ -79,7 +78,21 @@ class streamdf:
                 self._sigx= sigx
             self._sigx2= self._sigx**2.
             self._sigv2= self._sigv**2.
+            self._lnsigx= numpy.log(self._sigx)
+            self._lnsigv= numpy.log(self._sigv)
         else: #calculate estimates sigmas for the actions
+            #Calculate actions, frequencies, and angles for the progenitor
+            acfs= aA.actionsFreqsAngles(self._progenitor,maxn=3)
+            self._progenitor_jr= acfs[0][0]
+            self._progenitor_lz= acfs[1][0]
+            self._progenitor_jz= acfs[2][0]
+            self._progenitor_Omegar= acfs[3]
+            self._progenitor_Omegaphi= acfs[4]
+            self._progenitor_Omegaz= acfs[5]
+            self._progenitor_angler= acfs[6]
+            self._progenitor_anglephi= acfs[7]
+            self._progenitor_anglez= acfs[8]
+            #From the progenitor orbit, determine the sigmas in J and angle
             self._sigjr= (self._progenitor.rap()-self._progenitor.rperi())/numpy.pi*self._sigv
             self._siglz= self._progenitor.rperi()*self._sigv
             self._sigjz= 2.*self._progenitor.zmax()/numpy.pi*self._sigv
@@ -92,17 +105,10 @@ class streamdf:
             self._siglz2= self._siglz**2.
             self._sigjz2= self._sigjz**2.
             self._sigangle2= self._sigangle**2.
-            #Also calculate actions, frequencies, and angles for the progenitor
-            acfs= aA.actionsFreqsAngles(self._progenitor,maxn=3)
-            self._progenitor_jr= acfs[0][0]
-            self._progenitor_lz= acfs[1][0]
-            self._progenitor_jz= acfs[2][0]
-            self._progenitor_Omegar= acfs[3]
-            self._progenitor_Omegaphi= acfs[4]
-            self._progenitor_Omegaz= acfs[5]
-            self._progenitor_angler= acfs[6]
-            self._progenitor_anglephi= acfs[7]
-            self._progenitor_anglez= acfs[8]
+            self._lnsigjr= numpy.log(self._sigjr)
+            self._lnsiglz= numpy.log(self._siglz)
+            self._lnsigjz= numpy.log(self._sigjz)
+            self._lnsigangle= numpy.log(self._sigangle)
         return None
         
     def __call__(self,*args,**kwargs):
@@ -157,7 +163,7 @@ class streamdf:
                             +(vY-self._progenitor_vY)**2.
                             +(vZ-self._progenitor_vZ)**2.))
         logdf= _mylogsumexp(logdft,axis=1)
-        out= numpy.sum(logdf)-3.*X.shape[0]*numpy.log(self._sigv*self._sigx)
+        out= numpy.sum(logdf)-3.*X.shape[0]*(self._lnsigv+self._lnsigx)
         if log:
             return out
         else:
@@ -171,9 +177,22 @@ class streamdf:
             kwargs.pop('log')
         else:
             log= False
-        jr,lz,jz,Or,Ophi,Oz,ar,aphi,az= self.prepData4aA(*args,**kwargs)
-        
-        raise NotImplementedError("Using action-angle framework not immplemented yet")
+        djr,dlz,djz,dOr,dOphi,dOz,dar,daphi,daz= self.prepData4aA(*args,**kwargs)
+        no= len(djr)
+        logdfJ= numpy.sum(-0.5*(1./self._sigjr2*djr**2.
+                                +1./self._siglz2*dlz**2.
+                                +1./self._sigjz2*djz**2.))\
+                                -no*(self._lnsigjr+self._lnsiglz+self._lnsigjz)
+        da2= dar**2.+daphi**2.+daz**2.
+        do2= dOr**2.+dOphi**2.+dOz**2.
+        doa= dar*dOr+daphi*dOphi+daz*dOz
+        logdfA= numpy.sum(-0.5/self._sigangle2*(da2-doa**2./do2)\
+                               -0.5*numpy.log(do2))-2.*no*self._lnsigangle
+        out= logdfA+logdfJ
+        if log:
+            return out
+        else:
+            return numpy.exp(out)
 
     def prepData4Direct(self,*args,**kwargs):
         """
@@ -207,14 +226,31 @@ class streamdf:
         INPUT:
            __call__ inputs
         OUTPUT:
-           jr,lz,jz,Omegar,Omegaphi,Omegaz,angler,anglephi,anglez; each [nobj,ntimes]
+           djr,dlz,djz,dOmegar,dOmegaphi,dOmegaz,dangler,danglephi,danglez; each [nobj,ntimes]; differences wrt the progenitor
         HISTORY:
            2013-09-17 - Written - Bovy (IAS)
         """
         if len(args) == 9: #actions, frequencies, and angles are given
             return args
         R,vR,vT,z,vz,phi= self._parse_call_args(False,*args)
-        return self._aA.actionsFreqsAngles(R,vR,vT,z,vz,phi,maxn=3)
+        jr,lz,jz,Or,Ophi,Oz,ar,aphi,az= self._aA.actionsFreqsAngles(R,vR,vT,z,vz,phi,maxn=3)
+        djr= jr-self._progenitor_jr
+        dlz= lz-self._progenitor_lz
+        djz= jz-self._progenitor_jz
+        dOr= Or-self._progenitor_Omegar
+        dOphi= Ophi-self._progenitor_Omegaphi
+        dOz= Oz-self._progenitor_Omegaz
+        dar= ar-self._progenitor_angler
+        daphi= aphi-self._progenitor_anglephi
+        daz= az-self._progenitor_anglez
+        #Assuming single wrap, resolve large angle differences (wraps should be marginalized over)
+        dar[(dar < -4.)]+= 2.*numpy.pi
+        dar[(dar > 4.)]-= 2.*numpy.pi
+        daphi[(daphi < -4.)]+= 2.*numpy.pi
+        daphi[(daphi > 4.)]-= 2.*numpy.pi
+        daz[(daz < -4.)]+= 2.*numpy.pi
+        daz[(daz > 4.)]-= 2.*numpy.pi
+        return (djr,dlz,djz,dOr,dOphi,dOz,dar,daphi,daz)
 
     def _parse_call_args(self,directIntegration=True,*args):
         """Helper function to parse the arguments to the __call__ and related functions"""
