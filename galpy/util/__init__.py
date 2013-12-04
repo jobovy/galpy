fad8173ee8edd7d028289f0830d19fed69fdaac0
Jo Bovy <bovy@ias.edu>
2013-12-03 22:58:17 -0500

    add cholesky functions to util

--- galpy/util/__init__.py
+++ galpy/util/__init__.py
@@ -5,6 +5,7 @@ import copy
 import tempfile
 import pickle
 import numpy
+import scipy.linalg as linalg
 class galpyWarning(Warning):
     pass
 old_showwarning= copy.copy(warnings.showwarning)
@@ -75,20 +76,42 @@ def logsumexp(arr,axis=0):
         minarr= numpy.reshape(minarr,(arr.shape[0]))
     return minarr+numpy.log(numpy.sum(numpy.exp(arr-minminarr),axis=axis))
 
-def stable_cholesky(x,tiny):
+_TINY= 0.000000001
+def stable_cho_factor(x,tiny=_TINY):
     """
     NAME:
-       stable_cholesky
+       stable_cho_factor
     PURPOSE:
        Stable version of the cholesky decomposition
     INPUT:
        x - (sc.array) positive definite matrix
-       tiny - (double) tiny number to add to the covariance matrix to make the decomposition stable
+       tiny - (double) tiny number to add to the covariance matrix to make the decomposition stable (has a default)
     OUTPUT:
-       L - (matrix) lower matrix
+       (L,lowerFlag) - output from scipy.linalg.cho_factor for lower=True
     REVISION HISTORY:
        2009-09-25 - Written - Bovy (NYU)
     """
-    thisx= x+tiny*numpy.eye(x.shape[0])
-    L= numpy.linalg.cholesky(thisx)
-    return L
+    
+    return linalg.cho_factor(x+numpy.sum(numpy.diag(x))*tiny*numpy.eye(x.shape[0]),lower=True)
+
+def fast_cholesky_invert(A,logdet=False,tiny=_TINY):
+    """
+    NAME:
+       fast_cholesky_invert
+    PURPOSE:
+       invert a positive definite matrix by using its Cholesky decomposition
+    INPUT:
+       A - matrix to be inverted
+       logdet - (Bool) if True, return the logarithm of the determinant as well
+       tiny - (double) tiny number to add to the covariance matrix to make the decomposition stable (has a default)
+    OUTPUT:
+       A^{-1}
+    REVISION HISTORY:
+       2009-10-07 - Written - Bovy (NYU)
+    """
+    L= stable_cho_factor(A,tiny=tiny)
+    if logdet:
+        return (linalg.cho_solve(L,numpy.eye(A.shape[0])),
+                2.*numpy.sum(numpy.log(numpy.diag(L[0]))))
+    else:
+        return linalg.cho_solve(L,numpy.eye(A.shape[0]))
