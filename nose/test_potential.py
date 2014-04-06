32a9e0219729726aef047343e4ec4a95e71dd157
Jo Bovy <bovy@ias.edu>
2014-04-06 22:16:23 +0200

    test z2deriv

--- nose/test_potential.py
+++ nose/test_potential.py
@@ -119,3 +119,79 @@ def test_forceAsDeriv_potential():
                         assert((tzforce-mpotderivz)**2./tzforce**2. < 10.**ttol)
                 except AssertionError:
                     raise AssertionError("Calculation of the vertical force as the vertical derivative of the %s potential fails at (R,Z) = (%.3f,%.3f); diff = %e, rel. diff = %e" % (p,Rs[ii],Zs[jj],numpy.fabs(mpotderivz),numpy.fabs((tzforce-mpotderivz)/tzforce)))
+
+#Test whether the second derivative of the potential is minus the derivative of the force
+def test_2ndDeriv_potential():
+    from galpy import potential
+    #Grab all of the potentials
+    pots= [p for p in dir(potential) 
+           if ('Potential' in p and not 'plot' in p and not 'RZTo' in p 
+               and not 'evaluate' in p)]
+    rmpots= ['Potential','MWPotential','MovingObjectPotential',
+             'interpRZPotential', 'linearPotential', 'planarAxiPotential',
+             'planarPotential', 'verticalPotential']
+    for p in rmpots:
+        pots.remove(p)
+    Rs= numpy.array([0.5,1.,2.])
+    Zs= numpy.array([0.,.125,-.125,0.25,-0.25])
+    phis= numpy.array([0.,0.5,-0.5,1.,-1.,
+                       numpy.pi,0.5+numpy.pi,
+                       1.+numpy.pi])
+    #tolerances in log10
+    tol= {}
+    tol['default']= -8.
+    tol['DoubleExponentialDiskPotential']= -3. #these are more difficult
+    tol['RazorThinExponentialDiskPotential']= -6.
+    for p in pots:
+        #if not 'NFW' in p: continue #For testing the test
+        #Setup instance of potential
+        tclass= getattr(potential,p)
+        tp= tclass()
+        #raise AttributeError("Do something about potentials without normalize")
+        if hasattr(tp,'normalize'): tp.normalize(1.)
+        #Set tolerance
+        if p in tol.keys(): ttol= tol[p]
+        else: ttol= tol['default']
+        #2nd radial
+        if hasattr(tp,'_R2deriv'):
+            for ii in range(len(Rs)):
+                for jj in range(len(Zs)):
+                    if p == 'RazorThinExponentialDiskPotential' and numpy.fabs(Zs[jj]) > 0.: continue #Not implemented
+                    dr= 10.**-8.
+                    newR= Rs[ii]+dr
+                    dr= newR-Rs[ii] #Representable number
+                    if isinstance(tp,potential.linearPotential): 
+                        mRforcederivR= (tp.Rforce(Rs[ii])-tp.Rforce(Rs[ii]+dr))/dr
+                        tR2deriv= tp.R2deriv(Rs[ii])
+                    else:
+                        mRforcederivR= (tp.Rforce(Rs[ii],Zs[jj])-tp.Rforce(Rs[ii]+dr,Zs[jj]))/dr
+                        tR2deriv= tp.R2deriv(Rs[ii],Zs[jj])
+                    try:
+                        if tR2deriv**2. < 10.**ttol:
+                            assert(mRforcederivR**2. < 10.**ttol)
+                        else:
+                            assert((tR2deriv-mRforcederivR)**2./tR2deriv**2. < 10.**ttol)
+                    except AssertionError:
+                        raise AssertionError("Calculation of the second Radial derivative of the potential as the Radial derivative of the %s Radial force fails at (R,Z) = (%.3f,%.3f); diff = %e, rel. diff = %e" % (p,Rs[ii],Zs[jj],numpy.fabs(tR2deriv-mRforcederivR), numpy.fabs((tR2deriv-mRforcederivR)/tR2deriv)))
+        #2nd vertical
+        if not isinstance(tp,potential.planarPotential) \
+                and not isinstance(tp,potential.linearPotential) \
+                and hasattr(tp,'_z2deriv'):
+            print p, hasattr(tp,'_z2deriv')
+            for ii in range(len(Rs)):
+                for jj in range(len(Zs)):
+                    if p == 'RazorThinExponentialDiskPotential': continue #Not implemented, or badly defined
+                    if p == 'TwoPowerSphericalPotential': continue #Not implemented, or badly defined
+                    dz= 10.**-8.
+                    newz= Zs[jj]+dz
+                    dz= newz-Zs[jj] #Representable number
+                    mzforcederivz= (tp.zforce(Rs[ii],Zs[jj])-tp.zforce(Rs[ii],Zs[jj]+dz))/dz
+                    tz2deriv= tp.z2deriv(Rs[ii],Zs[jj])
+                    try:
+                        if tz2deriv**2. < 10.**ttol:
+                            assert(mzforcederivz**2. < 10.**ttol)
+                        else:
+                            assert((tz2deriv-mzforcederivz)**2./tz2deriv**2. < 10.**ttol)
+                    except AssertionError:
+                        raise AssertionError("Calculation of the second vertical derivative of the potential as the vertical derivative of the %s vertical force fails at (R,Z) = (%.3f,%.3f); diff = %e, rel. diff = %e" % (p,Rs[ii],Zs[jj],numpy.fabs(tz2deriv-mzforcederivz), numpy.fabs((tz2deriv-mzforcederivz)/tz2deriv)))
+#    raise AssertionError
