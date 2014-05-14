938c65a3597fdaf3b259523456dac00925348aeb
Jo Bovy <bovy@ias.edu>
2014-05-14 19:51:16 -0400

    start on orbit test suite; test energy conservation for the different potentials and the different integrators

---
+++ nose/test_orbit.py
@@ -0,0 +1,67 @@
+##############################TESTS ON ORBITS##################################
+import sys
+import numpy
+import os
+from galpy import potential
+_TRAVIS= bool(os.getenv('TRAVIS'))
+if not _TRAVIS:
+    _QUICKTEST= True #Run a more limited set of tests
+
+# Test whether the energy of simple orbits is conserved for different
+# integrators
+def test_energy_conservation():
+    #Basic parameters for the test
+    times= numpy.linspace(0.,280.,10001) #~10 Gyr at the Solar circle
+    integrators= ['odeint','leapfrog','leapfrog_c',
+                  'rk4_c','rk6_c',
+                  'symplec4_c','symplec6_c',
+                  'dopr54_c']
+    #Grab all of the potentials
+    pots= [p for p in dir(potential) 
+           if ('Potential' in p and not 'plot' in p and not 'RZTo' in p 
+               and not 'evaluate' in p)]
+    rmpots= ['Potential','MWPotential','MovingObjectPotential',
+             'interpRZPotential', 'linearPotential', 'planarAxiPotential',
+             'planarPotential', 'verticalPotential','PotentialError']
+    if _TRAVIS: #travis CI
+        rmpots.append('DoubleExponentialDiskPotential')
+        rmpots.append('RazorThinExponentialDiskPotential')
+    for p in rmpots:
+        pots.remove(p)
+    #tolerances in log10
+    tol= {}
+    tol['default']= -10.
+    tol['DoubleExponentialDiskPotential']= -6. #these are more difficult
+    for p in pots:
+        if _QUICKTEST and not 'NFW' in p: continue #For testing the test
+        #Setup instance of potential
+        if p in tol.keys(): ttol= tol[p]
+        else: ttol= tol['default']
+        try:
+            tclass= getattr(potential,p)
+        except AttributeError:
+            tclass= getattr(sys.modules[__name__],p)
+        tp= tclass()
+        if not hasattr(tp,'normalize'): continue #skip these
+        tp.normalize(1.)
+        for integrator in integrators:
+            o= setup_orbit_energy(tp)
+            o.integrate(times,tp,method=integrator)
+            tEs= o.E(times)
+            print p, integrator, (numpy.std(tEs)/numpy.mean(tEs))**2.
+            try:
+                assert((numpy.std(tEs)/numpy.mean(tEs))**2. < 10.**ttol)
+            except AssertionError:
+                raise AssertionError("Energy conservation during the orbit integration fails for potential %s and integrator %s" %(p,integrator))
+#    raise AssertionError
+    return None
+
+def setup_orbit_energy(tp):
+    from galpy.orbit import Orbit
+    if isinstance(tp,potential.linearPotential): 
+        o= Orbit([1.,1.])
+    elif isinstance(tp,potential.planarPotential): 
+        o= Orbit([1.,1.1,1.1,0.])
+    else:
+        o= Orbit([1.,1.1,1.1,0.1,0.1,0.])
+    return o
