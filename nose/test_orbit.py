4d4e3f61963615edaf665f1dd1aabc43458b65c1
Jo Bovy <bovy@ias.edu>
2014-05-17 19:45:54 -0400

    test of analytic calculation of eccentricity, peri-, and apocenter radii

--- nose/test_orbit.py
+++ nose/test_orbit.py
@@ -138,6 +138,7 @@ def test_energy_symplec_longterm():
    
 # Test that the eccentricity of circular orbits is zero
 def test_eccentricity():
+    #return None
     #Basic parameters for the test
     times= numpy.linspace(0.,7.,251) #~10 Gyr at the Solar circle
     integrators= ['dopr54_c', #first, because we do it for all potentials
@@ -218,6 +219,7 @@ def test_eccentricity():
     
 # Test that the pericenter of orbits launched with vR=0 and vT > vc is the starting radius
 def test_pericenter():
+    #return None
     #Basic parameters for the test
     times= numpy.linspace(0.,7.,251) #~10 Gyr at the Solar circle
     integrators= ['dopr54_c', #first, because we do it for all potentials
@@ -298,6 +300,7 @@ def test_pericenter():
 
 # Test that the apocenter of orbits launched with vR=0 and vT < vc is the starting radius
 def test_apocenter():
+    #return None
     #Basic parameters for the test
     times= numpy.linspace(0.,7.,251) #~10 Gyr at the Solar circle
     integrators= ['dopr54_c', #first, because we do it for all potentials
@@ -379,6 +382,7 @@ def test_apocenter():
 
 # Test that the zmax of orbits launched with vz=0 is the starting height
 def test_zmax():
+    #return None
     #Basic parameters for the test
     times= numpy.linspace(0.,7.,251) #~10 Gyr at the Solar circle
     integrators= ['dopr54_c', #first, because we do it for all potentials
@@ -444,6 +448,97 @@ def test_zmax():
 # Test the vT of circular orbits is always vc
 
 # Test that the eccentricity, apo-, and pericenters of orbits calculated analytically agrees with the numerical calculation
+def test_analytic_ecc_rperi_rap():
+    #Basic parameters for the test
+    times= numpy.linspace(0.,20.,251) #~10 Gyr at the Solar circle
+    integrators= ['dopr54_c', #first, because we do it for all potentials
+                  'odeint', #direct python solver
+                  'leapfrog','leapfrog_c',
+                  'rk4_c','rk6_c',
+                  'symplec4_c','symplec6_c']
+    #Grab all of the potentials
+    pots= [p for p in dir(potential) 
+           if ('Potential' in p and not 'plot' in p and not 'RZTo' in p 
+               and not 'evaluate' in p)]
+    rmpots= ['Potential','MWPotential','MovingObjectPotential',
+             'interpRZPotential', 'linearPotential', 'planarAxiPotential',
+             'planarPotential', 'verticalPotential','PotentialError']
+    rmpots.append('FlattenedPowerPotential') #odd behavior, issue #148
+    if _TRAVIS: #travis CI
+        rmpots.append('DoubleExponentialDiskPotential')
+        rmpots.append('RazorThinExponentialDiskPotential')
+    for p in rmpots:
+        pots.remove(p)
+    #tolerances in log10
+    tol= {}
+    tol['default']= -10.
+    tol['DoubleExponentialDiskPotential']= -6. #these are more difficult
+    tol['IsochronePotential']= -6. #these are more difficult
+    tol['JaffePotential']= -6. #these are more difficult
+    tol['PowerSphericalPotential']= -8. #these are more difficult
+    tol['PowerSphericalPotentialwCutoff']= -8. #these are more difficult
+    for p in pots:
+        #Setup instance of potential
+        if p in tol.keys(): ttol= tol[p]
+        else: ttol= tol['default']
+        if p == 'MWPotential':
+            tp= potential.MWPotential
+            ptp= [ttp.toPlanar() for ttp in tp]
+        else:
+            try:
+                tclass= getattr(potential,p)
+            except AttributeError:
+                tclass= getattr(sys.modules[__name__],p)
+            tp= tclass()
+            if not hasattr(tp,'normalize'): continue #skip these
+            tp.normalize(1.)
+            ptp= tp.toPlanar()
+        for integrator in integrators:
+            for ii in range(4):
+                if ii == 0: #axi, full
+                    #First do axi
+                    o= setup_orbit_analytic(tp,axi=True)
+                    o.integrate(times,tp,method=integrator)
+                elif ii == 1: #track azimuth, full
+                    #First do axi
+                    o= setup_orbit_analytic(tp,axi=False)
+                    o.integrate(times,tp,method=integrator)
+                elif ii == 2: #axi, planar
+                    #First do axi
+                    o= setup_orbit_analytic(ptp,axi=True)
+                    o.integrate(times,ptp,method=integrator)
+                elif ii == 3: #track azimuth, full
+                    #First do axi
+                    o= setup_orbit_analytic(ptp,axi=False)
+                    o.integrate(times,ptp,method=integrator)
+                #Eccentricity
+                tecc= o.e()
+                tecc_analytic= o.e(analytic=True)
+#                print p, integrator, tecc, tecc_analytic, (tecc-tecc_analytic)**2.
+                try:
+                    assert((tecc-tecc_analytic)**2. < 10.**ttol)
+                except AssertionError:
+                    raise AssertionError("Analytically computed eccentricity does not agree with numerical estimate for potential %s and integrator %s" %(p,integrator))
+                #Pericenter radius
+                trperi= o.rperi()
+                trperi_analytic= o.rperi(analytic=True)
+#                print p, integrator, trperi, trperi_analytic, (trperi-trperi_analytic)**2.
+                try:
+                    assert((trperi-trperi_analytic)**2. < 10.**ttol)
+                except AssertionError:
+                    raise AssertionError("Analytically computed pericenter radius does not agree with numerical estimate for potential %s and integrator %s" %(p,integrator))
+                #Apocenter radius
+                trap= o.rap()
+                trap_analytic= o.rap(analytic=True)
+#                print p, integrator, trap, trap_analytic, (trap-trap_analytic)**2.
+                try:
+                    assert((trap-trap_analytic)**2. < 10.**ttol)
+                except AssertionError:
+                    raise AssertionError("Analytically computed apocenter radius does not agree with numerical estimate for potential %s and integrator %s" %(p,integrator))
+            if _QUICKTEST and not 'NFW' in p: break
+    #raise AssertionError
+    return None
+    
 
 # Check that adding a linear orbit to a planar orbit gives a FullOrbit
 
@@ -522,4 +617,19 @@ def setup_orbit_zmax(tp,axi=False):
         o= Orbit([1.,0.,0.98,0.05,0.])
     else:
         o= Orbit([1.,0.,0.98,0.05,0.,0.])
+    return o
+
+# Setup the orbit for the apocenter test
+def setup_orbit_analytic(tp,axi=False):
+    from galpy.orbit import Orbit
+    if isinstance(tp,potential.planarPotential): 
+        if axi:
+            o= Orbit([1.,0.1,0.9])
+        else:
+            o= Orbit([1.,0.1,0.9,0.])
+    else:
+        if axi:
+            o= Orbit([1.,0.1,0.9,0.,0.])
+        else:
+            o= Orbit([1.,0.1,0.9,0.,0.,0.])
     return o
