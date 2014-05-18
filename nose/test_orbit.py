2227d7150ad76e82af0392b25085fae68b0a5e3f
Jo Bovy <bovy@ias.edu>
2014-05-17 22:36:03 -0400

    test analytic computation of zmax

--- nose/test_orbit.py
+++ nose/test_orbit.py
@@ -739,6 +739,72 @@ def test_analytic_ecc_rperi_rap():
     #raise AssertionError
     return None
     
+# Check that zmax calculated analytically agrees with numerical calculation
+def test_analytic_zmax():
+    #Basic parameters for the test
+    times= numpy.linspace(0.,20.,251) #~10 Gyr at the Solar circle
+    integrators= ['dopr54_c', #first, because we do it for all potentials
+                  'odeint', #direct python solver
+                  'leapfrog','leapfrog_c',
+                  'rk4_c','rk6_c',
+                  'symplec4_c','symplec6_c']
+    #Grab all of the potentials
+    pots= [p for p in dir(potential) 
+           if ('Potential' in p and not 'plot' in p and not 'RZTo' in p 
+               and not 'evaluate' in p)]
+    rmpots= ['Potential','MWPotential','MovingObjectPotential',
+             'interpRZPotential', 'linearPotential', 'planarAxiPotential',
+             'planarPotential', 'verticalPotential','PotentialError']
+    rmpots.append('FlattenedPowerPotential') #odd behavior, issue #148
+    if _TRAVIS: #travis CI
+        rmpots.append('DoubleExponentialDiskPotential')
+        rmpots.append('RazorThinExponentialDiskPotential')
+    for p in rmpots:
+        pots.remove(p)
+    #tolerances in log10
+    tol= {}
+    tol['default']= -10.
+    tol['DoubleExponentialDiskPotential']= -6. #these are more difficult
+    tol['RazorThinExponentialDiskPotential']= -4. #these are more difficult
+    tol['HernquistPotential']= -8. #these are more difficult
+    tol['JaffePotential']= -8. #these are more difficult
+    tol['MiyamotoNagaiPotential']= -7. #these are more difficult
+    tol['LogarithmicHaloPotential']= -7. #these are more difficult
+    tol['KeplerPotential']= -7. #these are more difficult
+    tol['PowerSphericalPotentialwCutoff']= -8. #these are more difficult
+    for p in pots:
+        #Setup instance of potential
+        if p in tol.keys(): ttol= tol[p]
+        else: ttol= tol['default']
+        if p == 'MWPotential':
+            tp= potential.MWPotential
+        else:
+            try:
+                tclass= getattr(potential,p)
+            except AttributeError:
+                tclass= getattr(sys.modules[__name__],p)
+            tp= tclass()
+            if not hasattr(tp,'normalize'): continue #skip these
+            tp.normalize(1.)
+        for integrator in integrators:
+            for ii in range(2):
+                if ii == 0: #axi, full
+                    #First do axi
+                    o= setup_orbit_analytic_zmax(tp,axi=True)
+                elif ii == 1: #track azimuth, full
+                    #First do axi
+                    o= setup_orbit_analytic_zmax(tp,axi=False)
+                o.integrate(times,tp,method=integrator)
+                tzmax= o.zmax()
+                tzmax_analytic= o.zmax(analytic=True)
+#                print p, integrator, tzmax, tzmax_analytic, (tzmax-tzmax_analytic)**2.
+                try:
+                    assert((tzmax-tzmax_analytic)**2. < 10.**ttol)
+                except AssertionError:
+                    raise AssertionError("Analytically computed zmax does not agree with numerical estimate for potential %s and integrator %s" %(p,integrator))
+            if _QUICKTEST and not 'NFW' in p: break
+    #raise AssertionError
+    return None
 
 # Check that adding a linear orbit to a planar orbit gives a FullOrbit
 def test_add_linear_planar_orbit():
@@ -855,4 +921,13 @@ def setup_orbit_analytic(tp,axi=False):
             o= Orbit([1.,0.1,0.9,0.,0.])
         else:
             o= Orbit([1.,0.1,0.9,0.,0.,0.])
+    return o
+
+# Setup the orbit for the zmax test
+def setup_orbit_analytic_zmax(tp,axi=False):
+    from galpy.orbit import Orbit
+    if axi:
+        o= Orbit([1.,0.,1.,0.05,0.03])
+    else:
+        o= Orbit([1.,0.,1.,0.05,0.03,0.0])
     return o
