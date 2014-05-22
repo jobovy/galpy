4f81e0978eb17e95371a23cc040a50089b1ed967
Jo Bovy <bovy@ias.edu>
2014-05-22 17:09:25 -0400

    start on module to test streamdf; add expected failure decorator

---
+++ nose/test_streamdf.py
@@ -0,0 +1,18 @@
+import functools
+import nose
+
+# Decorator for expected failure
+def expected_failure(test):
+    @functools.wraps(test)
+    def inner(*args, **kwargs):
+        try:
+            test(*args, **kwargs)
+        except Exception:
+            raise nose.SkipTest
+        else:
+            raise AssertionError('Test is expected to fail, but passed instead')
+    return inner
+
+@expected_failure
+def test_setup():
+    return None
