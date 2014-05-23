1e6da1b188ccb919f796a027fe990461b76c2639
Jo Bovy <bovy@ias.edu>
2014-05-22 22:02:17 -0400

    tests of different actionAngleIsochroneApprox setups and of plotting

--- nose/test_actionAngle.py
+++ nose/test_actionAngle.py
@@ -1,7 +1,7 @@
 import numpy
 
+#Test the actionAngleIsochroneApprox against an isochrone potential: actions
 def test_actionAngleIsochroneApprox_otherIsochrone_actions():
-    """Test the actionAngleIsochroneApprox against an isochrone potential: actions"""
     from galpy.potential import IsochronePotential
     from galpy.actionAngle import actionAngleIsochroneApprox, \
         actionAngleIsochrone
@@ -20,8 +20,8 @@ def test_actionAngleIsochroneApprox_otherIsochrone_actions():
     assert djz < 10.**-10., 'actionAngleIsochroneApprox applied to isochrone potential fails for Jz at %f%%' % (djz*100.)
     return None
 
-def test_actionAngleIsochroneApprox_otherIsochrone_freqs():
-    """Test the actionAngleIsochroneApprox against an isochrone potential: frequencies"""
+#Test the actionAngleIsochroneApprox against an isochrone potential: frequencies
+def test_actionAngleIsochroneApprox_otherIsochrone_freqs():   
     from galpy.potential import IsochronePotential
     from galpy.actionAngle import actionAngleIsochroneApprox, \
         actionAngleIsochrone
@@ -39,8 +39,50 @@ def test_actionAngleIsochroneApprox_otherIsochrone_freqs():
     assert dOz < 10.**-6., 'actionAngleIsochroneApprox applied to isochrone potential fails for Oz at %f%%' % (dOz*100.)
     return None
 
-def test_actionAngleIsochroneApprox_bovy14():
-    """Test the actionAngleIsochroneApprox used in Bovy (2014)"""
+#Check that actionAngleIsochroneApprox gives the same answer for different setups
+def test_actionAngleIsochroneApprox_diffsetups(): 
+    from galpy.potential import LogarithmicHaloPotential, \
+        IsochronePotential
+    from galpy.actionAngle import actionAngleIsochroneApprox, \
+        actionAngleIsochrone
+    from galpy.orbit import Orbit
+    lp= LogarithmicHaloPotential(normalize=1.,q=0.9)
+    #Different setups
+    aAI= actionAngleIsochroneApprox(pot=lp,b=0.8)
+    aAIip= actionAngleIsochroneApprox(pot=lp,
+                                      ip=IsochronePotential(normalize=1.,
+                                                            b=0.8))
+    aAIaAIip= actionAngleIsochroneApprox(pot=lp,
+                                         aAI=actionAngleIsochrone(ip=IsochronePotential(normalize=1.,
+                                                                                        b=0.8)))
+    aAIrk6= actionAngleIsochroneApprox(pot=lp,b=0.8,integrate_method='rk6_c')
+    aAIlong= actionAngleIsochroneApprox(pot=lp,b=0.8,tintJ=200.)
+    aAImany= actionAngleIsochroneApprox(pot=lp,b=0.8,ntintJ=20000)
+    #Orbit to test on
+    obs= Orbit([1.56148083,0.35081535,-1.15481504,
+                0.88719443,-0.47713334,0.12019596])
+    #Actions, frequencies, angles
+    acfs= numpy.array(list(aAI.actionsFreqsAngles(obs))).flatten()
+    acfsip= numpy.array(list(aAIip.actionsFreqsAngles(obs))).flatten()
+    acfsaAIip= numpy.array(list(aAIaAIip.actionsFreqsAngles(obs))).flatten()
+    acfsrk6= numpy.array(list(aAIrk6.actionsFreqsAngles(obs))).flatten()
+    acfslong= numpy.array(list(aAIlong.actionsFreqsAngles(obs))).flatten()
+    acfsmany= numpy.array(list(aAImany.actionsFreqsAngles(obs))).flatten()
+    #Check that they are the same
+    assert numpy.amax(numpy.fabs((acfs-acfsip)/acfs)) < 10.**-16., \
+        'actionAngleIsochroneApprox calculated w/ b= and ip= set to the equivalent IsochronePotential do not agree'
+    assert numpy.amax(numpy.fabs((acfs-acfsaAIip)/acfs)) < 10.**-16., \
+        'actionAngleIsochroneApprox calculated w/ b= and aAI= set to the equivalent IsochronePotential do not agree'
+    assert numpy.amax(numpy.fabs((acfs-acfsrk6)/acfs)) < 10.**-8., \
+        'actionAngleIsochroneApprox calculated w/ integrate_method=dopr54_c and rk6_c do not agree at %g%%' %(100.*numpy.amax(numpy.fabs((acfs-acfsrk6)/acfs)))
+    assert numpy.amax(numpy.fabs((acfs-acfslong)/acfs)) < 10.**-2., \
+        'actionAngleIsochroneApprox calculated w/ tintJ=100 and 200 do not agree at %g%%' % (100.*numpy.amax(numpy.fabs((acfs-acfslong)/acfs)))
+    assert numpy.amax(numpy.fabs((acfs-acfsmany)/acfs)) < 10.**-4., \
+        'actionAngleIsochroneApprox calculated w/ ntintJ=10000 and 20000 do not agree at %g%%' % (100.*numpy.amax(numpy.fabs((acfs-acfsmany)/acfs)))
+    return None
+
+#Test the actionAngleIsochroneApprox used in Bovy (2014)
+def test_actionAngleIsochroneApprox_bovy14():   
     from galpy.potential import LogarithmicHaloPotential
     from galpy.actionAngle import actionAngleIsochroneApprox
     from galpy.orbit import Orbit
@@ -50,14 +92,30 @@ def test_actionAngleIsochroneApprox_bovy14():
                 0.88719443,-0.47713334,0.12019596])
     times= numpy.linspace(0.,100.,51)
     obs.integrate(times,lp,method='dopr54_c')
-    js= numpy.empty((3,len(times)))
-    for ii in range(len(times)):
-        tjr, tjp, tjz= aAI(obs(times[ii]))
-        js[0,ii]= tjr
-        js[1,ii]= tjp
-        js[2,ii]= tjz
+    js= aAI(obs.R(times),obs.vR(times),obs.vT(times),obs.z(times),
+            obs.vz(times),obs.phi(times),nonaxi=True) #nonaxi to test that part of the code
     maxdj= numpy.amax(numpy.fabs((js-numpy.tile(numpy.mean(js,axis=1),(len(times),1)).T)),axis=1)/numpy.mean(js,axis=1)
     assert maxdj[0] < 3.*10.**-2., 'Jr conservation for the GD-1 like orbit of Bovy (2014) fails at %f%%' % (100.*maxdj[0])
     assert maxdj[1] < 10.**-2., 'Lz conservation for the GD-1 like orbit of Bovy (2014) fails at %f%%' % (100.*maxdj[1])
     assert maxdj[2] < 2.*10.**-2., 'Jz conservation for the GD-1 like orbit of Bovy (2014) fails at %f%%' % (100.*maxdj[2])
     return None
+
+def test_actionAngleIsochroneApprox_plotting():   
+    from galpy.potential import LogarithmicHaloPotential
+    from galpy.actionAngle import actionAngleIsochroneApprox
+    from galpy.orbit import Orbit
+    lp= LogarithmicHaloPotential(normalize=1.,q=0.9)
+    aAI= actionAngleIsochroneApprox(pot=lp,b=0.8)
+    obs= Orbit([1.56148083,0.35081535,-1.15481504,
+                0.88719443,-0.47713334,0.12019596])
+    #Various plots that should be produced
+    aAI.plot(obs)
+    aAI.plot(obs,type='jr')
+    aAI.plot(obs,type='lz')
+    aAI.plot(obs,type='jz')
+    aAI.plot(obs,type='jr',downsample=True)
+    aAI.plot(obs,type='lz',downsample=True)
+    aAI.plot(obs,type='jz',downsample=True)
+    aAI.plot(obs,type='araz')
+    aAI.plot(obs,type='araphi',dePeriod=True)
+    aAI.plot(obs,type='azaphi',dePeriod=True)
