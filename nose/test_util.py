bf98cc9591b36ccc911c550f62ab26b249b9ee86
Jo Bovy <bovy@ias.edu>
2014-06-08 16:01:43 -0400

    test logsumexp

--- nose/test_util.py
+++ nose/test_util.py
@@ -18,3 +18,12 @@ def test_save_pickles():
     finally:
         os.remove(tmp_savefilename)
     return None
+
+def test_logsumexp():
+    from galpy.util import logsumexp
+    sumthis= numpy.array([[0.,1.]])
+    sum= numpy.log(numpy.exp(0.)+numpy.exp(1.))
+    assert numpy.all(numpy.fabs(logsumexp(sumthis,axis=0)-sumthis) < 10.**-10.), 'galpy.util.logsumexp did not work as expected'
+    assert numpy.fabs(logsumexp(sumthis,axis=1)-sum) < 10.**-10., 'galpy.util.logsumexp did not work as expected'
+    assert numpy.fabs(logsumexp(sumthis,axis=None)-sum) < 10.**-10., 'galpy.util.logsumexp did not work as expected'
+    return None
