c32bcd83ef25f10b458b241e7bb49def2c65765c
fardal <fardal@astro.umass.edu>
2014-06-13 23:23:28 -0400

    More debug.

--- galpy/df_src/streamdf.py
+++ galpy/df_src/streamdf.py
@@ -193,7 +193,7 @@ class streamdf:
         else:
             if (deltaAngleTrack > deltaAngleTrackLim):
                 warnings.warn("WARNING: angle range large compared to plausible value.", galpyWarning)
-        if (deltaAngleTrack > 100.):  
+        if (abs(deltaAngleTrack) > 100.):  
             raise Exception('Suspiciously large stream angle required, abort.')
 
         #Determine the stream track
