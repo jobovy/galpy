37de751929c584d0f3ff0de6531320f515562555
Jo Bovy <bovy@ias.edu>
2014-06-23 22:06:03 -0400

    test vector input of interpRZPotential's evaluate

--- nose/test_interp_potential.py
+++ nose/test_interp_potential.py
@@ -20,8 +20,9 @@ def test_interpolation_potential():
     zs= numpy.linspace(0.0,0.2,20)
     for r in rs:
         for z in zs:
-            print numpy.fabs((rzpot(r,z)
-                              -potential.evaluatePotentials(r,z,potential.MWPotential))/potential.evaluatePotentials(r,z,potential.MWPotential))
             assert numpy.fabs((rzpot(r,z)
                               -potential.evaluatePotentials(r,z,potential.MWPotential))/potential.evaluatePotentials(r,z,potential.MWPotential)) < 10.**-6., 'RZPot interpolation w/ interpRZPotential fails at (R,z) = (%g,%g)' % (r,z)
+    #Test all at the same time to use vector evaluation
+    mr,mz= numpy.meshgrid(rs,zs)
+    assert numpy.all(numpy.fabs((rzpot(mr,mz)-potential.evaluatePotentials(mr,mz,potential.MWPotential))/potential.evaluatePotentials(mr,mz,potential.MWPotential)) < 10.**-6.), 'RZPot interpolation w/ interpRZPotential fails for vector input'
     return None
