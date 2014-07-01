ec663e2ecddaedaf4dc89eda99eee4bc8912caf9
Jo Bovy <bovy@ias.edu>
2014-07-01 09:52:55 -0400

    also add the new Dehnen potential to liouville

--- nose/test_orbit.py
+++ nose/test_orbit.py
@@ -344,6 +344,7 @@ def test_liouville_planar():
     pots.append('mockFlatEllipticalDiskPotential')
     pots.append('mockFlatLopsidedDiskPotential')
     pots.append('mockFlatDehnenBarPotential')
+    pots.append('mockSlowFlatDehnenBarPotential')
     #pots.append('mockFlatSteadyLogSpiralPotential')
     #pots.append('mockFlatTransientLogSpiralPotential')
     rmpots= ['Potential','MWPotential','MovingObjectPotential',
@@ -413,7 +414,7 @@ def test_liouville_planar():
                                  rectIn=True,rectOut=True)
                 dvy= o.getOrbit_dxdv()[-1,:]
             tjac= numpy.linalg.det(numpy.array([dx,dy,dvx,dvy]))
-            #print p, integrator, numpy.fabs(tjac-1.)
+            print p, integrator, numpy.fabs(tjac-1.)
             assert numpy.fabs(tjac-1.) < 10.**ttol, 'Liouville theorem jacobian differs from one by %g for %s and integrator %s' % (numpy.fabs(tjac-1.),p,integrator)
             if _QUICKTEST and not 'NFW' in p: break
     return None
