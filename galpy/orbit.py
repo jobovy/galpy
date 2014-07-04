d687e8a69ebe9fdb1155db6a3a8100f4d7b8c9fb
Jo Bovy <bovy@ias.edu>
2014-07-03 22:36:11 -0400

    further removal of BC functions

--- galpy/orbit.py
+++ galpy/orbit.py
@@ -3,15 +3,8 @@ from galpy.orbit_src import Orbit
 #
 # Functions
 #
-# Boundary conditions
-zEqZeroBC= Orbit._zEqZeroBC
-vzEqZeroBC= Orbit._vzEqZeroBC
-REqZeroBC= Orbit._REqZeroBC
-REqOneBC= Orbit._REqOneBC
-vREqZeroBC= Orbit._vREqZeroBC
-vTEqZeroBC= Orbit._vTEqZeroBC
-vTEqOneBC= Orbit._vTEqOneBC
-phiEqZeroBC= Orbit._phiEqZeroBC
+
+#none
 
 #
 # Classes
