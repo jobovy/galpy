97372daafa23aaab8ef3eb4b06e50f237659f4f0
Jo Bovy <bovy@ias.edu>
2014-07-21 15:25:08 +0100

    tests of oortA

--- nose/test_diskdf.py
+++ nose/test_diskdf.py
@@ -60,3 +60,33 @@ def test_dehnendf_cold_powerfall_vr():
     assert numpy.fabs(df.meanvR(2.)-0.) < 10.**-3., 'mean vR of cold dehnendf in a power-law rotation curve is not close to zero at R=2'
     return None
 
+# Tests for cold population, flat rotation curve: A = 0.5
+def test_dehnendf_cold_flat_oortA():
+    df= dehnendf(profileParams=(0.3333333333333333,1.0, 0.01),
+                 beta=0.,correct=False)
+    assert numpy.fabs(df.oortA(1.)-0.5*1./1.) < 10.**-3., 'Oort A of cold dehnendf in a flat rotation curve is not close to V_c at R=1'
+    assert numpy.fabs(df.oortA(0.5)-0.5*1./0.5) < 10.**-3., 'Oort A of cold dehnendf in a flat rotation curve is not close to V_c at R=0.5'
+    assert numpy.fabs(df.oortA(2.)-0.5*1./2.) < 10.**-3., 'Oort A of cold dehnendf in a flat rotation curve is not close to V_c at R=2'
+    return None
+
+# Tests for cold population, power-law rotation curve: A = 0.5
+def test_dehnendf_cold_powerrise_oortA():
+    # Rising rotation curve
+    beta= 0.2
+    df= dehnendf(profileParams=(0.3333333333333333,1.0, 0.01),
+                 beta=beta,correct=False)
+    assert numpy.fabs(df.oortA(1.)-0.5*1./1.*(1.-beta)) < 10.**-3., 'Oort A of cold dehnendf in a power-law rotation curve is not close to V_c at R=1'
+    assert numpy.fabs(df.oortA(0.5)-0.5*(0.5)**beta/0.5*(1.-beta)) < 10.**-3., 'Oort A of cold dehnendf in a power-law rotation curve is not close to V_c at R=0.5'
+    assert numpy.fabs(df.oortA(2.)-0.5*(2.)**beta/2.*(1.-beta)) < 10.**-3., 'Oort A of cold dehnendf in a power-law rotation curve is not close to V_c at R=2'
+    return None
+
+def test_dehnendf_cold_powerfall_oortA():
+    # Falling rotation curve
+    beta= -0.2
+    df= dehnendf(profileParams=(0.3333333333333333,1.0, 0.01),
+                 beta=beta,correct=False)
+    assert numpy.fabs(df.oortA(1.)-0.5*1./1.*(1.-beta)) < 10.**-3., 'Oort A of cold dehnendf in a power-law rotation curve is not close to V_c at R=1'
+    assert numpy.fabs(df.oortA(0.5)-0.5*(0.5)**beta/0.5*(1.-beta)) < 10.**-3., 'Oort A of cold dehnendf in a power-law rotation curve is not close to V_c at R=0.5'
+    assert numpy.fabs(df.oortA(2.)-0.5*(2.)**beta/2.*(1.-beta)) < 10.**-3., 'Oort A of cold dehnendf in a power-law rotation curve is not close to V_c at R=2'
+    return None
+
