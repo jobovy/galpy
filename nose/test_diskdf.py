36901845bc591fbe9f9bee37906fcf3f1f3aacad
Jo Bovy <bovy@ias.edu>
2014-07-21 16:38:52 +0100

    tests of skewvT

--- nose/test_diskdf.py
+++ nose/test_diskdf.py
@@ -31,6 +31,36 @@ def test_dehnendf_cold_powerfall_vt():
     assert numpy.fabs(df.meanvT(2.)-(2.)**beta) < 10.**-3., 'mean vT of cold dehnendf in a power-law rotation curve is not close to V_c at R=2'
     return None
 
+# Tests for cold population, flat rotation curve: <vt> =~ v_c
+def test_dehnendf_cold_flat_skewvt():
+    df= dehnendf(profileParams=(0.3333333333333333,1.0, 0.01),
+                 beta=0.,correct=False)
+    assert numpy.fabs(df.skewvT(1.)) < 1./20., 'skew vT of cold dehnendf in a flat rotation curve is not close to zero at R=1'
+    assert numpy.fabs(df.skewvT(0.5)) < 1./20., 'skew vT of cold dehnendf in a flat rotation curve is not close to zero at R=0.5'
+    assert numpy.fabs(df.skewvT(2.)) < 1./20., 'skew vT of cold dehnendf in a flat rotation curve is not close to zero at R=2'
+    return None
+
+# Tests for cold population, power-law rotation curve: <vt> =~ v_c
+def test_dehnendf_cold_powerrise_skewvt():
+    # Rising rotation curve
+    beta= 0.2
+    df= dehnendf(profileParams=(0.3333333333333333,1.0, 0.01),
+                 beta=beta,correct=False)
+    assert numpy.fabs(df.skewvT(1.)) < 1./20., 'skew vT of cold dehnendf in a power-law rotation curve is not close to zero at R=1'
+    assert numpy.fabs(df.skewvT(0.5)) < 1./20., 'skew vT of cold dehnendf in a power-law rotation curve is not close to zero at R=0.5'
+    assert numpy.fabs(df.skewvT(2.)) < 1./20., 'skew vT of cold dehnendf in a power-law rotation curve is not close to zero at R=2'
+    return None
+
+def test_dehnendf_cold_powerfall_skewvt():
+    # Falling rotation curve
+    beta= -0.2
+    df= dehnendf(profileParams=(0.3333333333333333,1.0, 0.01),
+                 beta=beta,correct=False)
+    assert numpy.fabs(df.skewvT(1.)) < 1./20., 'skew vT of cold dehnendf in a power-law rotation curve is not close to zero at R=1'
+    assert numpy.fabs(df.skewvT(0.5)) < 1./20., 'skew vT of cold dehnendf in a power-law rotation curve is not close to zero at R=0.5'
+    assert numpy.fabs(df.skewvT(2.)) < 1./20., 'skew vT of cold dehnendf in a power-law rotation curve is not close to zero at R=2'
+    return None
+
 # Tests for cold population, flat rotation curve: <vr> = 0
 def test_dehnendf_cold_flat_vr():
     df= dehnendf(profileParams=(0.3333333333333333,1.0, 0.01),
