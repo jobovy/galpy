9ef772d1e5a8627112c4e0d7dfaa8729604c0dbe
Jo Bovy <bovy@ias.edu>
2014-08-11 16:08:59 -0400

    test of dehnendf sample w/ returnROrbit and rising rotation curve

--- nose/test_diskdf.py
+++ nose/test_diskdf.py
@@ -1095,6 +1095,23 @@ def test_dehnendf_sample_flat_returnROrbit():
     assert numpy.fabs(numpy.mean(dvts)) < 0.1, 'mean vT of sampled points does not agree with an estimate based on asymmetric drift'
     return None
 
+def test_dehnendf_sample_powerrise_returnROrbit():
+    beta= 0.2
+    dfc= dehnendf(beta=beta,profileParams=(1./4.,1.,0.2))
+    numpy.random.seed(1)
+    os= dfc.sample(n=100,returnROrbit=True)
+    #Test the spatial distribution
+    rs= numpy.array([o.R() for o in os])
+    assert numpy.fabs(numpy.mean(rs)-0.5) < 0.1, 'mean R of sampled points does not agree with that of the input surface profile'
+    assert numpy.fabs(numpy.std(rs)-numpy.sqrt(2.)/4.) < 0.06, 'stddev R of sampled points does not agree with that of the input surface profile'
+    #Test the velocity distribution
+    vrs= numpy.array([o.vR() for o in os])
+    assert numpy.fabs(numpy.mean(vrs)) < 0.05, 'mean vR of sampled points does not agree with that of the input surface profile (i.e., it is not zero)'
+    vts= numpy.array([o.vT() for o in os])
+    dvts= numpy.array([vt-r**beta+dfc.asymmetricdrift(r) for (r,vt) in zip(rs,vts)])
+    assert numpy.fabs(numpy.mean(dvts)) < 0.2, 'mean vT of sampled points does not agree with an estimate based on asymmetric drift'
+    return None
+
 def test_dehnendf_sample_flat_returnOrbit():
     beta= 0.
     dfc= dehnendf(beta=beta,profileParams=(1./4.,1.,0.2))
