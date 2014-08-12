33f8ee19c0026df464bd5a2b6278e00a65bdca72
Jo Bovy <bovy@ias.edu>
2014-08-12 11:28:43 -0700

    further tests of meanvR for staeckel approx and adiabatic approx

--- nose/test_qdf.py
+++ nose/test_qdf.py
@@ -11,8 +11,39 @@ def test_meanvR_adiabatic_gl():
     qdf= quasiisothermaldf(1./4.,0.2,0.1,1.,1.,
                            pot=MWPotential,aA=aAA,cutcounter=True)
     #In the mid-plane
-    assert numpy.fabs(qdf.meanvR(0.9,0.,gl=True)) < 0.01, "qdf's meanvr is not equal to zero"
+    assert numpy.fabs(qdf.meanvR(0.9,0.,gl=True)) < 0.01, "qdf's meanvr is not equal to zero for adiabatic approx."
     #higher up
-    assert numpy.fabs(qdf.meanvR(0.9,0.2,gl=True)) < 0.01, "qdf's meanvr is not equal to zero"
-    assert numpy.fabs(qdf.meanvR(0.9,-0.25,gl=True)) < 0.01, "qdf's meanvr is not equal to zero"
+    assert numpy.fabs(qdf.meanvR(0.9,0.2,gl=True)) < 0.01, "qdf's meanvr is not equal to zero for adiabatic approx."
+    assert numpy.fabs(qdf.meanvR(0.9,-0.25,gl=True)) < 0.01, "qdf's meanvr is not equal to zero for adiabatic approx."
     return None
+
+def test_meanvR_adiabatic_mc():
+    qdf= quasiisothermaldf(1./4.,0.2,0.1,1.,1.,
+                           pot=MWPotential,aA=aAA,cutcounter=True)
+    #In the mid-plane
+    assert numpy.fabs(qdf.meanvR(0.9,0.,mc=True)) < 0.01, "qdf's meanvr is not equal to zero for adiabatic approx."
+    #higher up
+    assert numpy.fabs(qdf.meanvR(0.9,0.2,mc=True)) < 0.01, "qdf's meanvr is not equal to zero for adiabatic approx."
+    assert numpy.fabs(qdf.meanvR(0.9,-0.25,mc=True)) < 0.01, "qdf's meanvr is not equal to zero for adiabatic approx."
+    return None
+
+def test_meanvR_staeckel_gl():
+    qdf= quasiisothermaldf(1./4.,0.2,0.1,1.,1.,
+                           pot=MWPotential,aA=aAS,cutcounter=True)
+    #In the mid-plane
+    assert numpy.fabs(qdf.meanvR(0.9,0.,gl=True)) < 0.01, "qdf's meanvr is not equal to zero for staeckel approx."
+    #higher up
+    assert numpy.fabs(qdf.meanvR(0.9,0.2,gl=True)) < 0.01, "qdf's meanvr is not equal to zero for staeckel approx."
+    assert numpy.fabs(qdf.meanvR(0.9,-0.25,gl=True)) < 0.01, "qdf's meanvr is not equal to zero for staeckel approx."
+    return None
+
+def test_meanvR_staeckel_mc():
+    qdf= quasiisothermaldf(1./4.,0.2,0.1,1.,1.,
+                           pot=MWPotential,aA=aAS,cutcounter=True)
+    #In the mid-plane
+    assert numpy.fabs(qdf.meanvR(0.9,0.,mc=True)) < 0.01, "qdf's meanvr is not equal to zero for staeckel approx."
+    #higher up
+    assert numpy.fabs(qdf.meanvR(0.9,0.2,mc=True)) < 0.01, "qdf's meanvr is not equal to zero for staeckel approx."
+    assert numpy.fabs(qdf.meanvR(0.9,-0.25,mc=True)) < 0.01, "qdf's meanvr is not equal to zero for staeckel approx."
+    return None
+
