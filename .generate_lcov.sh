16266d8f928ba02dbde76ae5180d03c4ce185e97
Jo Bovy <bovy@ias.edu>
2014-08-15 11:42:39 -0700

    much simpler lcov script with single_ext

--- .generate_lcov.sh
+++ .generate_lcov.sh
@@ -2,46 +2,11 @@
 # rm old build
 rm -rf build/
 rm -vf galpy_integrate_c.so galpy_actionAngle_c.so galpy_interppotential_c.so
-# Test the orbit extension
-$PYTHON setup.py build_ext --coverage --orbit_ext --inplace
-$PYTHON setup.py develop --coverage --orbit_ext --prefix=~/local
-lcov --capture --directory build/temp.linux-x86_64-2.7/galpy/ --output-file coverage_orbit_initial.info --initial
+# Test the extensions
+$PYTHON setup.py build_ext --coverage --single_ext --inplace
+$PYTHON setup.py develop --coverage --single_ext --prefix=~/local
 /usr/local/python-2.7.3/bin/nosetests -v -w nose/ -e plotting
-lcov --capture --directory build/temp.linux-x86_64-2.7/galpy/ --output-file coverage_orbit.info
-rm -rf build/
-rm -vf galpy_integrate_c.so galpy_actionAngle_c.so galpy_interppotential_c.so
-# Test the actionAngle extension
-$PYTHON setup.py build_ext --coverage --actionAngle_ext --inplace
-$PYTHON setup.py develop --coverage --actionAngle_ext --prefix=~/local
-lcov --capture --directory build/temp.linux-x86_64-2.7/galpy/ --output-file coverage_actionAngle_initial.info --initial
-/usr/local/python-2.7.3/bin/nosetests -v -w nose/ -e plotting
-lcov --capture --directory build/temp.linux-x86_64-2.7/galpy/ --output-file coverage_actionAngle.info
-rm -rf build/
-rm -vf galpy_integrate_c.so galpy_actionAngle_c.so galpy_interppotential_c.so
-# Test the interppotential extension
-$PYTHON setup.py build_ext --coverage --interppotential_ext --inplace
-$PYTHON setup.py develop --coverage --interppotential_ext --prefix=~/local
-lcov --capture --directory build/temp.linux-x86_64-2.7/galpy/ --output-file coverage_interppotential_initial.info --initial
-/usr/local/python-2.7.3/bin/nosetests -v -w nose/ -e plotting
-lcov --capture --directory build/temp.linux-x86_64-2.7/galpy/ --output-file coverage_interppotential.info
-# Combine
-lcov --add-tracefile coverage_orbit.info --output-file dummy.info
-if [ "$?" -ne "0" ]; then
-    cp coverage_orbit_initial.info coverage_orbit.info
-fi
-lcov --add-tracefile coverage_actionAngle.info --output-file dummy.info
-if [ "$?" -ne "0" ]; then
-    cp coverage_actionAngle_initial.info coverage_actionAngle.info
-fi
-lcov --add-tracefile coverage_interppotential.info --output-file dummy.info
-if [ "$?" -ne "0" ]; then
-    cp coverage_interppotential_initial.info coverage_interppotential.info
-fi
-lcov --add-tracefile coverage_orbit.info --add-tracefile coverage_actionAngle.info --add-tracefile coverage_interppotential.info -output-file coverage.info
-cp coverage.info ~/public_html/galpy_lcov/
+lcov --capture --directory build/temp.linux-x86_64-2.7/galpy/ --output-file coverage.info
 # Generate HTML
 genhtml coverage.info --output-directory ~/public_html/galpy_lcov/
-rm -vf dummy.info
-rm -vf coverage_orbit.info coverage_actionAngle.info coverage_interppotential.info
-rm -vf coverage_orbit_initial.info coverage_actionAngle_initial.info coverage_interppotential_initial.info
 rm -vf coverage.info
