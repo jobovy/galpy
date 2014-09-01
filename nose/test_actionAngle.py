59cd7f1efd7589cf63066dab568186d3e15487a8
Jo Bovy <bovy@ias.edu>
2014-09-01 11:43:30 -0400

    start on some StaeckelGrid tests

--- nose/test_actionAngle.py
+++ nose/test_actionAngle.py
@@ -1001,6 +1001,80 @@ def test_actionAngleStaeckel_otherIsochrone_angles():
     assert daz < 10.**-4., 'actionAngleStaeckel applied to isochrone potential fails for az at %g%%' % (daz*100.)
     return None
 
+#Basic sanity checking of the actionAngleStaeckelGrid actions (incl. conserved, bc takes a lot of time)
+@expected_failure
+def test_actionAngleStaeckelGrid_basicAndConserved_actions():
+    from galpy.actionAngle import actionAngleStaeckelGrid
+    from galpy.orbit import Orbit
+    from galpy.potential import MWPotential
+    aAA= actionAngleStaeckelGrid(pot=MWPotential,delta=0.71,c=False)
+    #circular orbit
+    R,vR,vT,z,vz= 1.,0.,1.,0.,0. 
+    js= aAA(R,vR,vT,z,vz,0.)
+    print js
+    assert numpy.fabs(js[0]) < 10.**-16., 'Circular orbit in the MWPotential does not have Jr=0'
+    assert numpy.fabs(js[2]) < 10.**-16., 'Circular orbit in the MWPotential does not have Jz=0'
+    #Close-to-circular orbit
+    R,vR,vT,z,vz= 1.01,0.01,1.,0.01,0.01 
+    js= aAA(Orbit([R,vR,vT,z,vz]))
+    assert numpy.fabs(js[0]) < 10.**-4., 'Close-to-circular orbit in the MWPotential does not have small Jr'
+    assert numpy.fabs(js[2]) < 10.**-3., 'Close-to-circular orbit in the MWPotentialspherical LogarithmicHalo does not have small Jz'
+    #Check that actions are conserved along the orbit
+    obs= Orbit([1.05, 0.02, 1.05, 0.03,0.])
+    check_actionAngle_conserved_actions(aAA,obs,MWPotential,
+                                        -1.2,-8.,-1.7,ntimes=101)
+    return None
+
+#Basic sanity checking of the actionAngleStaeckel actions
+def test_actionAngleStaeckelGrid_basic_actions_c():
+    from galpy.actionAngle import actionAngleStaeckelGrid
+    from galpy.orbit import Orbit
+    from galpy.potential import MWPotential
+    aAA= actionAngleStaeckelGrid(pot=MWPotential,delta=0.71,c=True)
+    #circular orbit
+    R,vR,vT,z,vz= 1.,0.,1.,0.,0. 
+    js= aAA(R,vR,vT,z,vz)
+    assert numpy.fabs(js[0]) < 10.**-8., 'Circular orbit in the MWPotential does not have Jr=0'
+    assert numpy.fabs(js[2]) < 10.**-8., 'Circular orbit in the MWPotential does not have Jz=0'
+    #Close-to-circular orbit
+    R,vR,vT,z,vz= 1.01,0.01,1.,0.01,0.01 
+    js= aAA(Orbit([R,vR,vT,z,vz]))
+    assert numpy.fabs(js[0]) < 10.**-4., 'Close-to-circular orbit in the MWPotential does not have small Jr'
+    assert numpy.fabs(js[2]) < 10.**-3., 'Close-to-circular orbit in the MWPotentialspherical LogarithmicHalo does not have small Jz'
+
+#Test the actions of an actionAngleStaeckel
+def test_actionAngleStaeckelGrid_conserved_actions_c():
+    from galpy.potential import MWPotential
+    from galpy.actionAngle import actionAngleStaeckelGrid
+    from galpy.orbit import Orbit
+    obs= Orbit([1.05, 0.02, 1.05, 0.03,0.])
+    aAA= actionAngleStaeckelGrid(pot=MWPotential,delta=0.71,c=True)
+    check_actionAngle_conserved_actions(aAA,obs,MWPotential,
+                                        -1.4,-8.,-1.7,ntimes=101)
+    return None
+
+#Test the actionAngleStaeckel against an isochrone potential: actions
+@expected_failure
+def test_actionAngleStaeckelGrid_Isochrone_actions():
+    from galpy.potential import IsochronePotential
+    from galpy.actionAngle import actionAngleStaeckelGrid, \
+        actionAngleIsochrone
+    ip= IsochronePotential(normalize=1.,b=1.2)
+    aAI= actionAngleIsochrone(ip=ip)
+    aAA= actionAngleStaeckelGrid(pot=ip,delta=0.1,c=True)
+    R,vR,vT,z,vz,phi= 1.01, 0.05, 1.05, 0.05,0.,2.
+    ji= aAI(R,vR,vT,z,vz,phi)
+    jia= aAA(R,vR,vT,z,vz,phi)
+    djr= numpy.fabs((ji[0]-jia[0])/ji[0])
+    dlz= numpy.fabs((ji[1]-jia[1])/ji[1])
+    djz= numpy.fabs((ji[2]-jia[2])/ji[2])
+    print ji, jia, djr, dlz, djz
+    assert djr < 10.**-1.2, 'actionAngleStaeckel applied to isochrone potential fails for Jr at %f%%' % (djr*100.)
+    #Lz and Jz are easy, because ip is a spherical potential
+    assert dlz < 10.**-10., 'actionAngleStaeckel applied to isochrone potential fails for Lz at %f%%' % (dlz*100.)
+    assert djz < 10.**-1.2, 'actionAngleStaeckel applied to isochrone potential fails for Jz at %f%%' % (djz*100.)
+    return None
+
 #Test the actionAngleIsochroneApprox against an isochrone potential: actions
 def test_actionAngleIsochroneApprox_otherIsochrone_actions():
     from galpy.potential import IsochronePotential
