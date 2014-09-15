ca6c51e7c50a31541c70a3243a9f106310c1f797
Jo Bovy <bovy@ias.edu>
2014-09-15 10:25:50 -0400

    further section 2 tests, #188

--- nose/test_galpypaper.py
+++ nose/test_galpypaper.py
@@ -17,3 +17,28 @@ def test_overview():
                            pot=np,aA=aA)
     assert numpy.fabs((qdf(o)-61.57476085)/61.57476085) < 10.**-3., 'qdf calculation in the overview section has changed'
     return None
+
+def test_import():
+    import galpy
+    import galpy.potential
+    import galpy.orbit
+    import galpy.actionAngle
+    import galpy.df
+    import galpy.util
+    return None
+
+def test_units():
+    import galpy.util.bovy_conversion as conversion
+    print conversion.force_in_pcMyr2(220.,8.)#pc/Myr^2
+    assert numpy.fabs(conversion.force_in_pcMyr2(220.,8.)-6.32793804994) < 10.**-4., 'unit conversion has changed'
+    print conversion.dens_in_msolpc3(220.,8.)#Msolar/pc^3
+    assert numpy.fabs(conversion.dens_in_msolpc3(220.,8.)-0.175790330079) < 10.**-4., 'unit conversion has changed'
+    print conversion.surfdens_in_msolpc2(220.,8.)#Msolar/pc^2
+    assert numpy.fabs(conversion.surfdens_in_msolpc2(220.,8.)-1406.32264063) < 10.**-4., 'unit conversion has changed'
+    print conversion.mass_in_1010msol(220.,8.)#10^10 Msolar
+    assert numpy.fabs(conversion.mass_in_1010msol(220.,8.)-9.00046490005) < 10.**-4., 'unit conversion has changed'
+    print conversion.freq_in_Gyr(220.,8.)#1/Gyr
+    assert numpy.fabs(conversion.freq_in_Gyr(220.,8.)-28.1245845523) < 10.**-4., 'unit conversion has changed'
+    print conversion.time_in_Gyr(220.,8.)#Gyr
+    assert numpy.fabs(conversion.time_in_Gyr(220.,8.)-0.0355560807712) < 10.**-4., 'unit conversion has changed'
+    return None
