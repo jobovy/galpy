19c3972986ceb15e73ec15eb081112fe3d6d2fa4
Jo Bovy <bovy@ias.edu>
2014-11-24 12:39:02 -0500

    test SnapShotPotential has

--- nose/test_snapshotpotential.py
+++ nose/test_snapshotpotential.py
@@ -41,3 +41,15 @@ def test_snapshotKeplerPotential_zforce():
     assert numpy.fabs(sp.zforce(1.,-0.5)-kp.zforce(1.,-0.5)) < 10.**-8., 'SnapshotPotential with single unit mass does not correspond to KeplerPotential'
     return None
 
+def test_snapshotKeplerPotential_hash():
+    # Test that hashing the previous grid works
+    # Set up a snapshot with just one unit mass at the origin
+    s= pynbody.new(star=1)
+    s['mass']= 1.
+    s['eps']= 0.
+    sp= potential.SnapshotPotential(s)
+    kp= potential.KeplerPotential(amp=1.) #should be the same
+    assert numpy.fabs(sp(1.,0.)-kp(1.,0.)) < 10.**-8., 'SnapshotPotential with single unit mass does not correspond to KeplerPotential'
+    assert numpy.fabs(sp(1.,0.)-kp(1.,0.)) < 10.**-8., 'SnapshotPotential with single unit mass does not correspond to KeplerPotential'
+    return None
+
