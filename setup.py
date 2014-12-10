59bb8a95cb7c8bf317749bda285905f7f653b915
Jo Bovy <bovy@ias.edu>
2014-12-10 11:30:03 -0500

    release v1 today, fixes #221

--- setup.py
+++ setup.py
@@ -191,7 +191,7 @@ else:
     interppotential_c_incl= False
 
 setup(name='galpy',
-      version='1.0.beta',
+      version='1.0',
       description='Galactic Dynamics in python',
       author='Jo Bovy',
       author_email='bovy@ias.edu',
