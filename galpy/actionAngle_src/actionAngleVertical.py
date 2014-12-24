7bf7877113ef55a34aa755e64d10440f5fc30c1e
Jo Bovy <bovy@ias.edu>
2014-12-24 11:12:21 -0500

    python3 dictionary compatibility for actionAngleVertical

--- galpy/actionAngle_src/actionAngleVertical.py
+++ galpy/actionAngle_src/actionAngleVertical.py
@@ -35,7 +35,7 @@ class actionAngleVertical(actionAngle):
            2012-06-01 - Written - Bovy (IAS)
         """
         actionAngle.__init__(self,*args,**kwargs)
-        if not kwargs.has_key('pot'): #pragma: no cover
+        if not 'pot' in kwargs: #pragma: no cover
             raise IOError("Must specify pot= for actionAngleVertical")
         self._verticalpot= kwargs['pot']
         return None
