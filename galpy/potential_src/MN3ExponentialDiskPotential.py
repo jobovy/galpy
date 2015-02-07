b63a62b46200e8e8c58093726d078e7624ce9e9e
Jo Bovy <bovy@ias.edu>
2015-02-07 14:38:24 -0800

    add equations from Tables 1 and 2

--- galpy/potential_src/MN3ExponentialDiskPotential.py
+++ galpy/potential_src/MN3ExponentialDiskPotential.py
@@ -58,3 +58,30 @@ class MN3ExponentialDiskPotential(Potential):
 
         """
         return None
+
+# Equations from Table 1
+def _mass1_tab1(brd):
+    return -0.0090*brd**4.+0.0640*brd**3.-0.1653*brd**2.+0.1164*brd+1.9487
+def _mass2_tab1(brd):
+    return 0.0173*brd**4.-0.0903*brd**3.+0.0877*brd**2.+0.2029*brd-1.3077
+def _mass3_tab1(brd):
+    return -0.0051*brd**4.+0.0287*brd**3.-0.0361*brd**2.-0.0544*brd+0.2242
+def _a1_tab1(brd):
+    return -0.0358*brd**4.+0.2610*brd**3.-0.6987*brd**2.-0.1193*brd+2.0074
+def _a2_tab1(brd):
+    return -0.0830*brd**4.+0.4992*brd**3.-0.7967*brd**2.-1.2966*brd+4.4441
+def _a3_tab1(brd):
+    return -0.0247*brd**4.+0.1718*brd**3.-0.4124*brd**2.-0.5944*brd+0.7333
+# Equations from Table 2
+def _mass1_tab2(brd):
+    return 0.0036*brd**4.-0.0330*brd**3.+0.1117*brd**2.-0.1335*brd+0.1749
+def _mass2_tab2(brd):
+    return -0.0131*brd**4.+0.1090*brd**3.-0.3035*brd**2.+0.2921*brd-5.7976
+def _mass3_tab2(brd):
+    return -0.0048*brd**4.+0.0454*brd**3.-0.1425*brd**2.+0.1012*brd+6.7120
+def _a1_tab2(brd):
+    return -0.0158*brd**4.+0.0993*brd**3.-0.2070*brd**2.-0.7089*brd+0.6445
+def _a2_tab2(brd):
+    return -0.0319*brd**4.+0.1514*brd**3.-0.1279*brd**2.-0.9325*brd+2.6836
+def _a3_tab2(brd):
+    return -0.0326*brd**4.+0.1816*brd**3.-0.2943*brd**2.-0.6329*brd+2.3193
