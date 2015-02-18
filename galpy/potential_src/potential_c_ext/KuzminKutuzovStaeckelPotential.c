132fe9343282d4cef5e3ed8c6dde62f42e7cb0f9
Wilma Trick <trick@mpia.de>
2015-02-18 01:19:56 +0100

    Added amp in output of KuzminKutuzovStaeckelPotential c functions.

--- galpy/potential_src/potential_c_ext/KuzminKutuzovStaeckelPotential.c
+++ galpy/potential_src/potential_c_ext/KuzminKutuzovStaeckelPotential.c
@@ -1,4 +1,3 @@
-# KuzminKutuzovStaeckelPotential.c
 #include <math.h>
 #include <galpy_potentials.h>
 //KuzminKutuzovStaeckelPotential
@@ -12,14 +11,14 @@ double KuzminKutuzovStaeckelPotentialEval(double R,double Z, double phi,
   double ac    = *(args+1);
   double Delta = *(args+2);
   //Coordinate transformation
-  double gamma = Delta*Delta / (1.-ac*ac)
-  double alpha = gamma - Delta*Delta
+  double gamma = Delta*Delta / (1.-ac*ac);
+  double alpha = gamma - Delta*Delta;
   double term  =     R*R + z*z - alpha - gamma;
   double discr = pow(R*R + z*z - Delta*Delta, 2.) + (4. * Delta*Delta * R*R);
   double l     = 0.5 * (term + sqrt(discr)); 
   double n     = 0.5 * (term - sqrt(discr));
   //Calculate potential
-  return - amp /(sqrt(l) + sqrt(n));
+  return -amp /(sqrt(l) + sqrt(n));
 }
 double KuzminKutuzovStaeckelPotentialRforce(double R,double Z, double phi,
 				      double t,
@@ -41,7 +40,7 @@ double KuzminKutuzovStaeckelPotentialRforce(double R,double Z, double phi,
   //Calculate Rforce
   double dVdl = 0.5/sqrt(l)/pow(sqrt(l)+sqrt(n),2.);
   double dVdn = 0.5/sqrt(n)/pow(sqrt(l)+sqrt(n),2.);
-  return -(dldR * dVdl + dndR * dVdn);
+  return -amp * (dldR * dVdl + dndR * dVdn);
 }
 double KuzminKutuzovStaeckelPotentialPlanarRforce(double R,double phi,
 					    double t,
@@ -58,8 +57,8 @@ double KuzminKutuzovStaeckelPotentialPlanarRforce(double R,double phi,
   double n     = -gamma;
   double dldR  = 2.*R;
   //Calculate Rforce
-  double dVdl = 0.5/sqrt(l)/pow(sqrt(l)+sqrt(n),2.)
-  return -dldR * dVdl;
+  double dVdl = 0.5/sqrt(l)/pow(sqrt(l)+sqrt(n),2.);
+  return -amp * dldR * dVdl;
 }
 double KuzminKutuzovStaeckelPotentialzforce(double R,double z,double phi,
 				      double t,
@@ -76,12 +75,12 @@ double KuzminKutuzovStaeckelPotentialzforce(double R,double z,double phi,
   double discr = pow(R*R + z*z - Delta*Delta, 2.) + (4. * Delta*Delta * R*R);
   double l     = 0.5 * (term + sqrt(discr)); 
   double n     = 0.5 * (term - sqrt(discr));
-  double dldz  = z * (1. + (R*R + z*z - Delta*Delta) / sqrt(discr))
-  double dndz  = z * (1. - (R*R + z*z - Delta*Delta) / sqrt(discr))
+  double dldz  = z * (1. + (R*R + z*z - Delta*Delta) / sqrt(discr));
+  double dndz  = z * (1. - (R*R + z*z - Delta*Delta) / sqrt(discr));
   //Calculate zforce
   double dVdl = 0.5/sqrt(l)/pow(sqrt(l)+sqrt(n),2.);
   double dVdn = 0.5/sqrt(n)/pow(sqrt(l)+sqrt(n),2.);
-  return -(dldz * dVdl + dndz * dVdn);
+  return -amp * (dldz * dVdl + dndz * dVdn);
 }
 double KuzminKutuzovStaeckelPotentialPlanarR2deriv(double R,double phi,
 					     double t,
@@ -100,7 +99,7 @@ double KuzminKutuzovStaeckelPotentialPlanarR2deriv(double R,double phi,
   double d2ldR2 = 2.;
   //Calculate R2deriv
   double dVdl   = 0.5/sqrt(l)/pow(sqrt(l)+sqrt(n),2.);
-  double d2Vdl2 = (-3.*sqrt(l)-sqrt(n)) / (4. * pow(l,1.5) * pow(sqrt(l)+sqrt(n),3.)
-  return d2ldR2 * dVdl + dldR*dldr*d2Vdl2
+  double d2Vdl2 = (-3.*sqrt(l)-sqrt(n)) / (4. * pow(l,1.5) * pow(sqrt(l)+sqrt(n),3.);
+  return amp * (d2ldR2 * dVdl + dldR*dldr*d2Vdl2);
 }
 
