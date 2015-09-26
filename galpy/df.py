8f3249904ede8e6ac4fd1398f3d059335a65c8c6
Jo Bovy <bovy@astro.utoronto.ca>
2015-09-25 21:43:48 -0400

    Add impulse functions to top level

--- galpy/df.py
+++ galpy/df.py
@@ -4,7 +4,17 @@ from galpy.df_src import evolveddiskdf
 from galpy.df_src import quasiisothermaldf
 from galpy.df_src import streamdf
 from galpy.df_src import streamgapdf
-
+#
+# Functions
+#
+impulse_deltav_plummer= streamgapdf.impulse_deltav_plummer
+impulse_deltav_plummer_curvedstream= streamgapdf.impulse_deltav_plummer_curvedstream
+impulse_deltav_hernquist= streamgapdf.impulse_deltav_hernquist
+impulse_deltav_hernquist_curvedstream= streamgapdf.impulse_deltav_hernquist_curvedstream
+impulse_deltav_general= streamgapdf.impulse_deltav_general
+impulse_deltav_general_curvedstream= streamgapdf.impulse_deltav_general_curvedstream
+impulse_deltav_general_orbitintegration= streamgapdf.impulse_deltav_general_orbitintegration
+impulse_deltav_general_fullplummerintegration= streamgapdf.impulse_deltav_general_fullplummerintegration
 #
 # Classes
 #
