261b738bb5288b70bf11cfd6ec104d98816d9585
Jo Bovy <bovy@astro.utoronto.ca>
2015-11-18 13:51:39 -0500

    NEMO snapshot reader

--- galpy/snapshot_src/nemo_util.py
+++ galpy/snapshot_src/nemo_util.py
@@ -21,31 +21,31 @@ def read(filename,ext=None):
     """
     if ext is None and filename.split('.')[-1] == 'nemo':
         ext= 'nemo'
+    elif ext is None:
+        ext= 'dat'
     # Convert to ASCII if necessary
     if ext.lower() == 'nemo':
         file_handle, asciifilename= tempfile.mkstemp()
+        os.close(file_handle)
         stderr= open('/dev/null','w')
         try:
-            subprocess.check_call(['s2a',filename],stdout=file_handle,
-                                  stderr=stderr)
+            subprocess.check_call(['s2a',filename,asciifilename])#,stderr=stderr)
         except subprocess.CalledProcessError:
-            file_handle.close()
             os.remove(asciifilename)
-        else:
-            file_handle.close()
         finally:
             stderr.close()
     else:
         asciifilename= filename
     # Now read
-    out= numpy.loadtxt(asciifilename,comment='#')
+    out= numpy.loadtxt(asciifilename,comments='#')
+    if ext.lower() == 'nemo': os.remove(asciifilename)
     # Get the number of snapshots
-    nt= (_wc(asciifilename)-numpy.prod(out.shape))//13 # 13 comments/snapshot
+    nt= (_wc(asciifilename)-out.shape[0])//13 # 13 comments/snapshot
     out= numpy.reshape(out,(nt,out.shape[0]//nt,out.shape[1]))
-    return out
+    return numpy.swapaxes(numpy.swapaxes(out,0,1),1,2)
 
 def _wc(filename):
     try:
-        return subprocess.check_output(['wc','-l',filename])
+        return int(subprocess.check_output(['wc','-l',filename]).split()[0])
     except subprocess.CalledProcessError:
         return numpy.nan
