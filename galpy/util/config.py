7eeb4c122cf019ce8a9159d4efc5cdbd6897c8e4
Jo Bovy <bovy@astro.utoronto.ca>
2016-01-04 10:26:51 -0500

    Add distance and velocity scales to configuration; allow them to be set on the flu

--- galpy/util/config.py
+++ galpy/util/config.py
@@ -4,11 +4,18 @@ try:
 except:
     from six.moves import configparser
 # The default configuration
-default_configuration= {'astropy-units':'False'}
+default_configuration= {'astropy-units':'False',
+                        'ro':'8.',
+                        'vo':'220.'}
 default_filename= os.path.join(os.path.expanduser('~'),'.galpyrc')
 def write_default(filename):
     writeconfig= configparser.ConfigParser()
     # Write different sections
+    writeconfig.add_section('normalization')
+    writeconfig.set('normalization','ro',
+                    default_configuration['ro'])
+    writeconfig.set('normalization','vo',
+                    default_configuration['vo'])
     writeconfig.add_section('astropy')
     writeconfig.set('astropy','astropy-units',
                     default_configuration['astropy-units'])
@@ -21,3 +28,9 @@ __config__= configparser.ConfigParser(default_configuration)
 if __config__.read([default_filename,'.galpyrc']) == []:
     write_default(default_filename)
     __config__.read(default_filename)
+
+# Set configuration variables on the fly
+def set_ro(ro):
+    __config__.set('normalization','ro',str(ro))
+def set_vo(vo):
+    __config__.set('normalization','vo',str(vo))
