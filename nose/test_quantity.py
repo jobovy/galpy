a0a6e9eda2a62bb2c5793e1259c5f742df5415f3
Jo Bovy <bovy@astro.utoronto.ca>
2016-01-04 22:47:54 -0500

    Tests of orbit initialization with uvw as Quantities

--- nose/test_quantity.py
+++ nose/test_quantity.py
@@ -30,3 +30,29 @@ def test_orbit_setup_radec_oddunits():
     assert numpy.fabs(o.vlos(use_physical=False)-130./1.0227121655399913) < 10.**-5., 'Orbit initialization with vlos as Quantity does not work as expected'
     return None
 
+def test_orbit_setup_radec_uvw():
+    from galpy.orbit import Orbit
+    o= Orbit([1.*units.rad,-0.25*units.rad,3000.*units.pc,
+              -30.*units.km/units.s,20.*units.km/units.s,
+              130.*units.km/units.s],radec=True,uvw=True)
+    assert numpy.fabs(o.ra(use_physical=False)-1./numpy.pi*180.) < 10.**-8., 'Orbit initialization with RA as Quantity does not work as expected'
+    assert numpy.fabs(o.dec(use_physical=False)+.25/numpy.pi*180.) < 10.**-8., 'Orbit initialization with Dec as Quantity does not work as expected'
+    assert numpy.fabs(o.dist(use_physical=False)-3.) < 10.**-8., 'Orbit initialization with distance as Quantity does not work as expected'
+    assert numpy.fabs(o.U(use_physical=False)+30.) < 10.**-8., 'Orbit initialization with pmra as Quantity does not work as expected'
+    assert numpy.fabs(o.V(use_physical=False)-20.) < 10.**-8., 'Orbit initialization with pmdec as Quantity does not work as expected'
+    assert numpy.fabs(o.W(use_physical=False)-130.) < 10.**-8., 'Orbit initialization with vlos as Quantity does not work as expected'
+    return None
+
+def test_orbit_setup_radec_uvw_oddunits():
+    from galpy.orbit import Orbit
+    o= Orbit([1.*units.rad,-0.25*units.rad,3000.*units.pc,
+              -30.*units.pc/units.Myr,20.*units.pc/units.Myr,
+              130.*units.pc/units.Myr],radec=True,uvw=True)
+    assert numpy.fabs(o.ra(use_physical=False)-1./numpy.pi*180.) < 10.**-8., 'Orbit initialization with RA as Quantity does not work as expected'
+    assert numpy.fabs(o.dec(use_physical=False)+.25/numpy.pi*180.) < 10.**-8., 'Orbit initialization with Dec as Quantity does not work as expected'
+    assert numpy.fabs(o.dist(use_physical=False)-3.) < 10.**-8., 'Orbit initialization with distance as Quantity does not work as expected'
+    assert numpy.fabs(o.U(use_physical=False)+30./1.0227121655399913) < 10.**-5., 'Orbit initialization with pmra as Quantity does not work as expected'
+    assert numpy.fabs(o.V(use_physical=False)-20./1.0227121655399913) < 10.**-5., 'Orbit initialization with pmdec as Quantity does not work as expected'
+    assert numpy.fabs(o.W(use_physical=False)-130./1.0227121655399913) < 10.**-5., 'Orbit initialization with vlos as Quantity does not work as expected'
+    return None
+
