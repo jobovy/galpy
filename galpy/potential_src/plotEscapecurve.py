8b1462e56a5faae2e2ae79e6dd63c184d4e716d2
Jo Bovy <bovy@astro.utoronto.ca>
2016-01-19 11:53:06 -0500

    Plot escape-velocity curve in physical coordinates

--- galpy/potential_src/plotEscapecurve.py
+++ galpy/potential_src/plotEscapecurve.py
@@ -44,16 +44,45 @@ def plotEscapecurve(Pot,*args,**kwargs):
        2010-08-08 - Written - Bovy (NYU)
 
     """
-    Rrange= kwargs.pop('Rrange',[0.01,5.])
+    # Using physical units or not?
+    if isinstance(Pot,list):
+        potro= Pot[0]._ro
+        roSet= Pot[0]._roSet
+        potvo= Pot[0]._vo
+        voSet= Pot[0]._voSet
+    else:
+        potro= Pot._ro
+        roSet= Pot._roSet
+        potvo= Pot._vo
+        voSet= Pot._voSet
+    if (kwargs.get('use_physical',False) \
+            and kwargs.get('ro',roSet) and kwargs.get('vo',voSet)) or \
+            (not 'use_physical' in kwargs \
+                 and kwargs.get('ro',roSet) and kwargs.get('vo',voSet)):
+        use_physical= True
+        potro= kwargs.get('ro',potro)
+        potvo= kwargs.get('vo',potvo)
+        xlabel= r'$R\,(\mathrm{kpc})$'
+        ylabel= r"$v_e(R)\,(\mathrm{km\,s}^{-1})$"
+        Rrange= kwargs.pop('Rrange',[0.01*potro,5.*potro])
+    else:
+        use_physical= False
+        xlabel= r"$R/R_0$"
+        ylabel= r"$v_e(R)/v_c(R_0)$"
+        Rrange= kwargs.pop('Rrange',[0.01,5.])
+    # Parse ro
     if _APY_LOADED:
-        if hasattr(Pot,'_ro'):
-            tro= Pot._ro
-        else:
-            tro= Pot[0]._ro
+        if isinstance(potro,units.Quantity):
+            potro= potro.to(units.kpc).value
+        if isinstance(potvo,units.Quantity):
+            potvo= potvo.to(units.km/units.s).value
         if isinstance(Rrange[0],units.Quantity):
-            Rrange[0]= Rrange[0].to(units.kpc).value/tro
+            Rrange[0]= Rrange[0].to(units.kpc).value
         if isinstance(Rrange[1],units.Quantity):
-            Rrange[1]= Rrange[1].to(units.kpc).value/tro
+            Rrange[1]= Rrange[1].to(units.kpc).value
+    if use_physical:
+        Rrange[0]/= potro
+        Rrange[1]/= potro
     grid= kwargs.pop('grid',1001)
     savefilename= kwargs.pop('savefilename',None)
     if not savefilename == None and os.path.exists(savefilename):
@@ -71,14 +100,23 @@ def plotEscapecurve(Pot,*args,**kwargs):
             pickle.dump(esccurve,savefile)
             pickle.dump(Rs,savefile)
             savefile.close()
+    if use_physical:
+        Rs*= potro
+        esccurve*= potvo
+        Rrange[0]*= potro
+        Rrange[1]*= potro
     if not 'xlabel' in kwargs:
-        kwargs['xlabel']= r"$R/R_0$"
+        kwargs['xlabel']= xlabel
     if not 'ylabel' in kwargs:
-        kwargs['ylabel']= r"$v_e(R)/v_c(R_0)$"
+        kwargs['ylabel']= ylabel
     if not 'xrange' in kwargs:
         kwargs['xrange']= Rrange
     if not 'yrange' in kwargs:
-        kwargs['yrange']= [0.,1.2*nu.amax(esccurve)]
+        kwargs['yrange']=\
+            [0.,1.2*nu.amax(esccurve[True-nu.isnan(esccurve)])]
+    kwargs.pop('ro',None)
+    kwargs.pop('vo',None)
+    kwargs.pop('use_physical',None)
     return plot.bovy_plot(Rs,esccurve,*args,
                           **kwargs)
 
