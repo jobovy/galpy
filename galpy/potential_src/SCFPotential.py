8fec523f1b1cbcb2ec02426fdd0553575803dab6
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-05-17 19:43:28 -0400

    Removed _P and _C and replaced them with scipy's legendre and gegenbauer function

--- galpy/potential_src/SCFPotential.py
+++ galpy/potential_src/SCFPotential.py
@@ -5,6 +5,7 @@ if _APY_LOADED:
     from astropy import units
     
 from galpy.util import bovy_coords
+from scipy.special import eval_gegenbauer, lpmn
 
 class SCFPotential(Potential):
    
@@ -53,58 +54,7 @@ class SCFPotential(Potential):
         
         return None
 
-    def _C(self,xi, L, N):
-        """
-        NAME:
-           _C
-        PURPOSE:
-           Evaluate C_n,l (the Gegenbauer polynomial) for 0 <= l < L and 0<= n < N 
-        INPUT:
-           xi - radial transformed variable
-           L - Size of the L dimension
-           N - Size of the N dimension
-        OUTPUT:
-           An LxN Gegenbauer Polynomial 
-        HISTORY:
-           2016-05-16 - Written - Aladdin 
-        """
-        fact = nu.math.factorial
-        CC = nu.zeros((N,L), float) 
-        for l in range(L):
-            for n in range(N - 1):
-                alpha = 2*l + 3./2.
-                if n==0:
-                    CC[n][l] = 1
-                    continue
-                elif n==1: CC[n][l] = 2*alpha*xi
-                CC[n+1][l] = (n + 1)**-1. * (2*(n + alpha)*xi*CC[n][l] - (n + 2*alpha - 1)*CC[n-1][l])
-        return CC
-    
-    def _P(self, x, L):
-        """
-        NAME:
-           _P
-        PURPOSE:
-           Evaluate P_l,m (the Legendre polynomial) for 0 <= l < L and 0<= m <  L 
-        INPUT:
-           x - point that the polynomial will evaluate on
-           L - Size of the L dimension
-        OUTPUT:
-           An LxL Legendre Polynomial 
-        HISTORY:
-           2016-05-16 - Written - Aladdin 
-        """
-        def fact(n):
-            if n <=1: return 1.
-            else: return n*fact(n-2)
-        PP = nu.zeros((L,L), float)
-        for l in range(L):
-            for m in range(l + 1):
-                if l == m: PP[l][m] = (-1.)**m *fact(2*m - 1)*(1. - x**2)**(m/2.)
-                elif l - 1 == m: PP[l][m] = x*(2*m + 1.)*PP[m][m]
-                else: PP[l][m] = (l -m)**-1. * (x*(2*l - 1.)*PP[l-1,m]- (l + m - 1.)*PP[l-2, m])
-        return PP
-    
+ 
     def _Nroot(self, L):
         """
         NAME:
@@ -141,12 +91,11 @@ class SCFPotential(Potential):
            2016-05-17 - Written - Aladdin 
         """
         xi = (1. - r)/(1. + r)
-        CC = self._C(xi, L, N)
         rho = nu.zeros((N,L), float)
         for n in range(N):
             for l in range(L):
                 K = 0.5 * n * (n + 4*l + 3) + (l + 1)*(2*l + 1)
-                rho[n][l] = K/(2*nu.pi) * (r**l)/ (r*(1 + r)**(2*l + 3)) * CC[n][l]* (4*nu.pi)**0.5
+                rho[n][l] = K/(2*nu.pi) * (r**l)/ (r*(1 + r)**(2*l + 3)) * eval_gegenbauer(n,2*l + 3./2, xi)* (4*nu.pi)**0.5
         return rho   
     def _phiTilde(self, r, N,L):
         """
@@ -164,11 +113,10 @@ class SCFPotential(Potential):
            2016-05-17 - Written - Aladdin 
         """
         xi = (1. - r)/(1. + r)
-        CC = self._C(xi, L, N)
         phi = nu.zeros((N,L), float)
         for n in range(N):
             for l in range(L):
-                phi[n][l] = - (r**l)/ ((1 + r)**(2*l + 1)) * CC[n][l]* (4*nu.pi)**0.5
+                phi[n][l] = - (r**l)/ ((1 + r)**(2*l + 1)) * eval_gegenbauer(n,2*l + 3./2, xi)* (4*nu.pi)**0.5
         return phi  
         
     def _dens(self, R, z, phi=0., t=0.):
@@ -192,7 +140,7 @@ class SCFPotential(Potential):
         r, theta, phi = bovy_coords.cyl_to_spher(R,phi,z)
         NN = self._Nroot(L)
 
-        PP = self._P(nu.cos(theta),L)
+        PP = lpmn(L,L,nu.cos(theta))[0]
         rho = nu.zeros((N,L,L), float) ## rho_n,l,m
         rho_tilde = self._rhoTilde(r, N, L) ##tilde rho_n,l
         def coeff(l,m):
@@ -225,7 +173,7 @@ class SCFPotential(Potential):
         r, theta, phi = bovy_coords.cyl_to_spher(R,phi,z)
         N, L, M = Areal.shape
         NN = self._Nroot(L)
-        PP = self._P(nu.cos(theta),L)
+        PP = lpmn(L,L,nu.cos(theta))
         Phi = nu.zeros((N,L,L), float) ## rho_n,l,m
         ##TODO
         Phi_tilde = self._phiTilde(r, N, L) ##tilde rho_n,l
