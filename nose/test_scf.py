8c6767483bf7329bd4a95e9402d9ed1c78a87ac4
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-06-13 20:10:57 -0400

    Added NFW tests

--- nose/test_scf.py
+++ nose/test_scf.py
@@ -33,6 +33,40 @@ def test_scf_compute_spherical_zeeuw():
     assert numpy.fabs(Acos[0,0,0] - 2*3./4) < EPS, "Acos(n=0,l=0,m=0) = 3/2 fails. Found to be Acos(n=0,l=0,m=0) = {0}".format(Acos[0,0,0])
     assert numpy.fabs(Acos[1,0,0] - 2*1./12) < EPS, "Acos(n=1,l=0,m=0) = 1/6 fails. Found to be Acos(n=0,l=0,m=0) = {0}".format(Acos[0,0,0])
  
+##Tests that the numerically calculated results from axi_density1 matches with the analytic results
+def test_scf_compute_axi_density1():
+    A = potential.scf_compute_coeffs_axi(axi_density1, 10,10)
+    axi_coeffsTest(A[0], A[1])
+    analytically_calculated = numpy.array([[4./3, 7.* 3**(-5/2.), 2*11*5**(-5./2), 0],
+                                            [0, 0,0,0],
+                                            [0,11. / (3.**(5./2) * 5 * 7. * 2), 1. / (2*3.*5**.5*7.), 0]])
+    numerically_calculated = A[0][:3,:4,0]
+    shape = numerically_calculated.shape
+    for n in range(shape[0]):
+        for l in range(shape[1]):
+            assert numpy.fabs(numerically_calculated[n,l] - analytically_calculated[n,l]) < EPS, \
+        "Acos(n={0},l={1},0) = {2}, whereas it was analytically calculated to be {3}".format(n,l, numerically_calculated[n,l], analytically_calculated[n,l])
+
+
+##Tests that the numerically calculated results from axi_density2 matches with the analytic results
+def test_scf_compute_axi_density2():
+    A = potential.scf_compute_coeffs_axi(axi_density2, 10,10)
+    axi_coeffsTest(A[0], A[1])
+    analytically_calculated = 2*numpy.array([[1., 7.* 3**(-3/2.) /4., 3*11*5**(-5./2)/2., 0],
+                                            [0,0,0,0], ##I never did analytically solve for n=1
+                                            [0, 11./(7*5*3**(3./2)*2**(3.)), (7 * 5**(.5)*2**3.)**-1., 0]])
+    numerically_calculated = A[0][:3,:4,0]
+    shape = numerically_calculated.shape
+    for n in range(shape[0]):
+        if n ==1: continue 
+        for l in range(shape[1]):
+            assert numpy.fabs(numerically_calculated[n,l] - analytically_calculated[n,l]) < EPS, \
+        "Acos(n={0},l={1},0) = {2}, whereas it was analytically calculated to be {3}".format(n,l, numerically_calculated[n,l], analytically_calculated[n,l])
+ 
+def test_scf_compute_nfw(): 
+    Acos, Asin = potential.scf_compute_coeffs_spherical(rho_NFW, 10)
+    spherical_coeffsTest(Acos, Asin)
+
 ## Tests whether scf_compute_axi reduces to scf_compute_spherical for the Hernquist Potential   
 def test_scf_axiHernquistCoeffs_ReducesToSpherical():
     Aspherical = potential.scf_compute_coeffs_spherical(sphericalHernquistDensity, 10)
@@ -85,6 +119,14 @@ def test_densMatches_axi_density2():
     scf = SCFPotential(amp=1, Acos=Acos, Asin=Asin)
     assertmsg = "Comparing axi_density2 with SCF fails at R={0}, Z={1}, phi={2}"
     compareFunctions(axi_density2,scf.dens, assertmsg) 
+    
+    
+## Tests whether scf density matches with NFW 
+def test_densMatches_nfw():
+    Acos, Asin = potential.scf_compute_coeffs_spherical(rho_NFW,10)
+    scf = SCFPotential(amp=1, Acos=Acos, Asin=Asin)
+    assertmsg = "Comparing nfw with SCF fails at R={0}, Z={1}, phi={2}"
+    compareFunctions(rho_NFW,scf.dens, assertmsg) 
 
 ## Tests whether scf potential matches with Hernquist potential
 def test_potentialMatches_hernquist():
@@ -94,7 +136,14 @@ def test_potentialMatches_hernquist():
     assertmsg = "Comparing the potential of Hernquist Potential with SCF fails at R={0}, Z={1}, phi={2}"
     compareFunctions(h,scf, assertmsg)
   
-  
+## Tests whether scf Potential matches with NFW 
+def test_densMatches_nfw():
+    nfw = potential.NFWPotential()
+    Acos, Asin = potential.scf_compute_coeffs_spherical(rho_NFW,10)
+    scf = SCFPotential(amp=1, Acos=Acos, Asin=Asin)
+    assertmsg = "Comparing nfw with SCF fails at R={0}, Z={1}, phi={2}"
+    compareFunctions(nfw,scf, assertmsg) 
+      
 ## Tests whether scf Rforce matches with Hernquist Rforce
 def test_RforceMatches_hernquist():
     h = potential.HernquistPotential()
@@ -119,6 +168,31 @@ def test_phiforceMatches_hernquist():
     scf = SCFPotential()
     assertmsg = "Comparing the azimuth force of Hernquist Potential with SCF fails at R={0}, Z={1}, phi={2}"
     compareFunctions(h.phiforce,scf.phiforce, assertmsg)
+    
+    
+## Tests whether scf Rforce matches with NFW Rforce
+def test_RforceMatches_nfw():
+    nfw = potential.NFWPotential()
+    Acos, Asin = potential.scf_compute_coeffs_spherical(rho_NFW,10)
+    scf = SCFPotential()
+    assertmsg = "Comparing the radial force of NFW Potential with SCF fails at R={0}, Z={1}, phi={2}"
+    compareFunctions(nfw.Rforce,scf.Rforce, assertmsg)
+      
+## Tests whether scf zforce matches with NFW Rforce
+def test_zforceMatches_nfw():
+    nfw = potential.NFWPotential()
+    Acos, Asin = potential.scf_compute_coeffs_spherical(rho_NFW,10)
+    scf = SCFPotential()
+    assertmsg = "Comparing the vertical force of NFW Potential with SCF fails at R={0}, Z={1}, phi={2}"
+    compareFunctions(nfw.zforce,scf.zforce, assertmsg)
+    
+## Tests whether scf phiforce matches with NFW Rforce
+def test_phiforceMatches_nfw():
+    nfw = potential.NFWPotential()
+    Acos, Asin = potential.scf_compute_coeffs_spherical(rho_NFW,10)
+    scf = SCFPotential()
+    assertmsg = "Comparing the azimuth force of NFW Potential with SCF fails at R={0}, Z={1}, phi={2}"
+    compareFunctions(nfw.phiforce,scf.phiforce, assertmsg)
 
 ## Checks Energy conservation for the SCF Hernquist Potential    
 def test_scfHernquist_energyConserved():
@@ -141,38 +215,12 @@ def test_scfaxi_density2_energyConserved():
     Acos, Asin = potential.scf_compute_coeffs_axi(axi_density2,10,10)
     energyConservationTest(Acos, Asin, "axi_density2")
   
+def test_scfNFW_energyConserved():
+    Acos, Asin = potential.scf_compute_coeffs_spherical(rho_NFW,10)
+    energyConservationTest(Acos, Asin, "NFW")
+    
+    
 
-##Tests that the numerically calculated results from axi_density1 matches with the analytic results
-def test_scf_compute_axi_density1():
-    A = potential.scf_compute_coeffs_axi(axi_density1, 10,10)
-    axi_coeffsTest(A[0], A[1])
-    analytically_calculated = numpy.array([[4./3, 7.* 3**(-5/2.), 2*11*5**(-5./2), 0],
-                                            [0, 0,0,0],
-                                            [0,11. / (3.**(5./2) * 5 * 7. * 2), 1. / (2*3.*5**.5*7.), 0]])
-    numerically_calculated = A[0][:3,:4,0]
-    shape = numerically_calculated.shape
-    for n in range(shape[0]):
-        for l in range(shape[1]):
-            assert numpy.fabs(numerically_calculated[n,l] - analytically_calculated[n,l]) < EPS, \
-        "Acos(n={0},l={1},0) = {2}, whereas it was analytically calculated to be {3}".format(n,l, numerically_calculated[n,l], analytically_calculated[n,l])
-
-
-##Tests that the numerically calculated results from axi_density2 matches with the analytic results
-def test_scf_compute_axi_density2():
-    A = potential.scf_compute_coeffs_axi(axi_density2, 10,10)
-    axi_coeffsTest(A[0], A[1])
-    analytically_calculated = 2*numpy.array([[1., 7.* 3**(-3/2.) /4., 3*11*5**(-5./2)/2., 0],
-                                            [0,0,0,0], ##I never did analytically solve for n=1
-                                            [0, 11./(7*5*3**(3./2)*2**(3.)), (7 * 5**(.5)*2**3.)**-1., 0]])
-    numerically_calculated = A[0][:3,:4,0]
-    shape = numerically_calculated.shape
-    for n in range(shape[0]):
-        if n ==1: continue 
-        for l in range(shape[1]):
-            assert numpy.fabs(numerically_calculated[n,l] - analytically_calculated[n,l]) < EPS, \
-        "Acos(n={0},l={1},0) = {2}, whereas it was analytically calculated to be {3}".format(n,l, numerically_calculated[n,l], analytically_calculated[n,l])
- 
- 
  
 ##############GENERIC FUNCTIONS BELOW###############
 
@@ -241,6 +289,10 @@ def sphericalHernquistDensity(R, z=0, phi=0):
 def rho_Zeeuw(R, z=0., phi=0., a=1.):
     r, theta, phi = bovy_coords.cyl_to_spher(R,z, phi)
     return 3./(4*numpy.pi) * numpy.power((a + r),-4.) * a
+    
+def rho_NFW(R, z=0, phi=0.):
+    nfw = potential.NFWPotential()
+    return nfw.dens(R,z, phi)
     
     
 def axi_density1(R, z=0, phi=0.):
