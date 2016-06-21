1fb8d548156d1873cad463f0d1f09a0b5eb3a0fe
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-06-20 20:50:51 -0400

    Made changes on how Acos and Asin are assigned.

--- galpy/potential_src/potential_c_ext/SCFPotential.c
+++ galpy/potential_src/potential_c_ext/SCFPotential.c
@@ -1,5 +1,7 @@
 #include <math.h>
 #include <galpy_potentials.h>
+#include <stdio.h>
+
 //SCF Disk potential
 //4 arguments: amp, Acos, Asin, a
 
@@ -10,10 +12,30 @@ double SCFPotentialRforce(double R,double Z, double phi,
   double * args= potentialArgs->args;
   //Get args
   double amp= *args++;
-  double Acos= *args++;
-  double Asin= *args++;
-  double a= *args;
-  //Calculate Rforce
+  double a = *args++;
+  int N = *args++;
+  int L = *args++;
+  int M = *args++;
+  double Acos[N][L][M];
+  double Asin[N][L][M];
+  
+  // Get Acos
+  for (int n=0; n <N;n= n+1){
+     for (int l=0; l <L;l= l+1){
+      for (int m=0; m <M;m= m+1){
+      Acos[n][l][m] = *args++;
+  }
+  } 
+  }
+  // Get Asin
+  for (int n=0; n <N;n= n+1){
+     for (int l=0; l <L;l= l+1){
+      for (int m=0; m <M;m= m+1){
+      Asin[n][l][m] = *args++;
+  }
+  } 
+  }
+
  return 1.;
 }
 
@@ -23,9 +45,30 @@ double SCFPotentialzforce(double R,double Z, double phi,
   double * args= potentialArgs->args;
   //Get args
   double amp= *args++;
-  double Acos= *args++;
-  double Asin= *args++;
-  double a= *args;
+  double a = *args++;
+  int N = *args++;
+  int L = *args++;
+  int M = *args++;
+  double Acos[N][L][M];
+  double Asin[N][L][M];
+  
+  // Get Acos
+  for (int n=0; n <N;n= n+1){
+     for (int l=0; l <L;l= l+1){
+      for (int m=0; m <M;m= m+1){
+      Acos[n][l][m] = *args++;
+  }
+  } 
+  }
+  // Get Asin
+  for (int n=0; n <N;n= n+1){
+     for (int l=0; l <L;l= l+1){
+      for (int m=0; m <M;m= m+1){
+      Asin[n][l][m] = *args++;
+  }
+  } 
+  }
+  
   //Calculate zforce
   return 1.;
 }
@@ -36,9 +79,29 @@ double SCFPotentialphiforce(double R,double Z, double phi,
   double * args= potentialArgs->args;
   //Get args
   double amp= *args++;
-  double Acos= *args++;
-  double Asin= *args++;
-  double a= *args;
+  double a = *args++;
+  int N = *args++;
+  int L = *args++;
+  int M = *args++;
+  double Acos[N][L][M];
+  double Asin[N][L][M];
+  
+  // Get Acos
+  for (int n=0; n <N;n= n+1){
+     for (int l=0; l <L;l= l+1){
+      for (int m=0; m <M;m= m+1){
+      Acos[n][l][m] = *args++;
+  }
+  } 
+  }
+  // Get Asin
+  for (int n=0; n <N;n= n+1){
+     for (int l=0; l <L;l= l+1){
+      for (int m=0; m <M;m= m+1){
+      Asin[n][l][m] = *args++;
+  }
+  } 
+  }
   //Calculate phiforce
   
   return 1.;
