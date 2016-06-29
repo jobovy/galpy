0d1de3a82eb582253f3c75508f22eba39cbe2d31
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-06-29 14:51:11 -0400

    Removed Forces and derivatives from computePlanarderiv that's not used.

--- galpy/potential_src/potential_c_ext/SCFPotential.c
+++ galpy/potential_src/potential_c_ext/SCFPotential.c
@@ -59,16 +59,20 @@ inline void compute_dphiTilde(double r, double a, int N, int L, double * C, doub
 inline void compute_d2phiTilde(double r, double a, int N, int L, double * C, double * dC,double * d2C, double * d2phiTilde){
     double xi;
     calculateXi(r, a, &xi);
-    double rterm = 1./(r*r) / ((a + r)*(a + r)*(a + r));
+    double rterm = -1./(r*r) / ((a + r)*(a + r)*(a + r));
 
     for (int l = 0; l < L; l++){
         if (l != 0){
             rterm *= (a*r)/((a + r)*(a + r));
             }
         for (int n = 0; n < N; n++){
-           *( d2phiTilde + l*N + n) = rterm*(*(C + l*N + n)*(a*a * (l - 1)*l - 2*a*l*(l + 2)*r + (l+1)*(l+2)*r*r) +
-            r*(a + r)*((*(dC + l*N + n))*(2*(a*l - (l + 1)*r)*(2*a)/((a + r)*(a + r)) - 4*r/((a + r)*(a + r))) +
-             4*a*a*r/((a + r)*(a + r)*(a + r))*(*(d2C + l*N + n)))) ;
+            double C_val = *(C + l*N + n);
+            double dC_val = *(dC + l*N + n);
+            double d2C_val = *(d2C + l*N + n);
+           *( d2phiTilde + l*N + n) = rterm*(C_val*(a*a * (l - 1)*l - 2*a*l*(l + 2)*r + (l+1)*(l+2)*r*r) +
+            r*(a + r)*(dC_val*(2*(a*l - (l + 1)*r)*(2*a)/((r + a)*(r + a)) - 4*r*a/((r + a)*(r + a))) +
+             4*a*a*r/((r + a)*(r + a)*(r + a))*d2C_val)) ;
+             
         }
     }
 
@@ -120,7 +124,6 @@ inline void computePlanarderiv(double a, int N, int L, int M,
                         double *Acos, double *Asin,
                         double r, double theta, double phi,
                         double *Farray){
-                                printf("In Function");
                                 double xi;
                                 calculateXi(r, a, &xi);
     
@@ -132,7 +135,6 @@ inline void computePlanarderiv(double a, int N, int L, int M,
                                 compute_C(xi, N, L, &C);
                                 compute_dC(xi, N, L, &dC);
                                 compute_d2C(xi, N, L, &d2C);
-                                printf("Success C");
                                 
                                 //Compute phiTilde and its derivative
                                 double phiTilde[L*N];
@@ -171,6 +173,7 @@ inline void computePlanarderiv(double a, int N, int L, int M,
                                         
                                         double Acos_val = *(Acos +m + M*l + M*L*n);
                                         double Asin_val = *(Asin +m + M*l + M*L*n);
+                                        /*
                                             F_r -= (Acos_val*mCos + Asin_val*mSin)*
                                                             P[m*L + l]*dphiTilde[l*N + n];
                                                             
@@ -180,44 +183,46 @@ inline void computePlanarderiv(double a, int N, int L, int M,
                                             F_phi -= m*(Asin_val*mCos - Acos_val*mSin)*
                                                             P[m*L + l]*phiTilde[l*N + n];
                                                             
+                                            */
                                             F_rr -= (Acos_val*mCos + Asin_val*mSin)*
                                                             P[m*L + l]*d2phiTilde[l*N + n];
-                                            
+                                            /*
                                             F_thetatheta -= (Acos_val*mCos + Asin_val*mSin)*
                                                             (dP[m*L + l]*phiTilde[l*N + n]*(-cos(theta)) + 
                                                             d2P[m*L + l]*phiTilde[l*N + n]*(sin(theta)*sin(theta)));
+                                            */                                            
                                             //F_phiphi
                                             F_phiphi += m*m*(Acos_val*mCos + Asin_val*mSin)*
                                                             P[m*L + l]*phiTilde[l*N + n];
-                                            
+                                            /*
                                             //F_rtheta
                                             F_rtheta += (Acos_val*mCos + Asin_val*mSin)*
                                                             dP[m*L + l]*dphiTilde[l*N + n]*(sin(theta));
-                                            
+                                            */
                                             //TODO F_rphi
                                             F_rphi -= m*(Asin_val*mCos - Acos_val*mSin)*
                                                             P[m*L + l]*dphiTilde[l*N + n];
-                                            
+                                            /*
                                             //F_thetaphi
                                             F_thetaphi -= m*(Asin_val*mCos - Acos_val*mSin)*
                                                             dP[m*L + l]*phiTilde[l*N + n]*(-sin(theta));
-                                          
+                                            */
                                             
                                         }                                    
                                     }     
                             }
                               
-                            *(Farray + 0) = F_r*sqrt(4*M_PI);
-                            *(Farray + 1) = F_theta*sqrt(4*M_PI);
-                            *(Farray + 2) = F_phi*sqrt(4*M_PI);
+                            //*(Farray + 0) = F_r*sqrt(4*M_PI);
+                            //*(Farray + 1) = F_theta*sqrt(4*M_PI);
+                            //*(Farray + 2) = F_phi*sqrt(4*M_PI);
                             
                             *(Farray + 3) = F_rr*sqrt(4*M_PI);
-                            *(Farray + 4) = F_thetatheta*sqrt(4*M_PI);
+                            //*(Farray + 4) = F_thetatheta*sqrt(4*M_PI);
                             *(Farray + 5) = F_phiphi*sqrt(4*M_PI);
                             
-                            *(Farray + 6) = F_rtheta*sqrt(4*M_PI);
+                            //*(Farray + 6) = F_rtheta*sqrt(4*M_PI);
                             *(Farray + 7) = F_rphi*sqrt(4*M_PI);
-                            *(Farray + 8) = F_thetaphi*sqrt(4*M_PI);
+                            //*(Farray + 8) = F_thetaphi*sqrt(4*M_PI);
 
                            
                         }
@@ -268,8 +273,8 @@ inline double computeForce(double a, int N, int L, int M,
                                             F_r -= (Acos_val*mCos + Asin_val*mSin)*
                                                             P[m*L + l]*dphiTilde[l*N + n];
                                                             
-                                            F_theta -= (Acos_val*mCos + Asin_val*mSin)*
-                                                            dP[m*L + l]*phiTilde[l*N + n]*(-sin(theta));
+                                            F_theta += (Acos_val*mCos + Asin_val*mSin)*
+                                                            dP[m*L + l]*phiTilde[l*N + n]*(sin(theta));
                                                             
                                             F_phi -= m*(Asin_val*mCos - Acos_val*mSin)*
                                                             P[m*L + l]*phiTilde[l*N + n];
@@ -291,7 +296,6 @@ inline double computeForce(double a, int N, int L, int M,
 double SCFPotentialEval(double R,double Z, double phi,
 				double t,
 				struct potentialArg * potentialArgs){
-				    
   double * args= potentialArgs->args;
   //Get args
   double a = *args++;
@@ -360,7 +364,7 @@ double SCFPotentialRforce(double R,double Z, double phi,
   double dr_dR = R/r; 
   double dtheta_dR = Z/(r*r);
   double dphi_dR = 0;
-  
+
  return computeForce(a, N, L, M, Acos, Asin, dr_dR,dtheta_dR, dphi_dR, r, theta, phi);
 }
 
@@ -435,20 +439,11 @@ double SCFPotentialPlanarR2deriv(double R, double Z, double phi,
   double* Asin = args + N*L*M;
   double r;
   double theta;
-  cyl_to_spher(R, 0, &r, &theta);
-  
-  double dr_dR = 1;
-  double dtheta_dR = 0;
-  double dphi_dR = 0;
-  double d2r_d2R = 0;
-  double d2theta_d2R = 0;
-  double d2phi_d2R = 0;
-  
+  cyl_to_spher(R, 0, &r, &theta);  
   double Farray[9];
-  printf("Calling Function");
  computePlanarderiv(a, N, L, M, Acos, Asin, r, theta, phi, &Farray);
  
-    return dr_dR*dr_dR*Farray[3];
+    return Farray[3];
  
                                                         
    }
@@ -468,19 +463,13 @@ double SCFPotentialPlanarphi2deriv(double R, double Z, double phi,
   double r;
   double theta;
   cyl_to_spher(R, 0, &r, &theta);
-  
-  double dr_dphi = 0;
-  double dtheta_dphi = 0;
-  double dphi_dphi = 1;
-  double d2r_d2phi = 0;
-  double d2theta_d2phi = 0;
-  double d2phi_d2phi = 0;
+
   
   double Farray[9];
   
  computePlanarderiv(a, N, L, M, Acos, Asin, r, theta, phi, &Farray);
  
-    return dphi_dphi*dphi_dphi*Farray[5];
+    return Farray[5];
  
                                                         
    }
@@ -500,30 +489,10 @@ double SCFPotentialPlanarRphideriv(double R, double Z, double phi,
   double theta;
   cyl_to_spher(R, 0, &r, &theta);
   
-  double dr_dR = 1;
-  double dtheta_dR = 0;
-  double dphi_dR = 0;
-  
-  double dr_dZ= 0;
-  double dtheta_dZ = -R;
-  double dphi_dZ = 0;
-
-  
-  double dr_dphi = 0;
-  double dtheta_dphi = 0;
-  double dphi_dphi = 1;
-  
-  double d2r_dRphi = 0;
-  double d2theta_dRphi = 0;
-  double d2phi_dRphi = 0;
-
-  
   
   double F[9];
   
   computePlanarderiv(a, N, L, M, Acos, Asin, r, theta, phi, &F);
- 
-    return F[7]*(dr_dR*dphi_dphi);
-        
+    return F[7];
                                                         
    }
