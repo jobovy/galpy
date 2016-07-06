9ad54e8999694544dfef736c4f6d520de0ea6479
Jo Bovy <bovy@astro.utoronto.ca>
2016-07-06 11:07:22 -0400

    Proper error message when interrupting a dxdv integration in C

--- galpy/orbit_src/integrateFullOrbit.py
+++ galpy/orbit_src/integrateFullOrbit.py
@@ -270,6 +270,9 @@ def integrateFullOrbit_dxdv_c(pot,yo,dyo,t,int_method,rtol=None,atol=None): #pra
                     ctypes.byref(err),
                     ctypes.c_int(int_method_c))
 
+    if err.value == -10:
+        raise KeyboardInterrupt("Orbit integration interrupted by CTRL-C (SIGINT)")
+
     #Reset input arrays
     if f_cont[0]: yo= nu.asfortranarray(yo)
     if f_cont[1]: t= nu.asfortranarray(t)
--- galpy/orbit_src/integratePlanarOrbit.py
+++ galpy/orbit_src/integratePlanarOrbit.py
@@ -343,6 +343,9 @@ def integratePlanarOrbit_dxdv_c(pot,yo,dyo,t,int_method,rtol=None,atol=None,
                     ctypes.byref(err),
                     ctypes.c_int(int_method_c))
 
+    if err.value == -10:
+        raise KeyboardInterrupt("Orbit integration interrupted by CTRL-C (SIGINT)")
+
     #Reset input arrays
     if f_cont[0]: yo= nu.asfortranarray(yo)
     if f_cont[1]: t= nu.asfortranarray(t)
