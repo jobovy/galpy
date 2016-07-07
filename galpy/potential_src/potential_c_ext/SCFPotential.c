07185b2f5a74e32767dd8ae861cc78110d2fb407
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-07-07 08:35:07 -0400

    Removed the z parameter from Planar forces and deriv, and set z = 0 from within these functions. Adjusted dphiTilde and d2phiTilde.

--- galpy/potential_src/potential_c_ext/SCFPotential.c
+++ galpy/potential_src/potential_c_ext/SCFPotential.c
@@ -14,6 +14,12 @@ inline void cyl_to_spher(double R, double Z,double *r, double *theta){
   *theta = atan2(R, Z);
 }
 
+double power(double x, int i){
+    if (i==0) 
+        return 1;
+    return x*power(x,i - 1);
+}
+
 inline void calculateXi(double r, double a, double *xi){
 *xi = (r - a)/(r + a);
 }
@@ -38,45 +44,42 @@ inline void compute_phiTilde(double r, double a, int N, int L, double* C, double
 inline void compute_dphiTilde(double r, double a, int N, int L, double * C, double * dC, double * dphiTilde){
     double xi;
     calculateXi(r, a, &xi);
-    double rterm1 = -1./((a + r)*(a + r));
-    double rterm2 = -1.;
-    double rterm3 = -(1. - xi)*(1. - xi) / (2*a*(a + r));
-
+    double rterm = 1./(r*power(a + r, 3));
     for (int l = 0; l < L; l++){
         if (l != 0){
-            double rterm = (a*r)/((a + r)*(a + r)); 
-            rterm1 *= rterm;
-            rterm2 = (l*(a + r)/r - (2*l + 1));
-            rterm3 *= rterm;
-            }
-        for (int n = 0; n < N; n++){
-           *( dphiTilde + l*N + n) = rterm1*rterm2* (*(C + l*N + n)) +
-            rterm3*(*(dC + l*N + n));
+            rterm *= (a*r)/power(a + r, 2) ; 
         }
+        for (int n = 0; n < N; n++){
+           *( dphiTilde + l*N + n) = rterm *(((2*l + 1)*r*(a + r) - l*power(a + r,2))*(*(C + l*N + n)) -
+           2*a*r*(*(dC + l*N + n)));
+        
     }
-
+}
 }
 
 inline void compute_d2phiTilde(double r, double a, int N, int L, double * C, double * dC,double * d2C, double * d2phiTilde){
     double xi;
     calculateXi(r, a, &xi);
-    double rterm = -1./(r*r) / ((a + r)*(a + r)*(a + r));
-
+    double rterm = 1./(r*r) / power(a + r,5);
+    
     for (int l = 0; l < L; l++){
+
+        
         if (l != 0){
-            rterm *= (a*r)/((a + r)*(a + r));
-            }
+            rterm *= (a*r)/power(a + r, 2);
+        }
         for (int n = 0; n < N; n++){
+    
             double C_val = *(C + l*N + n);
             double dC_val = *(dC + l*N + n);
             double d2C_val = *(d2C + l*N + n);
-           *( d2phiTilde + l*N + n) = rterm*(C_val*(a*a * (l - 1)*l - 2*a*l*(l + 2)*r + (l+1)*(l+2)*r*r) +
-            r*(a + r)*(dC_val*(2*(a*l - (l + 1)*r)*(2*a)/((r + a)*(r + a)) - 4*r*a/((r + a)*(r + a))) +
-             4*a*a*r/((r + a)*(r + a)*(r + a))*d2C_val)) ;
+           *( d2phiTilde + l*N + n) = rterm*(C_val*(l*(1 - l)*power(a + r, 4) - (4*l*l + 6*l + 2.)*r*r*power(a + r,2) +
+           l*(4*l + 2)*r*power(a + r,3))
+           + a*r*((4*r*r + 4*a*r + (8*l + 4)*r*(a + r)-
+           4*l*power(a + r,2))*dC_val - 4*a*r*d2C_val));
              
         }
     }
-
 }
 
 inline void compute_C(double xi, int N, int L, double * C_array){
@@ -102,7 +105,7 @@ inline void compute_dC(double xi, int N, int L, double * dC_array){
 inline void compute_d2C(double xi, int N, int L, double * d2C_array){
     for (int l = 0; l < L; l++){
         *(d2C_array +l*N) = 0;
-        if (N !=1)
+        if (N >1)
             *(d2C_array +l*N + 1) = 0;
         if (N > 2)
             gsl_sf_gegenpoly_array(N - 3, 7./2 + 2*l, xi, d2C_array + l*N + 2);
@@ -275,7 +278,7 @@ equations e = {Eq,&PhiTilde_Pointer, &P_Pointer, &Constant};
 void computeDeriv(double R,double Z, double phi,
 				double t,
 				struct potentialArg * potentialArgs, double * F){
-	double * args= potentialArgs->args;
+  double * args= potentialArgs->args;
   //Get args
   double a = *args++;
   int N = *args++;
@@ -454,14 +457,14 @@ double SCFPotentialphiforce(double R,double Z, double phi,
   return *(F + 0)*dr_dphi + *(F + 1)*dtheta_dphi + *(F + 2)*dphi_dphi;
 }
 
-double SCFPotentialPlanarRforce(double R,double Z, double phi,
+double SCFPotentialPlanarRforce(double R,double phi,
 				double t,
 				struct potentialArg * potentialArgs){
   return SCFPotentialRforce(R,0, phi,t,potentialArgs);
   
 }
 
-double SCFPotentialPlanarphiforce(double R,double Z, double phi,
+double SCFPotentialPlanarphiforce(double R,double phi,
 				double t,
 				struct potentialArg * potentialArgs){
   return SCFPotentialphiforce(R,0, phi,t,potentialArgs);
@@ -469,41 +472,41 @@ double SCFPotentialPlanarphiforce(double R,double Z, double phi,
 
 
 
-double SCFPotentialPlanarR2deriv(double R, double Z, double phi,
+double SCFPotentialPlanarR2deriv(double R, double phi,
                             double t,
                             struct potentialArg * potentialArgs){
 
   double Farray[3];
   
- computeDeriv(R, Z, phi, t,potentialArgs, &Farray) ;
+ computeDeriv(R, 0, phi, t,potentialArgs, &Farray) ;
  
-    return *Farray;
+    return *(Farray + 0);
  
                                                         
    }
 
 
-double SCFPotentialPlanarphi2deriv(double R, double Z, double phi,
+double SCFPotentialPlanarphi2deriv(double R, double phi,
                             double t,
                             struct potentialArg * potentialArgs){
  
   
   double Farray[3];
   
- computeDeriv(R, Z, phi, t,potentialArgs, &Farray) ;
+ computeDeriv(R, 0, phi, t,potentialArgs, &Farray) ;
  
     return *(Farray + 1);
  
                                                         
    }
    
-double SCFPotentialPlanarRphideriv(double R, double Z, double phi,
+double SCFPotentialPlanarRphideriv(double R, double phi,
                             double t,
                             struct potentialArg * potentialArgs){
   
   double Farray[3];
   
- computeDeriv(R, Z, phi, t,potentialArgs, &Farray) ;
+ computeDeriv(R, 0, phi, t,potentialArgs, &Farray) ;
  
     return *(Farray + 2);
                                                         
