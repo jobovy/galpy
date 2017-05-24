e8fb9bf0a062a9c046937a5139c981ccaad1d049
Jack Hong <jackhong6@gmail.com>
2017-05-23 23:47:16 -0400

    Add skeleton of SpiralArmsPotential.

---
+++ galpy/potential_src/SpiralArmsPotential.py
@@ -0,0 +1,255 @@
+###############################################################################
+#  SpiralArmsPotential.py: class that implements the spiral arms potential
+#                           from Cox and Gomez (2002)
+#
+#  Phi(r, phi, z) = -4*pi*G*H*rho0*exp(-(r-r0)/Rs)*sum(Cn/(Kn*Dn)*cos(n*gamma)*sech(Kn*z/Bn)^Bn)
+###############################################################################
+
+import numpy as np
+from galpy.util import bovy_coords, bovy_conversion
+from galpy.potential_src.Potential import Potential, _APY_LOADED
+
+if _APY_LOADED:
+    from astropy import units
+
+
+class SpiralArmsPotential(Potential):
+    """Class that implements the spiral arms potential from Cox and Gomez (2002).
+    
+    .. math::
+    
+        \\Phi(r, \\phi, z) = -4 \\pi GH \\rho_0 exp(-\\frac{r-r_0}{R_s}) \\sum(\\frac{C_n}{(K_n D_n} cos(n \\gamma) sech(\\frac{K_n z}{B_n})^B_n)
+        
+    where
+    
+    .. math::
+    
+        
+        
+    """
+
+    def __init__(self, amp=1., ro=None, vo=None, amp_units='density', normalize=False,
+                  N=2, alpha=0.3, r_ref=1, phi_ref=0, Rs=0.5, H=0.5, Cs=[1], omega=0):
+        """
+        NAME:       
+            __init__
+        PURPOSE:
+            initialize a spiral arm potential
+        INPUT:
+            :param amp: amplitude to be applied to the potential (default: 1); 
+                        can be a Quantity with units of density. (amp = 4 * pi * G *rho0)
+            :param normalize: if True, normalize such that vc(1.,0.)=1., or, if given as a number
+                              such that the force is this fraction of the force necessary to make vc(1.,0.)=1.
+            :param ro: distance scales for translation into internal units (default from configuration file)
+            :param vo: velocity scales for translation into internal units (default from configuration file)
+            :param N: number of spiral arms
+            :param alpha: pitch angle of the logarithmic spiral arms in radians (can be Quantity)
+            :param r_ref: fiducial radius where rho = rho0 (r_0 in the paper by Cox and Gomez)
+            :param phi_ref: reference angle (phi_p(r_0) in the paper by Cox and Gomez)
+            :param Rs: radial scale length of the drop-off in density amplitude of the arms
+            :param H: scale height of the stellar arm perturbation
+            :param Cs: list of constants multiplying the cos(n*gamma) term in the mass density expression
+            :param omega: rotational speed of the spiral arms
+        OUTPUT:
+            (none)
+        HISTORY:
+            2017-05-12  Jack Hong (UBC)
+        """
+        Potential.__init__(self, amp=amp, ro=ro, vo=vo, amp_units=amp_units)
+        if _APY_LOADED:
+            if isinstance(alpha, units.Quantity):
+                alpha = alpha.to(units.rad).value
+
+        self._N = N
+        self._alpha = alpha
+        self._r_ref = r_ref
+        self._phi_ref = phi_ref
+        self._Rs = Rs
+        self._H = H
+        self._Cs = np.array(Cs)
+        self._n = len(Cs)
+        self._omega = omega
+        self.isNonAxi = True
+
+    def _evaluate(self, R, z, phi=0., t=0.):
+        """
+        NAME:
+            _evaluate
+        PURPOSE:
+            Evaluate the potential at the given coordinates. (without the amp factor)
+        INPUT:
+            :param R: galactocentric cylindrical radius
+            :param z: vertical height
+            :param phi: azimuth
+            :param t: time
+        OUTPUT:
+            :return: Phi(R, z, phi, t)
+        HISTORY:
+            2017-05-12  Jack Hong (UBC)
+        """
+        R = max(1e-6, R)
+        Kn = self._K(R)
+        Bn = self._B(R)
+        Dn = self._D(R)
+        return -self._H * np.exp(-(R-self._r_ref)/self._Rs) \
+            * np.sum(self._Cs/(Kn * Dn) * np.cos(self._n * self._gamma(R, phi, t)) * 1/np.cosh(Kn * z / Bn) ** Bn)
+
+    def _Rforce(self, R, z, phi=0., t=0.):
+        """
+        NAME:
+            _Rforce
+        PURPOSE:
+            Evaluate the radial force for this potential at the given coordinates.
+        INPUT:
+            :param R: galactocentric cylindrical radius
+            :param z: vertical height
+            :param phi: azimuth
+            :param t: time
+        OUTPUT:
+            :return: the radial force
+        HISTORY:
+            2017-05-12  Jack Hong (UBC)
+        """
+        return 0.0
+
+    def _zforce(self, R, z, phi=0., t=0.):
+        """
+        NAME:
+            _zforce
+        PURPOSE:
+            Evaluate the vertical force for this potential at the given coordinates.
+        INPUT:
+            :param R: galactocentric cylindrical radius
+            :param z: vertical height
+            :param phi: azimuth
+            :param t: time
+        OUTPUT:
+            :return: the vertical force
+        HISTORY:
+            2017-05-12  Jack Hong (UBC) 
+        """
+
+    def _R2deriv(self, R, z, phi=0., t=0.):
+        """
+        NAME:
+            _R2deriv
+        PURPOSE:
+            Evaluate the second (cylindrical) radial derivative of the potential (d^2 potential / d R^2)
+        INPUT:
+            :param R: galactocentric cylindrical radius
+            :param z: vertical height
+            :param phi: azimuth
+            :param t: time
+        OUTPUT:
+            :return: the second radial derivative
+        HISTORY:
+            2017-05-12  Jack Hong (UBC)
+        """
+        return 0.0
+
+    def _z2deriv(self, R, z, phi=0., t=0.):
+        """
+        NAME:
+            _z2deriv
+        PURPOSE:
+            Evaluate the second (cylindrical) vertical derivative of the potential (d^2 potential / d z^2).
+        INPUT:
+            :param R: galactocentric cylindrical radius
+            :param z: vertical height
+            :param phi: azimuth
+            :param t: time
+        OUTPUT:
+            :return: the second vertical derivative
+        HISTORY:
+            2017-05-12  Jack Hong (UBC) 
+        """
+        return 0.0
+
+    def _Rzderiv(self, R, z, phi=0., t=0.):
+        """
+        NAME:
+            _Rzderiv
+        PURPOSE:
+            Evaluate the mixed (cylindrical) radial and vertical derivative of the potential (d^2 potential / d R d z).
+        INPUT:
+            :param R: galactocentric cylindrical radius
+            :param z: vertical height
+            :param phi: azimuth
+            :param t: time
+        OUTPUT:
+            :return: d^2 potential / d R d z
+        HISTORY:
+            2017-05-12  Jack Hong (UBC) 
+        """
+        return 0.0
+
+    def _dens(self, R, z, phi=0., t=0.):
+        """
+        NAME:
+            _dens
+        PURPOSE:
+            Evaluate the density. If not given, the density is computed using the Poisson equation
+            from the first and second derivatives of the potential (if all are implemented).
+        INPUT:
+            :param R: galactocentric cylindrical radius
+            :param z: vertical height
+            :param phi: azimuth
+            :param t: time
+        OUTPUT:
+            :return: the density
+        HISTORY:
+            2017-05-12  Jack Hong (UBC) 
+        """
+        return 0.0
+
+    def _mass(self, R, z=0, t=0.):
+        """
+        NAME:
+            _mass
+        PURPOSE:
+            Evaluate the mass. Return the mass up to R and between -z and z. 
+            If not given, the mass is computed by integrating the density (if it is implemented or can be 
+            calculated from the Poisson equation).
+        INPUT:
+            :param R: galactocentric cylindrical radius
+            :param z: vertical height
+            :param t: time
+        OUTPUT:
+            :return: the mass
+        HISTORY:
+            2017-05-12  Jack Hong (UBC) 
+        """
+        return 0.0
+
+    def _gamma(self, R, phi, t):
+        """Return gamma."""
+        R = max(1e-6, R)
+        return self._N * (phi - self._phi_ref - np.log(R / self._ro) / np.tan(self._alpha) + self._omega * t)
+
+    def _K(self, R):
+        """Return numpy array from K1 up to and including Kn."""
+        ns = np.arange(1, self._n+1)
+        return ns * self._N / R / np.sin(self._alpha)
+
+    def _dK_dR(self, R):
+        """Return numpy array of dK/dR from K1 up to and including Kn."""
+        ns = np.arange(1, self._n+1)
+        return -ns * self._N / R**2 / np.sin(self._alpha)
+
+    def _B(self, R):
+        """Return numpy array from B1 up to and including Bn."""
+        Ks = self._K(R)
+        return Ks * self._H * (1 + 0.4 * Ks * self._H)
+
+    def _dB_dR(self):
+        """Return numpy array of constants from """
+        return 0
+
+    def _D(self, R):
+        """Return numpy array from D1 up to and including Dn."""
+        Ks = self._K(R)
+        return (1 + Ks * self._H + 0.3 * (Ks * self._H) ** 2) / (1 + 0.3 * Ks * self._H)
+
+    def _dD_dR(self, R):
+        """Return numpy array of dD/dR from D1 up to and including Dn"""
+        return 0
