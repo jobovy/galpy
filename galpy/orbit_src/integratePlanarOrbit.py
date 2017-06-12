3ef3de3c8ae87120c2f766ab31ca8cab2838791a
Jack Hong <jackhong6@gmail.com>
2017-06-12 15:00:22 -0400

    Edited integrateFullOrbit.py and integratePlanarOrbit.py to include SpiralArmsPotential.

--- galpy/orbit_src/integratePlanarOrbit.py
+++ galpy/orbit_src/integratePlanarOrbit.py
@@ -224,6 +224,11 @@ def _parse_pot(pot):
                     pot_args.extend([0,hz.get('h',0.0375)])
                 elif hztype == 'sech2':
                     pot_args.extend([1,hz.get('h',0.0375)])
+        elif isinstance(p, potential_src.planarPotential.planarPotentialFromFullPotential) \
+                and isinstance(p._Pot, potential.SpiralArmsPotential):
+            pot_type.append(27)
+            pot_args.extend([p._Pot._amp, p._Pot._N, p._Pot._alpha, p._pot._r_ref, p._Pot._phi_ref, p._Pot._Rs,
+                             p._Pot._H, p._Pot._Cs, p._Pot._omega])
     pot_type= nu.array(pot_type,dtype=nu.int32,order='C')
     pot_args= nu.array(pot_args,dtype=nu.float64,order='C')
     return (npot,pot_type,pot_args)
