dd28b150d46d089dc065634798ef6900f3936594
Jo Bovy <bovy@astro.utoronto.ca>
2017-06-23 15:12:26 -0400

    Bunch of changes of - to ^ for python3

--- galpy/potential_src/DoubleExponentialDiskPotential.py
+++ galpy/potential_src/DoubleExponentialDiskPotential.py
@@ -135,7 +135,7 @@ class DoubleExponentialDiskPotential(Potential):
                 floatIn= False
             out= nu.empty(len(R))
             indx= (R <= 6.)
-            out[True-indx]= self._kp(R[True-indx],z[True-indx])
+            out[True^indx]= self._kp(R[True^indx],z[True^indx])
             R4max= nu.copy(R)
             R4max[(R < 1.)]= 1.
             kmax= self._kmaxFac*self._beta
