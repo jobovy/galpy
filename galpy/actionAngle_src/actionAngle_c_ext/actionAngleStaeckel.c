ce693b4ff4ea0ac0acfbf038708bf0d89e8dc325
Jo Bovy <bovy@astro.utoronto.ca>
2017-12-12 22:56:23 -0500

    Add C function to compute umin,umax,vmin in the Staeckel approximation

--- galpy/actionAngle_src/actionAngle_c_ext/actionAngleStaeckel.c
+++ galpy/actionAngle_src/actionAngle_c_ext/actionAngleStaeckel.c
@@ -87,6 +87,10 @@ struct u0EqArg{
   Function Declarations
 */
 void calcu0(int,double *,double *,int,int *,double *,double,double *,int *);
+void actionAngleStaeckel_uminUmaxVmin(int,double *,double *,double *,double *,
+				      double *,double *,int,int *,double *,
+				      double,double *,
+				      double *,double *,int *);
 void actionAngleStaeckel_actions(int,double *,double *,double *,double *,
 				 double *,double *,int,int *,double *,double,
 				 double *,double *,int *);
@@ -270,6 +274,110 @@ void calcu0(int ndata,
   free_potentialArgs(npot,actionAngleArgs);
   free(actionAngleArgs);
   *err= status;
+}
+void actionAngleStaeckel_uminUmaxVmin(int ndata,
+				      double *R,
+				      double *vR,
+				      double *vT,
+				      double *z,
+				      double *vz,
+				      double *u0,
+				      int npot,
+				      int * pot_type,
+				      double * pot_args,
+				      double delta,
+				      double *umin,
+				      double *umax,
+				      double *vmin,
+				      int * err){
+  // Just copied this over from actionAngleStaeckel_actions below, not elegant
+  // but does the job...
+  int ii;
+  //Set up the potentials
+  struct potentialArg * actionAngleArgs= (struct potentialArg *) malloc ( npot * sizeof (struct potentialArg) );
+  parse_actionAngleArgs(npot,actionAngleArgs,&pot_type,&pot_args,false);
+  //E,Lz
+  double *E= (double *) malloc ( ndata * sizeof(double) );
+  double *Lz= (double *) malloc ( ndata * sizeof(double) );
+  calcEL(ndata,R,vR,vT,z,vz,E,Lz,npot,actionAngleArgs);
+  //Calculate all necessary parameters
+  double *ux= (double *) malloc ( ndata * sizeof(double) );
+  double *vx= (double *) malloc ( ndata * sizeof(double) );
+  Rz_to_uv_vec(ndata,R,z,ux,vx,delta);
+  double *coshux= (double *) malloc ( ndata * sizeof(double) );
+  double *sinhux= (double *) malloc ( ndata * sizeof(double) );
+  double *sinvx= (double *) malloc ( ndata * sizeof(double) );
+  double *cosvx= (double *) malloc ( ndata * sizeof(double) );
+  double *pux= (double *) malloc ( ndata * sizeof(double) );
+  double *pvx= (double *) malloc ( ndata * sizeof(double) );
+  double *sinh2u0= (double *) malloc ( ndata * sizeof(double) );
+  double *cosh2u0= (double *) malloc ( ndata * sizeof(double) );
+  double *v0= (double *) malloc ( ndata * sizeof(double) );
+  double *sin2v0= (double *) malloc ( ndata * sizeof(double) );
+  double *potu0v0= (double *) malloc ( ndata * sizeof(double) );
+  double *potupi2= (double *) malloc ( ndata * sizeof(double) );
+  double *I3U= (double *) malloc ( ndata * sizeof(double) );
+  double *I3V= (double *) malloc ( ndata * sizeof(double) );
+  UNUSED int chunk= CHUNKSIZE;
+#pragma omp parallel for schedule(static,chunk) private(ii)
+  for (ii=0; ii < ndata; ii++){
+    *(coshux+ii)= cosh(*(ux+ii));
+    *(sinhux+ii)= sinh(*(ux+ii));
+    *(cosvx+ii)= cos(*(vx+ii));
+    *(sinvx+ii)= sin(*(vx+ii));
+    *(pux+ii)= delta * (*(vR+ii) * *(coshux+ii) * *(sinvx+ii) 
+			+ *(vz+ii) * *(sinhux+ii) * *(cosvx+ii));
+    *(pvx+ii)= delta * (*(vR+ii) * *(sinhux+ii) * *(cosvx+ii) 
+			- *(vz+ii) * *(coshux+ii) * *(sinvx+ii));
+    *(sinh2u0+ii)= sinh(*(u0+ii)) * sinh(*(u0+ii));
+    *(cosh2u0+ii)= cosh(*(u0+ii)) * cosh(*(u0+ii));
+    *(v0+ii)= 0.5 * M_PI; //*(vx+ii);
+    *(sin2v0+ii)= sin(*(v0+ii)) * sin(*(v0+ii));
+    *(potu0v0+ii)= evaluatePotentialsUV(*(u0+ii),*(v0+ii),delta,
+					npot,actionAngleArgs);
+    *(I3U+ii)= *(E+ii) * *(sinhux+ii) * *(sinhux+ii)
+      - 0.5 * *(pux+ii) * *(pux+ii) / delta / delta
+      - 0.5 * *(Lz+ii) * *(Lz+ii) / delta / delta / *(sinhux+ii) / *(sinhux+ii) 
+      - ( *(sinhux+ii) * *(sinhux+ii) + *(sin2v0+ii))
+      *evaluatePotentialsUV(*(ux+ii),*(v0+ii),delta,
+			    npot,actionAngleArgs)
+      + ( *(sinh2u0+ii) + *(sin2v0+ii) )* *(potu0v0+ii);
+    *(potupi2+ii)= evaluatePotentialsUV(*(u0+ii),0.5 * M_PI,delta,
+					npot,actionAngleArgs);
+    *(I3V+ii)= - *(E+ii) * *(sinvx+ii) * *(sinvx+ii)
+      + 0.5 * *(pvx+ii) * *(pvx+ii) / delta / delta
+      + 0.5 * *(Lz+ii) * *(Lz+ii) / delta / delta / *(sinvx+ii) / *(sinvx+ii)
+      - *(cosh2u0+ii) * *(potupi2+ii)
+      + ( *(sinh2u0+ii) + *(sinvx+ii) * *(sinvx+ii))
+      * evaluatePotentialsUV(*(u0+ii),*(vx+ii),delta,
+			     npot,actionAngleArgs);
+  }
+  //Calculate 'peri' and 'apo'centers
+  calcUminUmax(ndata,umin,umax,ux,pux,E,Lz,I3U,delta,u0,sinh2u0,v0,sin2v0,
+	       potu0v0,npot,actionAngleArgs);
+  calcVmin(ndata,vmin,vx,pvx,E,Lz,I3V,delta,u0,cosh2u0,sinh2u0,potupi2,
+	   npot,actionAngleArgs);
+  //Free
+  free_potentialArgs(npot,actionAngleArgs);
+  free(actionAngleArgs);
+  free(E);
+  free(Lz);
+  free(ux);
+  free(vx);
+  free(coshux);
+  free(sinhux);
+  free(sinvx);
+  free(cosvx);
+  free(pux);
+  free(pvx);
+  free(sinh2u0);
+  free(cosh2u0);
+  free(v0);
+  free(sin2v0);
+  free(potu0v0);
+  free(potupi2);
+  free(I3U);
+  free(I3V);
 }
 void actionAngleStaeckel_actions(int ndata,
 				 double *R,
