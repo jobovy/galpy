5270a07d6ce605c629c4aa9c6f2b92adca6a1cc3
Jo Bovy <bovy@astro.utoronto.ca>
2018-12-25 16:36:30 -0500

    Compile new bovy_coords C functions

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ if WIN32:
     extra_compile_args.append("-DWIN32")
 
 #Orbit integration C extension
-orbit_int_c_src= ['galpy/util/bovy_symplecticode.c', 'galpy/util/bovy_rk.c', 'galpy/util/leung_dop853.c']
+orbit_int_c_src= ['galpy/util/bovy_symplecticode.c', 'galpy/util/bovy_rk.c', 'galpy/util/leung_dop853.c','galpy/util/bovy_coords.c']
 orbit_int_c_src.extend(glob.glob('galpy/potential/potential_c_ext/*.c'))
 orbit_int_c_src.extend(glob.glob('galpy/orbit/orbit_c_ext/*.c'))
 orbit_int_c_src.extend(glob.glob('galpy/util/interp_2d/*.c'))
@@ -223,7 +223,7 @@ else:
 actionAngle_c_src= glob.glob('galpy/actionAngle/actionAngle_c_ext/*.c')
 actionAngle_c_src.extend(glob.glob('galpy/potential/potential_c_ext/*.c'))
 actionAngle_c_src.extend(glob.glob('galpy/util/interp_2d/*.c'))
-actionAngle_c_src.extend(['galpy/util/bovy_symplecticode.c', 'galpy/util/bovy_rk.c', 'galpy/util/leung_dop853.c'])
+actionAngle_c_src.extend(['galpy/util/bovy_symplecticode.c', 'galpy/util/bovy_rk.c', 'galpy/util/leung_dop853.c','galpy/util/bovy_coords.c'])
 actionAngle_c_src.append('galpy/orbit/orbit_c_ext/integrateFullOrbit.c')
 actionAngle_include_dirs= ['galpy/actionAngle/actionAngle_c_ext',
                            'galpy/orbit/orbit_c_ext',
@@ -252,7 +252,7 @@ else:
 #interppotential C extension
 interppotential_c_src= glob.glob('galpy/potential/potential_c_ext/*.c')
 interppotential_c_src.extend(glob.glob('galpy/potential/interppotential_c_ext/*.c'))
-interppotential_c_src.extend(['galpy/util/bovy_symplecticode.c', 'galpy/util/bovy_rk.c', 'galpy/util/leung_dop853.c'])
+interppotential_c_src.extend(['galpy/util/bovy_symplecticode.c', 'galpy/util/bovy_rk.c', 'galpy/util/leung_dop853.c','galpy/util/bovy_coords.c'])
 interppotential_c_src.append('galpy/orbit/orbit_c_ext/integrateFullOrbit.c')
 interppotential_c_src.extend(glob.glob('galpy/util/interp_2d/*.c'))
 
