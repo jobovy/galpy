405d26b2b2ccf56a3f63d24cdf6f6a740ee26875
Jo Bovy <bovy@astro.utoronto.ca>
2019-01-01 13:57:52 -0500

    Test Orbits integration also in 3 and 5 phase-space dims

--- tests/test_orbits.py
+++ tests/test_orbits.py
@@ -10,7 +10,9 @@ def test_integration_1d():
     times= numpy.linspace(0.,10.,1001)
     orbits_list= [Orbit([1.,0.1]),Orbit([0.1,1.]),Orbit([-0.2,0.3])]
     orbits= Orbits(orbits_list)
-    # Integrate as Orbits
+    # Integrate as Orbits, twice to make sure initial cond. isn't changed
+    orbits.integrate(times,
+                     potential.toVerticalPotential(potential.MWPotential2014,1.))
     orbits.integrate(times,
                      potential.toVerticalPotential(potential.MWPotential2014,1.))
     # Integrate as multiple Orbits
@@ -29,7 +31,8 @@ def test_integration_2d():
     orbits_list= [Orbit([1.,0.1,1.,0.]),Orbit([.9,0.3,1.,-0.3]),
                   Orbit([1.2,-0.3,0.7,5.])]
     orbits= Orbits(orbits_list)
-    # Integrate as Orbits
+    # Integrate as Orbits, twice to make sure initial cond. isn't changed
+    orbits.integrate(times,potential.MWPotential2014)
     orbits.integrate(times,potential.MWPotential2014)
     # Integrate as multiple Orbits
     for o in orbits_list:
@@ -46,13 +49,34 @@ def test_integration_2d():
         assert numpy.amax(numpy.fabs(orbits_list[ii].phi(times)-orbits.phi(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
     return None
     
+def test_integration_p3d():
+    # 3D phase-space integration
+    from galpy.orbit import Orbit, Orbits
+    times= numpy.linspace(0.,10.,1001)
+    orbits_list= [Orbit([1.,0.1,1.]),Orbit([.9,0.3,1.]),
+                  Orbit([1.2,-0.3,0.7])]
+    orbits= Orbits(orbits_list)
+    # Integrate as Orbits, twice to make sure initial cond. isn't changed
+    orbits.integrate(times,potential.MWPotential2014)
+    orbits.integrate(times,potential.MWPotential2014)
+    # Integrate as multiple Orbits
+    for o in orbits_list:
+        o.integrate(times,potential.MWPotential2014)
+    # Compare
+    for ii in range(len(orbits)):
+        assert numpy.amax(numpy.fabs(orbits_list[ii].R(times)-orbits.R(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
+        assert numpy.amax(numpy.fabs(orbits_list[ii].vR(times)-orbits.vR(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
+        assert numpy.amax(numpy.fabs(orbits_list[ii].vT(times)-orbits.vT(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
+    return None
+    
 def test_integration_3d():
     from galpy.orbit import Orbit, Orbits
     times= numpy.linspace(0.,10.,1001)
     orbits_list= [Orbit([1.,0.1,1.,0.,0.1,0.]),Orbit([.9,0.3,1.,-0.3,0.4,3.]),
                   Orbit([1.2,-0.3,0.7,.5,-0.5,6.])]
     orbits= Orbits(orbits_list)
-    # Integrate as Orbits
+    # Integrate as Orbits, twice to make sure initial cond. isn't changed
+    orbits.integrate(times,potential.MWPotential2014)
     orbits.integrate(times,potential.MWPotential2014)
     # Integrate as multiple Orbits
     for o in orbits_list:
@@ -69,6 +93,28 @@ def test_integration_3d():
         assert numpy.amax(numpy.fabs(orbits_list[ii].vR(times)-orbits.vR(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
         assert numpy.amax(numpy.fabs(orbits_list[ii].vT(times)-orbits.vT(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
         assert numpy.amax(numpy.fabs(orbits_list[ii].phi(times)-orbits.phi(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
+    return None
+    
+def test_integration_p5d():
+    # 5D phase-space integration
+    from galpy.orbit import Orbit, Orbits
+    times= numpy.linspace(0.,10.,1001)
+    orbits_list= [Orbit([1.,0.1,1.,0.,0.1]),Orbit([.9,0.3,1.,-0.3,0.4]),
+                  Orbit([1.2,-0.3,0.7,.5,-0.5])]
+    orbits= Orbits(orbits_list)
+    # Integrate as Orbits, twice to make sure initial cond. isn't changed
+    orbits.integrate(times,potential.MWPotential2014)
+    orbits.integrate(times,potential.MWPotential2014)
+    # Integrate as multiple Orbits
+    for o in orbits_list:
+        o.integrate(times,potential.MWPotential2014)
+    # Compare
+    for ii in range(len(orbits)):
+        assert numpy.amax(numpy.fabs(orbits_list[ii].z(times)-orbits.z(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
+        assert numpy.amax(numpy.fabs(orbits_list[ii].vz(times)-orbits.vz(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
+        assert numpy.amax(numpy.fabs(orbits_list[ii].R(times)-orbits.R(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
+        assert numpy.amax(numpy.fabs(orbits_list[ii].vR(times)-orbits.vR(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
+        assert numpy.amax(numpy.fabs(orbits_list[ii].vT(times)-orbits.vT(times)[ii])) < 1e-10, 'Integration of multiple orbits as Orbits does not agree with integrating multiple orbits'
     return None
     
 # Tests that integrating Orbits agrees with integrating multiple Orbit 
