18f936b2abc4684496349e85622a4bfac7e946fe
Jo Bovy <bovy@astro.utoronto.ca>
2019-08-05 00:08:58 -0400

    Add second order numerical derivatives of Potentials X2deriv

--- galpy/potential/NumericalPotentialDerivativesMixin.py
+++ galpy/potential/NumericalPotentialDerivativesMixin.py
@@ -3,9 +3,14 @@
 ###############################################################################
 class NumericalPotentialDerivativesMixin(object):
     def __init__(self,*args,**kwargs):
-        self._dR= kwargs.get('dR',1e-8)
-        self._dphi= kwargs.get('dphi',1e-8)
-        self._dz= kwargs.get('dz',1e-8)
+        # For first derivatives
+        self._dR= kwargs.pop('dR',1e-8)
+        self._dphi= kwargs.pop('dphi',1e-8)
+        self._dz= kwargs.pop('dz',1e-8)
+        # For second derivatives
+        self._dR2= kwargs.pop('dR2',1e-4)
+        self._dphi2= kwargs.pop('dphi2',1e-4)
+        self._dz2= kwargs.pop('dz2',1e-4)
     
     def _Rforce(self,R,z,phi=0.,t=0.):
         # Do forward difference because R cannot be negative
@@ -32,3 +37,35 @@ class NumericalPotentialDerivativesMixin(object):
         dphi= phiplusdphi-phiminusdphi
         return (self._evaluate(R,z,phi=phiminusdphi,t=t)
                    -self._evaluate(R,z,phi=phiplusdphi,t=t))/dphi
+
+    def _R2deriv(self,R,z,phi=0.,t=0.):
+        # Do forward difference because R cannot be negative
+        RplusdR= R+self._dR2
+        Rplus2dR= R+2.*self._dR2
+        Rplus3dR= R+3.*self._dR2
+        dR= (Rplus3dR-R)/3.
+        return (2.*self._evaluate(R,z,phi=phi,t=t)
+                -5.*self._evaluate(RplusdR,z,phi=phi,t=t)
+                +4.*self._evaluate(Rplus2dR,z,phi=phi,t=t)
+                -1.*self._evaluate(Rplus3dR,z,phi=phi,t=t))/dR**2.
+
+    def _z2deriv(self,R,z,phi=0.,t=0.):
+        # Central derivative
+        zplusdz= z+self._dz2
+        zminusdz= z-self._dz2
+        dz= (zplusdz-zminusdz)/2.
+        return (self._evaluate(R,zplusdz,phi=phi,t=t)
+                   +self._evaluate(R,zminusdz,phi=phi,t=t)
+                   -2.*self._evaluate(R,z,phi=phi,t=t))/dz**2.
+
+    def _phi2deriv(self,R,z,phi=0.,t=0.):
+        if not self.isNonAxi: return 0.
+        # Central derivative
+        phiplusdphi= phi+self._dphi2
+        phiminusdphi= phi-self._dphi2
+        dphi= (phiplusdphi-phiminusdphi)/2.
+        return (self._evaluate(R,z,phi=phiplusdphi,t=t)
+                   +self._evaluate(R,z,phi=phiminusdphi,t=t)
+                   -2.*self._evaluate(R,z,phi=phi,t=t))/dphi**2.
+
+        
