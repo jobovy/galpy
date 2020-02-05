874d18fe5a1f30ac09f561afe344a8113163de87
Jo Bovy <bovy@astro.utoronto.ca>
2020-02-05 14:05:28 -0500

    Switch README format in setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ no_compiler = False  # Flag for cases where we are sure there is no compiler exi
 
 long_description= ''
 previous_line= ''
-with open('README.rst') as dfile:
+with open('README.md') as dfile:
     for line in dfile:
         if not 'image' in line and not 'target' in line \
                 and not 'DETAILED' in line and not '**master**' in line \
@@ -322,6 +322,7 @@ setup(name='galpy',
       author_email='bovy@astro.utoronto.ca',
       license='New BSD',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/jobovy/galpy',
       package_dir = {'galpy/': ''},
       packages=['galpy','galpy/orbit','galpy/potential',
@@ -329,7 +330,7 @@ setup(name='galpy',
                 'galpy/actionAngle'],
       package_data={'galpy/orbit':['named_objects.json'],
                     'galpy/df':['data/*.sav'],
-                    "": ["README.rst","README.dev","LICENSE","AUTHORS.rst"]},
+                    "": ["README.md","README.dev","LICENSE","AUTHORS.rst"]},
       include_package_data=True,
       install_requires=['numpy>=1.7','scipy','matplotlib','pytest','six',
                         'future','setuptools'],
