01db3bab2e943209a6bb3e2e822a506668ec1322
Jo Bovy <bovy@astro.utoronto.ca>
2020-03-08 01:11:11 -0500

    Add viewdoc extension to link to the source code in the docs [ci skip]

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,8 @@ import datetime
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax','sphinx.ext.ifconfig']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax','sphinx.ext.ifconfig',
+              'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
