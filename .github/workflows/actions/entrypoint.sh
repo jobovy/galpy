4f725f48fa360fb1955530a8d4a33ed2f1560887
Jo Bovy <bovy@astro.utoronto.ca>
2020-04-23 15:45:20 -0400

    Only build wheels for galpy, upload as artifact [ci skip]

--- .github/workflows/actions/entrypoint.sh
+++ .github/workflows/actions/entrypoint.sh
@@ -7,7 +7,7 @@ PYTHON_VERSIONS=("cp35-cp35m" "cp36-cp36m" "cp37-cp37m" "cp38-cp38")
 for PYTHON_VERSION in ${PYTHON_VERSIONS[@]}; do
     /opt/python/${PYTHON_VERSION}/bin/pip install --upgrade pip
     /opt/python/${PYTHON_VERSION}/bin/pip install -U wheel auditwheel
-    /opt/python/${PYTHON_VERSION}/bin/pip wheel . -w /github/workspace/wheelhouse/
+    /opt/python/${PYTHON_VERSION}/bin/python setup.py bdist_wheel -d wheelhouse
 done
 
 for whl in /github/workspace/wheelhouse/*.whl; do
