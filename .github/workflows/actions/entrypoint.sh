3a2abcb0c134df3c637bec31b60bfb21ac3068e5
Jo Bovy <bovy@astro.utoronto.ca>
2020-04-23 15:28:40 -0400

    Fix errors [ci skip]

--- .github/workflows/actions/entrypoint.sh
+++ .github/workflows/actions/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-yum install gsl-devel
+yum -y install gsl-devel
 
 PYTHON_VERSIONS=("cp35-cp35m" "cp36-cp36m" "cp37-cp37m" "cp38-cp38")
 
 for PYTHON_VERSION in ${PYTHON_VERSIONS[@]}; do
-    /opt/python/${PYTHON}/bin/pip install --upgrade pip
-    /opt/python/${PYTHON}/bin/pip install -U wheel auditwheel
-    /opt/python/${PYTHON}/bin/pip wheel . -w /github/workspace/wheelhouse/
+    /opt/python/${PYTHON_VERSION}/bin/pip install --upgrade pip
+    /opt/python/${PYTHON_VERSION}/bin/pip install -U wheel auditwheel
+    /opt/python/${PYTHON_VERSION}/bin/pip wheel . -w /github/workspace/wheelhouse/
 done
 
 for whl in /github/workspace/wheelhouse/*.whl; do
