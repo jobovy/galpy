1dc3c45578098c81d0d8b1738ffef3bc7afff4f7
Jo Bovy <bovy@astro.utoronto.ca>
2020-04-23 14:57:29 -0400

    Try building manylinux2014 wheels [ci skip]

---
+++ .github/workflows/actions/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+yum install gsl-devel
+
+PYTHON_VERSIONS=("cp35-cp35m" "cp36-cp36m" "cp37-cp37m" "cp38-cp38")
+
+for PYTHON_VERSION in ${PYTHON_VERSIONS[@]}; do
+    /opt/python/${PYTHON}/bin/pip install --upgrade pip
+    /opt/python/${PYTHON}/bin/pip install -U wheel auditwheel
+    /opt/python/${PYTHON}/bin/pip wheel . -w /github/workspace/wheelhouse/
+done
+
+for whl in /github/workspace/wheelhouse/*.whl; do
+    auditwheel repair $whl
+done
