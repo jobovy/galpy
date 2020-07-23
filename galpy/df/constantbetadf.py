3d0002ed9b13f87b3878334863738e681c253226
James Lane <jameslane93@gmail.com>
2020-07-23 12:37:03 -0700

    Add __init__ and imports for constantbetadf

--- galpy/df/constantbetadf.py
+++ galpy/df/constantbetadf.py
@@ -4,8 +4,23 @@ from .sphericaldf import anisotropicsphericaldf
 
 class constantbetadf(anisotropicsphericaldf):
     """Class that implements DFs of the form f(E,L) = L^{-2\beta} f(E) with constant beta anisotropy parameter"""
-    def __init__(self,ro=None,vo=None):
-        anisotropicsphericaldf.__init__(self,ro=ro,vo=vo)
+    def __init__(self,pot=None,beta=None,ro=None,vo=None):
+        """
+        NAME:
+
+            __init__
+
+        PURPOSE:
+
+            Initialize a spherical DF with constant anisotropy parameter
+
+        INPUT:
+
+            pot - Spherical potential which determines the DF
+        """
+        anisotropicsphericaldf.__init__(self,pot=pot,dftype='constant',
+            ro=ro,vo=vo)
+        self.beta = beta
 
     def f1E(self,E):
         # Stub for computing f_1(E) in BT08 nomenclature
