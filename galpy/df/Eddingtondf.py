931e00ec7a4603ec6082091c982575639dda5a20
Jo Bovy <bovy@astro.utoronto.ca>
2020-09-01 20:02:01 -0400

    Sample eta for the isotropic case directly using the analytic inverse CDF

--- galpy/df/Eddingtondf.py
+++ galpy/df/Eddingtondf.py
@@ -2,7 +2,6 @@
 # formula
 from .sphericaldf import sphericaldf
 import numpy
-import scipy.interpolate
 
 class Eddingtondf(sphericaldf):
     """Class that implements isotropic spherical DFs computed using the Eddington formula"""
@@ -19,12 +18,4 @@ class Eddingtondf(sphericaldf):
 
     def _sample_eta(self,n=1):
         """Sample the angle eta which defines radial vs tangential velocities"""
-        deta = 0.00005*numpy.pi
-        etas = (numpy.arange(0, numpy.pi, deta)+deta/2)
-        eta_pdf_cml = numpy.cumsum(numpy.sin(etas))
-        eta_pdf_cml_norm = eta_pdf_cml / eta_pdf_cml[-1]
-        eta_icml_interp = scipy.interpolate.interp1d(eta_pdf_cml_norm, etas, 
-            bounds_error=False, fill_value='extrapolate')
-        eta_samples = eta_icml_interp(numpy.random.uniform(size=n))
-        return eta_samples
-        
+        return numpy.arccos(1.-2.*numpy.random.uniform(size=n))
