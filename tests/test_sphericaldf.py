826b175f8ee0080937d24898667d59d6bec57dad
Jo Bovy <bovy@astro.utoronto.ca>
2020-09-02 11:43:23 -0400

    Add test of radial profile of isotropic Hernquist DF

--- tests/test_sphericaldf.py
+++ tests/test_sphericaldf.py
@@ -6,8 +6,9 @@ from galpy.df import isotropicHernquistdf
 
 # Test that the density distribution of the isotropic Hernquist is correct
 def test_isotropic_hernquist_dens_spherically_symmetric():
-    pot = potential.HernquistPotential(amp=2.,a=1.3)
+    pot= potential.HernquistPotential(amp=2.,a=1.3)
     dfh= isotropicHernquistdf(pot=pot)
+    numpy.random.seed(10)
     samp= dfh.sample(n=100000)
     # Check spherical symmetry for different harmonics l,m
     tol= 1e-2
@@ -25,10 +26,33 @@ def test_isotropic_hernquist_dens_spherically_symmetric():
     check_spherical_symmetry(samp,9,-6,tol)
     return None
     
+def test_isotropic_hernquist_dens_massprofile():
+    pot= potential.HernquistPotential(amp=2.,a=1.3)
+    dfh= isotropicHernquistdf(pot=pot)
+    numpy.random.seed(10)
+    samp= dfh.sample(n=100000)
+    tol= 5*1e-3
+    check_spherical_massprofile(samp,
+                                lambda r: pot.mass(r,use_physical=False)\
+                                   /pot.mass(numpy.amax(samp.r(use_physical=False)),
+                                             use_physical=False),
+                                tol,skip=1000)
+    return None
+    
 def check_spherical_symmetry(samp,l,m,tol):
     """Check for spherical symmetry by Monte Carlo integration of the
     spherical harmonic |Y_mn|^2 over the sample, should be zero unless l=m=0"""
-    thetas, phis= numpy.arctan2(samp.R(),samp.z()), samp.phi()
+    thetas, phis= numpy.arctan2(samp.R(use_physical=False),samp.z(use_physical=False)), samp.phi(use_physical=False)
     assert numpy.fabs(numpy.sum(special.lpmv(m,l,numpy.cos(thetas))*numpy.cos(m*phis))/samp.size-(l==0)*(m==0)) < tol, 'Sample does not appear to be spherically symmetric, fails spherical harmonics test for (l,m) = ({},{})'.format(l,m)
     return None
-    
+
+def check_spherical_massprofile(samp,mass_profile,tol,skip=100):
+    """Check that the cumulative distribution of radii follows the 
+    cumulative mass profile (normalized such that total mass = 1)"""
+    rs= samp.r(use_physical=False)
+    cumul_rs= numpy.sort(rs)
+    cumul_mass= numpy.linspace(0.,1.,len(rs))
+    for ii in range(len(rs)//skip-1):
+        indx= (ii+1)*skip
+        assert numpy.fabs(cumul_mass[indx]-mass_profile(cumul_rs[indx])) < tol, 'Mass profile of samples does not agree with analytical one'
+    return None
