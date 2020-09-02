248819988a6adc48cb80beafefef9641c2db2069
Jo Bovy <bovy@astro.utoronto.ca>
2020-09-02 10:58:31 -0400

    Add test of the spherical symmetry of Hernquist DF samples

--- tests/test_sphericaldf.py
+++ tests/test_sphericaldf.py
@@ -1,6 +1,34 @@
 # Tests of spherical distribution functions
-
-#### STUB, this just to see that everything imports, remove later #####
-from galpy.df import Eddingtondf
+import numpy
+from scipy import special
+from galpy import potential
 from galpy.df import isotropicHernquistdf
-from galpy.df import constantbetaHernquistdf
+
+# Test that the density distribution of the isotropic Hernquist is correct
+def test_isotropic_hernquist_dens_spherically_symmetric():
+    pot = potential.HernquistPotential(amp=2.,a=1.3)
+    dfh= isotropicHernquistdf(pot=pot)
+    samp= dfh.sample(n=100000)
+    # Check spherical symmetry for different harmonics l,m
+    tol= 1e-2
+    check_spherical_symmetry(samp,0,0,tol)
+    check_spherical_symmetry(samp,1,0,tol)
+    check_spherical_symmetry(samp,1,-1,tol)
+    check_spherical_symmetry(samp,1,1,tol)
+    check_spherical_symmetry(samp,2,0,tol)
+    check_spherical_symmetry(samp,2,-1,tol)
+    check_spherical_symmetry(samp,2,-2,tol)
+    check_spherical_symmetry(samp,2,1,tol)
+    check_spherical_symmetry(samp,2,2,tol)
+    # and some higher order ones
+    check_spherical_symmetry(samp,3,1,tol)
+    check_spherical_symmetry(samp,9,-6,tol)
+    return None
+    
+def check_spherical_symmetry(samp,l,m,tol):
+    """Check for spherical symmetry by Monte Carlo integration of the
+    spherical harmonic |Y_mn|^2 over the sample, should be zero unless l=m=0"""
+    thetas, phis= numpy.arctan2(samp.R(),samp.z()), samp.phi()
+    assert numpy.fabs(numpy.sum(special.lpmv(m,l,numpy.cos(thetas))*numpy.cos(m*phis))/samp.size-(l==0)*(m==0)) < tol, 'Sample does not appear to be spherically symmetric, fails spherical harmonics test for (l,m) = ({},{})'.format(l,m)
+    return None
+    
