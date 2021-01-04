c1aa1044e0dd254ddf0eda13bc02d8b3adbac40b
Jo Bovy <bovy@astro.utoronto.ca>
2021-01-04 12:02:01 -0500

    Remove old double-exponential code

--- galpy/potential/DoubleExponentialDiskPotential.py
+++ galpy/potential/DoubleExponentialDiskPotential.py
@@ -7,10 +7,7 @@
 import numpy
 from scipy import special
 from ..util import conversion
-from .PowerSphericalPotential import KeplerPotential
 from .Potential import Potential
-_TOL= 1.4899999999999999e-15
-_MAXITER= 20
 
 def _de_psi(t):
     return t*numpy.tanh(numpy.pi/2.*numpy.sinh(t))
@@ -26,11 +23,9 @@ class DoubleExponentialDiskPotential(Potential):
         \\rho(R,z) = \\mathrm{amp}\\,\\exp\\left(-R/h_R-|z|/h_z\\right)
 
     """
-    def __init__(self,amp=1.,hr=1./3.,hz=1./16.,
-                 maxiter=_MAXITER,tol=0.001,normalize=False,
+    def __init__(self,amp=1.,hr=1./3.,hz=1./16.,normalize=False,
                  ro=None,vo=None,
-                 new=True,kmaxFac=2.,glorder=10,
-                 de=True,de_h=1e-3,de_n=10000):
+                 de_h=1e-3,de_n=10000):
         """
         NAME:
 
@@ -48,11 +43,11 @@ class DoubleExponentialDiskPotential(Potential):
 
            hz - scale-height (can be Quantity)
 
-           tol - relative accuracy of potential-evaluations
-
-           maxiter - scipy.integrate keyword
-
            normalize - if True, normalize such that vc(1.,0.)=1., or, if given as a number, such that the force is this fraction of the force necessary to make vc(1.,0.)=1.
+
+           de_h= (1e-3) step used in numerical integration (use 1000 for a lower accuracy version that is typically still high accuracy enough, but faster)
+
+           de_b= (10000) number of points used in numerical integration
 
            ro=, vo= distance and velocity scales for translation into internal units (default from configuration file)
 
@@ -74,37 +69,13 @@ class DoubleExponentialDiskPotential(Potential):
         hz= conversion.parse_length(hz,ro=self._ro)
         self.hasC= True
         self.hasC_dens= True
-        self._kmaxFac= kmaxFac
-        self._glorder= glorder
         self._hr= hr
         self._scale= self._hr
         self._hz= hz
         self._alpha= 1./self._hr
         self._beta= 1./self._hz
-        self._gamma= self._alpha/self._beta
-        self._maxiter= maxiter
-        self._tol= tol
         self._zforceNotSetUp= True #We have not calculated a typical Kz yet
-        #Setup j0 zeros etc.
-        self._glx, self._glw= numpy.polynomial.legendre.leggauss(self._glorder)
-        self._nzeros=100
-        #j0 for potential and z
-        self._j0zeros= numpy.zeros(self._nzeros+1)
-        self._j0zeros[1:self._nzeros+1]= special.jn_zeros(0,self._nzeros)
-        self._dj0zeros= self._j0zeros-numpy.roll(self._j0zeros,1)
-        self._dj0zeros[0]= self._j0zeros[0]
-        #j1 for R
-        self._j1zeros= numpy.zeros(self._nzeros+1)
-        self._j1zeros[1:self._nzeros+1]= special.jn_zeros(1,self._nzeros)
-        self._dj1zeros= self._j1zeros-numpy.roll(self._j1zeros,1)
-        self._dj1zeros[0]= self._j1zeros[0]
-        #j2 for R2deriv
-        self._j2zeros= numpy.zeros(self._nzeros+1)
-        self._j2zeros[1:self._nzeros+1]= special.jn_zeros(2,self._nzeros)
-        self._dj2zeros= self._j2zeros-numpy.roll(self._j2zeros,1)
-        self._dj2zeros[0]= self._j2zeros[0]
         # For double-exponential formula
-        self._de= de
         self._de_h= de_h
         self._de_n= de_n
         self._de_j0zeros= special.jn_zeros(0,self._de_n)/numpy.pi
@@ -126,8 +97,6 @@ class DoubleExponentialDiskPotential(Potential):
                 (isinstance(normalize,(int,float)) \
                      and not isinstance(normalize,bool)): #pragma: no cover
             self.normalize(normalize)
-        #Load Kepler potential for large R
-        self._kp= KeplerPotential(normalize=4.*numpy.pi/self._alpha**2./self._beta)
 
     def _evaluate(self,R,z,phi=0.,t=0.,dR=0,dphi=0):
         """
@@ -158,33 +127,15 @@ class DoubleExponentialDiskPotential(Potential):
             outShape= R.shape # this code can't do arbitrary shapes
             R= R.flatten()
             z= z.flatten()
-        if self._de:
-            fun= lambda x: (self._alpha**2.+(x/R[:,numpy.newaxis])**2.)**-1.5\
-                *(self._beta*numpy.exp(-x/R[:,numpy.newaxis]*numpy.fabs(z[:,numpy.newaxis]))
-                  -x/R[:,numpy.newaxis]*numpy.exp(-self._beta*numpy.fabs(z[:,numpy.newaxis])))\
-                  /(self._beta**2.-(x/R[:,numpy.newaxis])**2.)
-            out= -4.*numpy.pi*self._alpha/R*\
-                numpy.nansum(fun(self._de_j0_xs)*self._de_j0_weights,
-                             axis=1)
-            if floatIn: return out[0]
-            else: return numpy.reshape(out,outShape)
-        if True:
-            out= numpy.empty(len(R))
-            indx= (R <= 6.)
-            if numpy.sum(True^indx) > 0:
-                out[True^indx]= self._kp(R[True^indx],z[True^indx])
-            R4max= numpy.copy(R)
-            R4max[(R < 1.)]= 1.
-            kmax= self._kmaxFac*self._beta
-            for jj in range(len(R)):
-                if not indx[jj]: continue
-                maxj0zeroIndx= numpy.argmin((self._j0zeros-kmax*R4max[jj])**2.) #close enough
-                ks= numpy.array([0.5*(self._glx+1.)*self._dj0zeros[ii+1] + self._j0zeros[ii] for ii in range(maxj0zeroIndx)]).flatten()
-                weights= numpy.array([self._glw*self._dj0zeros[ii+1] for ii in range(maxj0zeroIndx)]).flatten()
-                evalInt= special.jn(0,ks*R[jj])*(self._alpha**2.+ks**2.)**-1.5*(self._beta*numpy.exp(-ks*numpy.fabs(z[jj]))-ks*numpy.exp(-self._beta*numpy.fabs(z[jj])))/(self._beta**2.-ks**2.)
-                out[jj]= -2.*numpy.pi*self._alpha*numpy.sum(weights*evalInt)
-            if floatIn: return out[0]
-            else: return numpy.reshape(out,outShape)
+        fun= lambda x: (self._alpha**2.+(x/R[:,numpy.newaxis])**2.)**-1.5\
+            *(self._beta*numpy.exp(-x/R[:,numpy.newaxis]*numpy.fabs(z[:,numpy.newaxis]))
+              -x/R[:,numpy.newaxis]*numpy.exp(-self._beta*numpy.fabs(z[:,numpy.newaxis])))\
+              /(self._beta**2.-(x/R[:,numpy.newaxis])**2.)
+        out= -4.*numpy.pi*self._alpha/R*\
+            numpy.nansum(fun(self._de_j0_xs)*self._de_j0_weights,
+                         axis=1)
+        if floatIn: return out[0]
+        else: return numpy.reshape(out,outShape)
     
     def _Rforce(self,R,z,phi=0.,t=0.):
         """
@@ -204,28 +155,12 @@ class DoubleExponentialDiskPotential(Potential):
            2012-12-26 - New method using Gaussian quadrature between zeros - Bovy (IAS)
            2020-12-24 - New method using Ogata's Bessel integral formula - Bovy (UofT)
         """
-        if self._de:
-            fun=  lambda x: x*(self._alpha**2.+(x/R)**2.)**-1.5\
-                *(self._beta*numpy.exp(-x/R*numpy.fabs(z))
-                  -x/R*numpy.exp(-self._beta*numpy.fabs(z)))\
-                  /(self._beta**2.-(x/R)**2.)
-            return -4.*numpy.pi*self._alpha/R**2.\
-                *numpy.nansum(fun(self._de_j1_xs)*self._de_j1_weights)
-        if True:
-            if isinstance(R,numpy.ndarray):
-                if not isinstance(z,numpy.ndarray): z= numpy.ones_like(R)*z
-                out= numpy.array([self._Rforce(rr,zz) for rr,zz in zip(R,z)])
-                return out
-            if (R > 16.*self._hr or R > 6.) and hasattr(self,'_kp'): return self._kp.Rforce(R,z)
-            if R < 1.: R4max= 1.
-            else: R4max= R
-            kmax= self._kmaxFac*self._beta
-            kmax= 2.*self._kmaxFac*self._beta
-            maxj1zeroIndx= numpy.argmin((self._j1zeros-kmax*R4max)**2.) #close enough
-            ks= numpy.array([0.5*(self._glx+1.)*self._dj1zeros[ii+1] + self._j1zeros[ii] for ii in range(maxj1zeroIndx)]).flatten()
-            weights= numpy.array([self._glw*self._dj1zeros[ii+1] for ii in range(maxj1zeroIndx)]).flatten()
-            evalInt= ks*special.jn(1,ks*R)*(self._alpha**2.+ks**2.)**-1.5*(self._beta*numpy.exp(-ks*numpy.fabs(z))-ks*numpy.exp(-self._beta*numpy.fabs(z)))/(self._beta**2.-ks**2.)
-            return -2.*numpy.pi*self._alpha*numpy.sum(weights*evalInt)
+        fun=  lambda x: x*(self._alpha**2.+(x/R)**2.)**-1.5\
+            *(self._beta*numpy.exp(-x/R*numpy.fabs(z))
+              -x/R*numpy.exp(-self._beta*numpy.fabs(z)))\
+              /(self._beta**2.-(x/R)**2.)
+        return -4.*numpy.pi*self._alpha/R**2.\
+            *numpy.nansum(fun(self._de_j1_xs)*self._de_j1_weights)
     
     def _zforce(self,R,z,phi=0.,t=0.):
         """
@@ -245,34 +180,16 @@ class DoubleExponentialDiskPotential(Potential):
            2012-12-26 - New method using Gaussian quadrature between zeros - Bovy (IAS)
            2020-12-24 - New method using Ogata's Bessel integral formula - Bovy (UofT)
         """
-        if self._de:
-            fun= lambda x: (self._alpha**2.+(x/R)**2.)**-1.5*x/R\
-                *(numpy.exp(-x/R*numpy.fabs(z))
-                  -numpy.exp(-self._beta*numpy.fabs(z)))\
-                  /(self._beta**2.-(x/R)**2.)
-            out= -4.*numpy.pi*self._alpha*self._beta/R*\
-                numpy.nansum(fun(self._de_j0_xs)*self._de_j0_weights)
-            if z > 0.:
-                return out
-            else:
-                return -out
-        if True:
-            if isinstance(R,numpy.ndarray):
-                if not isinstance(z,numpy.ndarray): z= numpy.ones_like(R)*z
-                out= numpy.array([self._zforce(rr,zz) for rr,zz in zip(R,z)])
-                return out
-            if R > 16.*self._hr or R > 6.: return self._kp.zforce(R,z)
-            if R < 1.: R4max= 1.
-            else: R4max= R
-            kmax= self._kmaxFac*self._beta
-            maxj0zeroIndx= numpy.argmin((self._j0zeros-kmax*R4max)**2.) #close enough
-            ks= numpy.array([0.5*(self._glx+1.)*self._dj0zeros[ii+1] + self._j0zeros[ii] for ii in range(maxj0zeroIndx)]).flatten()
-            weights= numpy.array([self._glw*self._dj0zeros[ii+1] for ii in range(maxj0zeroIndx)]).flatten()
-            evalInt= ks*special.jn(0,ks*R)*(self._alpha**2.+ks**2.)**-1.5*(numpy.exp(-ks*numpy.fabs(z))-numpy.exp(-self._beta*numpy.fabs(z)))/(self._beta**2.-ks**2.)
-            if z > 0.:
-                return -2.*numpy.pi*self._alpha*self._beta*numpy.sum(weights*evalInt)
-            else:
-                return 2.*numpy.pi*self._alpha*self._beta*numpy.sum(weights*evalInt)
+        fun= lambda x: (self._alpha**2.+(x/R)**2.)**-1.5*x/R\
+            *(numpy.exp(-x/R*numpy.fabs(z))
+              -numpy.exp(-self._beta*numpy.fabs(z)))\
+              /(self._beta**2.-(x/R)**2.)
+        out= -4.*numpy.pi*self._alpha*self._beta/R*\
+            numpy.nansum(fun(self._de_j0_xs)*self._de_j0_weights)
+        if z > 0.:
+            return out
+        else:
+            return -out
 
     def _R2deriv(self,R,z,phi=0.,t=0.):
         """
@@ -291,34 +208,14 @@ class DoubleExponentialDiskPotential(Potential):
            2012-12-27 - Written - Bovy (IAS)
            2020-12-24 - New method using Ogata's Bessel integral formula - Bovy (UofT)
         """
-        if self._de:
-            fun=  lambda x: x**2*(self._alpha**2.+(x/R)**2.)**-1.5\
-                *(self._beta*numpy.exp(-x/R*numpy.fabs(z))
-                  -x/R*numpy.exp(-self._beta*numpy.fabs(z)))\
-                  /(self._beta**2.-(x/R)**2.)
-            return 4.*numpy.pi*self._alpha/R**3.\
-                *numpy.nansum(fun(self._de_j0_xs)*self._de_j0_weights
-                              -fun(self._de_j1_xs)/self._de_j1_xs\
-                                  *self._de_j1_weights)
-        if True:
-            if isinstance(R,numpy.ndarray):
-                if not isinstance(z,numpy.ndarray): z= numpy.ones_like(R)*z
-                out= numpy.array([self._R2deriv(rr,zz) for rr,zz in zip(R,z)])
-                return out
-            if R > 16.*self._hr or R > 6.: return self._kp.R2deriv(R,z)
-            if R < 1.: R4max= 1.
-            else: R4max= R
-            kmax= 2.*self._kmaxFac*self._beta
-            maxj0zeroIndx= numpy.argmin((self._j0zeros-kmax*R4max)**2.) #close enough
-            maxj2zeroIndx= numpy.argmin((self._j2zeros-kmax*R4max)**2.) #close enough
-            ks0= numpy.array([0.5*(self._glx+1.)*self._dj0zeros[ii+1] + self._j0zeros[ii] for ii in range(maxj0zeroIndx)]).flatten()
-            weights0= numpy.array([self._glw*self._dj0zeros[ii+1] for ii in range(maxj0zeroIndx)]).flatten()
-            ks2= numpy.array([0.5*(self._glx+1.)*self._dj2zeros[ii+1] + self._j2zeros[ii] for ii in range(maxj2zeroIndx)]).flatten()
-            weights2= numpy.array([self._glw*self._dj2zeros[ii+1] for ii in range(maxj2zeroIndx)]).flatten()
-            evalInt0= ks0**2.*special.jn(0,ks0*R)*(self._alpha**2.+ks0**2.)**-1.5*(self._beta*numpy.exp(-ks0*numpy.fabs(z))-ks0*numpy.exp(-self._beta*numpy.fabs(z)))/(self._beta**2.-ks0**2.)
-            evalInt2= ks2**2.*special.jn(2,ks2*R)*(self._alpha**2.+ks2**2.)**-1.5*(self._beta*numpy.exp(-ks2*numpy.fabs(z))-ks2*numpy.exp(-self._beta*numpy.fabs(z)))/(self._beta**2.-ks2**2.)
-            return numpy.pi*self._alpha*(numpy.sum(weights0*evalInt0)
-                                      -numpy.sum(weights2*evalInt2))
+        fun=  lambda x: x**2*(self._alpha**2.+(x/R)**2.)**-1.5\
+            *(self._beta*numpy.exp(-x/R*numpy.fabs(z))
+              -x/R*numpy.exp(-self._beta*numpy.fabs(z)))\
+              /(self._beta**2.-(x/R)**2.)
+        return 4.*numpy.pi*self._alpha/R**3.\
+            *numpy.nansum(fun(self._de_j0_xs)*self._de_j0_weights
+                          -fun(self._de_j1_xs)/self._de_j1_xs\
+                          *self._de_j1_weights)
     
     def _z2deriv(self,R,z,phi=0.,t=0.):
         """
@@ -337,27 +234,12 @@ class DoubleExponentialDiskPotential(Potential):
            2012-12-26 - Written - Bovy (IAS)
            2020-12-24 - New method using Ogata's Bessel integral formula - Bovy (UofT)
         """
-        if self._de:
-            fun= lambda x: (self._alpha**2.+(x/R)**2.)**-1.5*x/R\
-                *(x/R*numpy.exp(-x/R*numpy.fabs(z))
-                  -self._beta*numpy.exp(-self._beta*numpy.fabs(z)))\
-                  /(self._beta**2.-(x/R)**2.)
-            return -4.*numpy.pi*self._alpha*self._beta/R*\
-                numpy.nansum(fun(self._de_j0_xs)*self._de_j0_weights)
-        if True:
-            if isinstance(R,numpy.ndarray):
-                if not isinstance(z,numpy.ndarray): z= numpy.ones_like(R)*z
-                out= numpy.array([self._z2deriv(rr,zz) for rr,zz in zip(R,z)])
-                return out
-            if R > 16.*self._hr or R > 6.: return self._kp.z2deriv(R,z)
-            if R < 1.: R4max= 1.
-            else: R4max= R
-            kmax= self._kmaxFac*self._beta
-            maxj0zeroIndx= numpy.argmin((self._j0zeros-kmax*R4max)**2.) #close enough
-            ks= numpy.array([0.5*(self._glx+1.)*self._dj0zeros[ii+1] + self._j0zeros[ii] for ii in range(maxj0zeroIndx)]).flatten()
-            weights= numpy.array([self._glw*self._dj0zeros[ii+1] for ii in range(maxj0zeroIndx)]).flatten()
-            evalInt= ks*special.jn(0,ks*R)*(self._alpha**2.+ks**2.)**-1.5*(ks*numpy.exp(-ks*numpy.fabs(z))-self._beta*numpy.exp(-self._beta*numpy.fabs(z)))/(self._beta**2.-ks**2.)
-            return -2.*numpy.pi*self._alpha*self._beta*numpy.sum(weights*evalInt)
+        fun= lambda x: (self._alpha**2.+(x/R)**2.)**-1.5*x/R\
+            *(x/R*numpy.exp(-x/R*numpy.fabs(z))
+              -self._beta*numpy.exp(-self._beta*numpy.fabs(z)))\
+              /(self._beta**2.-(x/R)**2.)
+        return -4.*numpy.pi*self._alpha*self._beta/R*\
+            numpy.nansum(fun(self._de_j0_xs)*self._de_j0_weights)
 
     def _Rzderiv(self,R,z,phi=0.,t=0.):
         """
@@ -376,34 +258,16 @@ class DoubleExponentialDiskPotential(Potential):
            2013-08-28 - Written - Bovy (IAS)
            2020-12-24 - New method using Ogata's Bessel integral formula - Bovy (UofT)
         """
-        if self._de:
-            fun= lambda x: (self._alpha**2.+(x/R)**2.)**-1.5*(x/R)**2.\
-                *(numpy.exp(-x/R*numpy.fabs(z))
-                  -numpy.exp(-self._beta*numpy.fabs(z)))\
-                  /(self._beta**2.-(x/R)**2.)
-            out= -4.*numpy.pi*self._alpha*self._beta/R*\
-                numpy.nansum(fun(self._de_j1_xs)*self._de_j1_weights)
-            if z > 0.:
-                return out
-            else:
-                return -out
-        if True:
-            if isinstance(R,numpy.ndarray):
-                if not isinstance(z,numpy.ndarray): z= numpy.ones_like(R)*z
-                out= numpy.array([self._Rzderiv(rr,zz) for rr,zz in zip(R,z)])
-                return out
-            if R > 6.: return self._kp.Rzderiv(R,z)
-            if R < 1.: R4max= 1.
-            else: R4max= R
-            kmax= 2.*self._kmaxFac*self._beta
-            maxj1zeroIndx= numpy.argmin((self._j1zeros-kmax*R4max)**2.) #close enough
-            ks= numpy.array([0.5*(self._glx+1.)*self._dj1zeros[ii+1] + self._j1zeros[ii] for ii in range(maxj1zeroIndx)]).flatten()
-            weights= numpy.array([self._glw*self._dj1zeros[ii+1] for ii in range(maxj1zeroIndx)]).flatten()
-            evalInt= ks**2.*special.jn(1,ks*R)*(self._alpha**2.+ks**2.)**-1.5*(numpy.exp(-ks*numpy.fabs(z))-numpy.exp(-self._beta*numpy.fabs(z)))/(self._beta**2.-ks**2.)
-            if z >= 0.:
-                return -2.*numpy.pi*self._alpha*self._beta*numpy.sum(weights*evalInt)
-            else:
-                return 2.*numpy.pi*self._alpha*self._beta*numpy.sum(weights*evalInt)
+        fun= lambda x: (self._alpha**2.+(x/R)**2.)**-1.5*(x/R)**2.\
+            *(numpy.exp(-x/R*numpy.fabs(z))
+              -numpy.exp(-self._beta*numpy.fabs(z)))\
+              /(self._beta**2.-(x/R)**2.)
+        out= -4.*numpy.pi*self._alpha*self._beta/R*\
+            numpy.nansum(fun(self._de_j1_xs)*self._de_j1_weights)
+        if z > 0.:
+            return out
+        else:
+            return -out
 
     def _dens(self,R,z,phi=0.,t=0.):
         """
