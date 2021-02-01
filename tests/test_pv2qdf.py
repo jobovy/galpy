ec12996584fd68b9423d14eff14a6a534554a5e7
Jo Bovy <bovy@astro.utoronto.ca>
2021-01-31 20:56:47 -0500

    Fix tests [ci skip]

--- tests/test_pv2qdf.py
+++ tests/test_pv2qdf.py
@@ -261,7 +261,7 @@ def test_pvRvz_staeckel_arrayin():
     qdf= quasiisothermaldf(1./4.,0.2,0.1,1.,1.,
                            pot=MWPotential,aA=aAS,cutcounter=True)
     R,z= 0.8, 0.1
-    pvRvz= qdf.pvRvz(0.1,0.05,R*numpy.ones(2),z*numpy.ones(2))
-    assert numpy.all(numpy.log(pvRvz)-numpy.log(qdf.pvRvz(0.1,0.05,R,z)) < 10.**-10.), 'pvRvz calculated with R and z array input does not equal to calculated with scalar input'
+    pvRvz= qdf.pvRvz(0.1*numpy.ones(2),0.05*numpy.ones(2),R*numpy.ones(2),z*numpy.ones(2))
+    assert numpy.all(numpy.fabs(numpy.log(pvRvz)-numpy.log(qdf.pvRvz(0.1,0.05,R,z))) < 10.**-10.), 'pvRvz calculated with R and z array input does not equal to calculated with scalar input'
     return None
 
