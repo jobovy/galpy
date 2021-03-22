a39b9287d37954964149066df99192cc053e0bda
Jo Bovy <bovy@astro.utoronto.ca>
2021-03-21 22:04:58 -0400

    Add rmin to AdiabaticContractionWrapperPotential

--- galpy/potential/AdiabaticContractionWrapperPotential.py
+++ galpy/potential/AdiabaticContractionWrapperPotential.py
@@ -14,7 +14,7 @@ class AdiabaticContractionWrapperPotential(interpSphericalPotential):
     """AdiabaticContractionWrapperPotential: Wrapper to adiabatically contract a DM halo in response to the growth of a baryonic component.
     """
     def __init__(self,amp=1.,pot=None,baryonpot=None,
-                 method='cautun',f_bar=None,rmax=50.,
+                 method='cautun',f_bar=None,rmin=None,rmax=50.,
                  ro=None,vo=None):
         """
         NAME:
@@ -37,6 +37,8 @@ class AdiabaticContractionWrapperPotential(interpSphericalPotential):
 
            f_bar= (None) baryon fraction; if None, computed from pot and baryonpot
 
+           rmin= (None) minimum radius to consider (default: rmax/2500)
+
            rmax= (50.) maximum radius to consider (can be Quantity)
 
            ro, vo= standard unit-conversion parameters
@@ -53,10 +55,12 @@ class AdiabaticContractionWrapperPotential(interpSphericalPotential):
         # Initialize with Force just to parse (ro,vo)
         Force.__init__(self,ro=ro,vo=vo)
         rmax= conversion.parse_length(rmax,ro=self._ro)
+        rmin= conversion.parse_length(rmin,ro=self._ro) if not rmin is None \
+            else rmax/2500.
         # Compute baryon and DM enclosed masses on radial grid
         from ..potential import mass
         rgrid= numpy.concatenate((numpy.array([0.]),
-                                  numpy.geomspace(rmax/300.,rmax,301)))
+                                  numpy.geomspace(rmin,rmax,301)))
         baryon_mass= numpy.array([mass(baryonpot,r,use_physical=False)
                                   for r in rgrid])
         dm_mass= numpy.array([mass(pot,r,use_physical=False)
