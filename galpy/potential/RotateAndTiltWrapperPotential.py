29e050d700fa195fddcf5128809af93a495af035
Ted Mackereth <tedmackereth@gmail.com>
2021-03-31 14:58:18 -0400

    include explicit R,phi,z force calculation

--- galpy/potential/RotateAndTiltWrapperPotential.py
+++ galpy/potential/RotateAndTiltWrapperPotential.py
@@ -1,5 +1,6 @@
 ###############################################################################
-#   RotateAndTiltWrapperPotential.py: Wrapper to tilt the z-axis of a potential
+#   RotateAndTiltWrapperPotential.py: Wrapper to rotate and tilt the z-axis
+#   of a potential
 ###############################################################################
 import numpy
 from .WrapperPotential import parentWrapperPotential
@@ -7,7 +8,9 @@ from ..util import conversion
 from ..util import _rotate_to_arbitrary_vector
 from ..util import coords
 class RotateAndTiltWrapperPotential(parentWrapperPotential):
-    """ Potential wrapper class that implements an adjustment to the z-axis vector of a given Potential. This can be used, for example, to tilt a disc to a desired inclination angle
+    """ Potential wrapper class that implements an adjustment to the rotation
+        and z-axis vector (tilt) of a given Potential. This can be used,
+        for example, to tilt a disc to a desired inclination angle
     """
     def __init__(self,amp=1.,zvec=None,pa=None,pot=None,
                  ro=None,vo=None):
@@ -33,7 +36,8 @@ class RotateAndTiltWrapperPotential(parentWrapperPotential):
            2020-03-29 - Started - Mackereth (UofT)
 
         """
-        self._setup_zvec_pa(zvec)
+        self._setup_zvec_pa(zvec, pa)
+        self._inv_rot = numpy.linalg.inv(self._rot)
         self.hasC= True
         self.hasC_dxdv= True
         self.isNonAxi = True
@@ -70,3 +74,45 @@ class RotateAndTiltWrapperPotential(parentWrapperPotential):
         args = (R, z)
         kwargs['phi'] = phi
         return self._wrap_pot_func(attribute)(self._pot,*args,**kwargs)
+
+    def _force_xyz(self,*args,**kwargs):
+        """ get the rectangular forces in the transformed frame """
+        #first figure out the R,phi,z force in the aligned frame...
+        o_R,o_phi,o_z = args[0],kwargs.get('phi',0.),0 if len(args) == 1 else args[1]
+        if o_phi is None:
+            o_phi = 0.
+        x,y,z= coords.cyl_to_rect(o_R, o_phi, o_z)
+        #apply rotation matrix
+        xyzp= numpy.dot(self._rot,numpy.array([x,y,z]))
+        #back to R,phi,z
+        t_R, t_phi, t_z = coords.rect_to_cyl(xyzp[0], xyzp[1], xyzp[2])
+        args = (t_R, t_z)
+        kwargs['phi'] = t_phi
+        #get the forces
+        Rforce_a = self._wrap_pot_func('_Rforce')(self._pot,*args,**kwargs)
+        phiforce_a = self._wrap_pot_func('_phiforce')(self._pot,*args,**kwargs)
+        zforce_a = self._wrap_pot_func('_zforce')(self._pot,*args,**kwargs)
+        #get the forces in x,y
+        xforce_a = numpy.cos(t_phi)*Rforce_a - numpy.sin(t_phi)*phiforce_a
+        yforce_a = numpy.sin(t_phi)*Rforce_a + numpy.cos(t_phi)*phiforce_a
+        #rotate back
+        Fxyz = numpy.dot(self._inv_rot, numpy.array([xforce_a,yforce_a,zforce_a]))
+        return Fxyz
+
+    def _Rforce(self,*args,**kwargs):
+        Fxyz = self._force_xyz(*args,**kwargs)
+        o_R,o_phi,o_z = args[0],kwargs.get('phi',0.),0 if len(args) == 1 else args[1]
+        if o_phi is None:
+            o_phi = 0.
+        return numpy.cos(o_phi)*Fxyz[0] + numpy.sin(o_phi)*Fxyz[1]
+
+    def _phiforce(self,*args,**kwargs):
+        Fxyz = self._force_xyz(*args,**kwargs)
+        o_R,o_phi,o_z = args[0],kwargs.get('phi',0.),0 if len(args) == 1 else args[1]
+        if o_phi is None:
+            o_phi = 0.
+        return -numpy.sin(o_phi)*Fxyz[0] + numpy.cos(o_phi)*Fxyz[1]
+
+    def _zforce(self,*args,**kwargs):
+        Fxyz = self._force_xyz(*args,**kwargs)
+        return Fxyz[2]
