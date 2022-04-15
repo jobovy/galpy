69cd32b5f6b4948fbbf85f4870fe0fc9e75a351c
Jo Bovy <bovy@astro.utoronto.ca>
2022-04-14 23:03:01 -0400

    Release v1.7.2

--- setup.py
+++ setup.py
@@ -252,7 +252,7 @@ class BuildExt(build_ext):
     
 setup(cmdclass=dict(build_ext=BuildExt), # this to allow compiler check above
       name='galpy',
-      version='1.8.0.dev0',
+      version='1.7.2',
       description='Galactic Dynamics in python',
       author='Jo Bovy',
       author_email='bovy@astro.utoronto.ca',
