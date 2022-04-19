f0e5cda1ca75d9f045b38d819062a69ad08cb4f9
Jo Bovy <bovy@astro.utoronto.ca>
2022-04-19 11:05:42 -0400

    Add quick test of streamspraydf against streamdf for the Bovy (2014) example stream

---
+++ tests/test_streamspraydf.py
@@ -0,0 +1,89 @@
+import numpy
+import pytest
+from galpy.df import streamdf, streamspraydf
+from galpy.orbit import Orbit
+from galpy.potential import LogarithmicHaloPotential
+from galpy.actionAngle import actionAngleIsochroneApprox
+from galpy.util import conversion #for unit conversions
+
+################################ Tests against streamdf ######################
+
+# Setup both DFs    
+@pytest.fixture(scope='module')
+def setup_testStreamsprayAgainstStreamdf():
+    lp= LogarithmicHaloPotential(normalize=1.,q=0.9)
+    aAI= actionAngleIsochroneApprox(pot=lp,b=0.8)
+    obs= Orbit([1.56148083,0.35081535,-1.15481504,
+                0.88719443,-0.47713334,0.12019596])
+    ro, vo= 8., 220.
+    # Set up streamdf
+    sigv= 0.365 #km/s
+    sdf_bovy14= streamdf(sigv/220.,progenitor=obs,pot=lp,aA=aAI,
+                         leading=True,
+                         nTrackChunks=11,
+                         tdisrupt=4.5/conversion.time_in_Gyr(vo,ro))
+    # Set up streamspraydf
+    spdf_bovy14= streamspraydf(2*10.**4./conversion.mass_in_msol(vo,ro),
+                               progenitor=obs,
+                               pot=lp,
+                               tdisrupt=4.5/conversion.time_in_Gyr(vo,ro))
+    return sdf_bovy14, spdf_bovy14
+    
+def test_sample_bovy14(setup_testStreamsprayAgainstStreamdf):
+    # Load objects that were setup above
+    sdf_bovy14, spdf_bovy14= setup_testStreamsprayAgainstStreamdf
+    numpy.random.seed(1)
+    RvR_sdf = sdf_bovy14.sample(n=1000)
+    RvR_spdf= spdf_bovy14.sample(n=1000,integrate=True)
+    #Sanity checks
+    # Range in Z
+    indx= (RvR_sdf[3] > 4./8.)*(RvR_sdf[3] < 5./8.)
+    #mean
+    assert numpy.fabs(numpy.mean(RvR_sdf[0][indx])-numpy.mean(RvR_spdf[0][indx])) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    assert numpy.fabs(numpy.mean(RvR_sdf[1][indx])-numpy.mean(RvR_spdf[1][indx])) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    assert numpy.fabs(numpy.mean(RvR_sdf[2][indx])-numpy.mean(RvR_spdf[2][indx])) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    assert numpy.fabs(numpy.mean(RvR_sdf[4][indx])-numpy.mean(RvR_spdf[4][indx])) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    assert numpy.fabs(numpy.mean(RvR_sdf[5][indx])-numpy.mean(RvR_spdf[5][indx])) < 4e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    # Another range in Z
+    indx= (RvR_sdf[3] > 5./8.)*(RvR_sdf[3] < 6./8.)
+    #mean
+    assert numpy.fabs(numpy.mean(RvR_sdf[0][indx])-numpy.mean(RvR_spdf[0][indx])) < 1e-1, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    assert numpy.fabs(numpy.mean(RvR_sdf[1][indx])-numpy.mean(RvR_spdf[1][indx])) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    assert numpy.fabs(numpy.mean(RvR_sdf[2][indx])-numpy.mean(RvR_spdf[2][indx])) < 4e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    assert numpy.fabs(numpy.mean(RvR_sdf[4][indx])-numpy.mean(RvR_spdf[4][indx])) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    assert numpy.fabs(numpy.mean(RvR_sdf[5][indx])-numpy.mean(RvR_spdf[5][indx])) < 1e-1, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean)'
+    return None
+
+@pytest.mark.xfail
+def test_bovy14_sampleXY(setup_testStreamsprayAgainstStreamdf):
+    # Load objects that were setup above
+    sdf_bovy14, spdf_bovy14= setup_testStreamsprayAgainstStreamdf
+    numpy.random.seed(1)
+    XvX_sdf = sdf_bovy14.sample(n=1000,xy=True)
+    XvX_spdf= spdf_bovy14.sample(n=1000,xy=True)
+    #Sanity checks
+    # Range in Z
+    indx= (XvX_sdf[2] > 4./8.)*(XvX_sdf[2] < 5./8.)
+    #mean
+    assert numpy.fabs(numpy.mean(XvX_sdf[0][indx])-numpy.mean(XvX_spdf[0][indx])) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean, xy)'
+    assert numpy.fabs(numpy.mean(XvX_sdf[1][indx])-numpy.mean(XvX_spdf[1][indx])) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean, xy)'
+    assert numpy.fabs(numpy.mean(XvX_sdf[4][indx])-numpy.mean(XvX_spdf[4][indx])) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean, xy)'
+    return None
+
+@pytest.mark.xfail
+def test_bovy14_sampleLB(setup_testStreamsprayAgainstStreamdf):
+    # Load objects that were setup above
+    sdf_bovy14, spdf_bovy14= setup_testStreamsprayAgainstStreamdf
+    numpy.random.seed(1)
+    LB_sdf = sdf_bovy14.sample(n=1000,lb=True)
+    LB_spdf = spdf_bovy14.sample(n=1000,lb=True)
+    #Sanity checks
+    # Range in l
+    indx= (LB_sdf[0] > 212.5)*(LB_sdf[0] < 217.5)
+    #mean
+    assert numpy.fabs(numpy.mean(LB_spdf[0][indx])/numpy.mean(LB_sdf[0][indx])-1.) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean, lb)'
+    assert numpy.fabs(numpy.mean(LB_spdf[1][indx])/numpy.mean(LB_sdf[1][indx])-1.) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean, lb)'
+    assert numpy.fabs(numpy.mean(LB_spdf[2][indx])/numpy.mean(LB_sdf[2][indx])-1.) < 3e-2, 'streamdf and streamspraydf do not generate similar samples for the Bovy (2014) stream (mean, lb)'
+    return None
+
+
