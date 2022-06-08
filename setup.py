bfb625d9682b34728a9b7484b740ac04ea7d8530
Jo Bovy <bovy@astro.utoronto.ca>
2022-06-08 15:43:04 -0400

    Force GSL version to 2.7 for pyodide build
    Signed-off-by: Jo Bovy <bovy@astro.utoronto.ca>

--- setup.py
+++ setup.py
@@ -108,7 +108,10 @@ try:
             subprocess.check_output(cmd,
                                     shell=sys.platform.startswith('win'))
 except (OSError,subprocess.CalledProcessError):
-    gsl_version= ['0','0']
+    if "PYODIDE" in os.environ:
+        gsl_version= ['2','7']
+    else:
+        gsl_version= ['0','0']
 else:
     if PY3:
         gsl_version= gsl_version.decode('utf-8')
