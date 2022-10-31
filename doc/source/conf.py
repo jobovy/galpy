10cbeecd6ecb9f40b99e7d46868faf3a6e7bf41e
Jo Bovy <bovy@astro.utoronto.ca>
2022-10-31 11:48:39 -0400

    Bump release to 1.8.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -90,7 +90,7 @@ copyright = f'2010 - {datetime.datetime.now().year}, Jo Bovy'
 # built documents.
 #
 # The short X.Y version.
-version = '1.8.1.dev0'
+version = '1.8.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
