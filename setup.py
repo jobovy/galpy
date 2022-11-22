c6a0e4dc01258546e9f968932fc058bb2c2acb5c
Jo Bovy <bovy@astro.utoronto.ca>
2022-11-22 15:28:11 -0500

    Only add new compiler flags on not WIN

--- setup.py
+++ setup.py
@@ -195,7 +195,8 @@ if single_ext: #add the code and libraries for the actionAngleTorus extensions
         galpy_c_include_dirs.extend(actionAngleTorus_include_dirs)
         galpy_c_include_dirs= list(set(galpy_c_include_dirs))
 
-extra_compile_args.insert(0,"-pthread")
+if not WIN32:
+    extra_compile_args.insert(0,"-pthread")
 
 #Installation of this extension using the GSL may (silently) fail, if the GSL
 #is built for the wrong architecture, on Mac you can install the GSL correctly
@@ -217,7 +218,8 @@ else:
 
 # Add the actionAngleTorus extension (src and include specified above)
 aAT_extra_compile_args= copy.deepcopy(extra_compile_args)
-aAT_extra_compile_args.append("-std=gnu++14")
+if not WIN32:
+    aAT_extra_compile_args.append("-std=gnu++14")
 actionAngleTorus_c= Extension('libgalpy_actionAngleTorus',
                               sources=actionAngleTorus_c_src,
                               libraries=galpy_c_libraries,
