97bbae3f00d487282a726d66e8a39c806bf788f1
Jo Bovy <bovy@astro.utoronto.ca>
2023-05-15 10:26:06 -0400

    Actually replace the SIMBAD URL with the https version

--- galpy/orbit/Orbits.py
+++ galpy/orbit/Orbits.py
@@ -7598,7 +7598,7 @@ def _from_name_oneobject(name, obs):
     # setup a SIMBAD query with the appropriate fields
     simbad = Simbad()
     # Make sure to make an HTTPS request so this code works in the browser
-    simbad.SIMBAD_URL.replace("http://", "https://")
+    simbad.SIMBAD_URL = simbad.SIMBAD_URL.replace("http://", "https://")
     simbad.add_votable_fields(
         "ra(d)", "dec(d)", "pmra", "pmdec", "rv_value", "plx", "distance"
     )
