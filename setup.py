0df02b3f6d171d7b01cea0131b38d6bb59e18b57
Jo Bovy <bovy@astro.utoronto.ca>
2023-07-02 23:50:11 -0400

    Bump version to next development version

--- setup.py
+++ setup.py
@@ -286,7 +286,7 @@ class BuildExt(build_ext):
 setup(
     cmdclass=dict(build_ext=BuildExt),  # this to allow compiler check above
     name="galpy",
-    version="1.9.0",
+    version="1.9.1.dev0",
     description="Galactic Dynamics in python",
     author="Jo Bovy",
     author_email="bovy@astro.utoronto.ca",
