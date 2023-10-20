055e3c8d7db2f6ab5efe1f2ff2362b96c1af8c8f
Jo Bovy <bovy@astro.utoronto.ca>
2023-10-19 22:35:43 -0400

    Small fixes to a bunch of errors in the numpy-style docstring conversion

--- galpy/potential/EllipticalDiskPotential.py
+++ galpy/potential/EllipticalDiskPotential.py
@@ -255,7 +255,7 @@ class EllipticalDiskPotential(planarPotential):
             * numpy.sin(2.0 * (phi - self._phib))
         )
 
-    def tform(self):
+    def tform(self):  # pragma: no cover
         """
         Return formation time of the perturbation.
 
