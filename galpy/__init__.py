043067ae939a0ab279f4712143822f62422694c1
Jo Bovy <bovy@astro.utoronto.ca>
2024-03-04 21:55:40 -0500

    Bump version to v1.9.2

--- galpy/__init__.py
+++ galpy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.9.2.dev0"
+__version__ = "1.9.2"
 # Check whether a new version is available
 import datetime
 import http.client
--- setup.py
+++ setup.py
@@ -286,7 +286,7 @@ class BuildExt(build_ext):
 setup(
     cmdclass=dict(build_ext=BuildExt),  # this to allow compiler check above
     name="galpy",
-    version="1.9.2.dev0",
+    version="1.9.2",
     description="Galactic Dynamics in python",
     author="Jo Bovy",
     author_email="bovy@astro.utoronto.ca",
