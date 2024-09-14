e587745333fe27c09051291c54a298859be20602
Jo Bovy <bovy@astro.utoronto.ca>
2024-09-14 10:23:38 -0400

    Add Python 3.13 wheels (#674)

--- setup.py
+++ setup.py
@@ -309,6 +309,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Environment :: WebAssembly :: Emscripten",
         "Topic :: Scientific/Engineering :: Astronomy",
         "Topic :: Scientific/Engineering :: Physics",
