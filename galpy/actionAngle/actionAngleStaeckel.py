a0b0b8255717dc5c2b914d6955b75acb90a566eb
Jo Bovy <bovy@astro.utoronto.ca>
2025-02-12 23:48:48 -0500

    Assign NaN to results of action-angle approximations for unbound orbits in the orbit interface; fixes #383

--- galpy/actionAngle/actionAngleStaeckel.py
+++ galpy/actionAngle/actionAngleStaeckel.py
@@ -214,7 +214,13 @@ class actionAngleStaeckel(actionAngle):
             else:
                 # Set up the actionAngleStaeckelSingle object
                 aASingle = actionAngleStaeckelSingle(
-                    R[0], vR[0], vT[0], z[0], vz[0], pot=self._pot, delta=delta
+                    R[0],
+                    vR[0],
+                    vT[0],
+                    z[0],
+                    vz[0],
+                    pot=self._pot,
+                    delta=delta[0] if hasattr(delta, "__len__") else delta,
                 )
                 return (
                     numpy.atleast_1d(aASingle.JR(**copy.copy(kwargs))),
