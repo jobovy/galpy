cc6715105a35b3f1c23ac4930b35143d2d9232d5
James Grimmett <grimmett.james@gmail.com>
2025-03-09 17:10:57 +1100

    implement caching for powersphericalpotentialwcutoff

--- galpy/potential/potential_c_ext/PowerSphericalPotentialwCutoff.c
+++ galpy/potential/potential_c_ext/PowerSphericalPotentialwCutoff.c
@@ -5,7 +5,7 @@
 #define M_PI 3.14159265358979323846
 #endif
 //PowerSphericalPotentialwCutoff
-//3  arguments: amp, alpha, rc
+//5  arguments: amp, alpha, rc, cached_r2, cached_force
 double mass(double r2,double alpha, double rc){
   return 2. * M_PI * pow ( rc , 3. - alpha ) * ( gsl_sf_gamma ( 1.5 - 0.5 * alpha ) * gsl_sf_gamma_inc_P ( 1.5 - 0.5 * alpha , r2 / rc / rc ) );
 }
@@ -27,13 +27,22 @@ double PowerSphericalPotentialwCutoffRforce(double R,double Z, double phi,
 					    struct potentialArg * potentialArgs){
   double * args= potentialArgs->args;
   //Get args
-  double amp= *args++;
-  double alpha= *args++;
-  double rc= *args;
+  double amp= *args;
+  double alpha= *(args + 1);
+  double rc= *(args + 2);
   //Radius
   double r2= R*R+Z*Z;
+  double force;
+  if ( r2 == *(args + 3) ) {
+      force= *(args + 4);
+  } else {
+      force= -amp * mass(r2, alpha, rc) / pow(r2,1.5);
+      //Setup caching
+      *(args + 3)= r2;
+      *(args + 4)= force;
+  }
   //Calculate Rforce
-  return - amp * mass (r2,alpha,rc) * R / pow(r2,1.5);
+  return force * R;
 }
 double PowerSphericalPotentialwCutoffPlanarRforce(double R,double phi,
 						  double t,
@@ -53,13 +62,22 @@ double PowerSphericalPotentialwCutoffzforce(double R,double Z,double phi,
 					    struct potentialArg * potentialArgs){
   double * args= potentialArgs->args;
   //Get args
-  double amp= *args++;
-  double alpha= *args++;
-  double rc= *args;
+  double amp= *args;
+  double alpha= *(args + 1);
+  double rc= *(args + 2);
   //Radius
   double r2= R*R+Z*Z;
-  //Calculate Rforce
-  return - amp * mass (r2,alpha,rc) * Z / pow(r2,1.5);
+  double force;
+  if ( r2 == *(args + 3) ) {
+      force= *(args + 4);
+  } else {
+      force= -amp * mass(r2, alpha, rc) / pow(r2,1.5);
+      //Setup caching
+      *(args + 3)= r2;
+      *(args + 4)= force;
+  }
+  //Calculate zforce
+  return force * Z;
 }
 double PowerSphericalPotentialwCutoffPlanarR2deriv(double R,double phi,
 						   double t,
