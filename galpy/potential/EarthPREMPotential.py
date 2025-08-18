f932ef4e148015d68c910261f9e3fd1bee53deaa
Yuzhe Zhang <yuhzhang@uni-mainz.de>
2025-08-18 21:27:26 +0200

    modified formatting

--- galpy/potential/EarthPREMPotential.py
+++ galpy/potential/EarthPREMPotential.py
@@ -1,8 +1,6 @@
 ###############################################################################
-#   EarthPREMPotentialpy: The potential of Earth
+#   EarthPREMPotentialpy: The potential of Earth based on PREM model
 ###############################################################################
-import numpy
-
 from ..util import conversion
 from ..util._optional_deps import _SYMPY_LOADED
 
@@ -11,28 +9,38 @@ from .SymbolicSphericalPotential import SymbolicSphericalPotential
 
 if _SYMPY_LOADED:
     import sympy
-    from sympy import symbols, integrate, Piecewise, pi, latex, sqrt
 
 
 r = sympy.Symbol("r", real=True, positive=True)
 EARTH_RADIUS_KM = 6371.0
 pieces = [
-        (13.0885 - 8.8381 * (r / EARTH_RADIUS_KM) ** 2, 0, 1221.5),
-        (12.5815 - 1.2638 * (r / EARTH_RADIUS_KM)
-         - 3.6426 * (r / EARTH_RADIUS_KM) ** 2
-         - 5.5281 * (r / EARTH_RADIUS_KM) ** 3, 1221.5, 3480.0),
-        (7.9565 - 6.4761 * (r / EARTH_RADIUS_KM)
-         + 5.5283 * (r / EARTH_RADIUS_KM) ** 2
-         - 3.0807 * (r / EARTH_RADIUS_KM) ** 3, 3480.0, 5701.0),
-        (5.3197 - 1.4836 * (r / EARTH_RADIUS_KM), 5701.0, 5771.0),
-        (11.2494 - 8.0298 * (r / EARTH_RADIUS_KM), 5771.0, 5971.0),
-        (7.1089 - 3.8045 * (r / EARTH_RADIUS_KM), 5971.0, 6151.0),
-        (2.6910 + 0.6924 * (r / EARTH_RADIUS_KM), 6151.0, 6346.6),
-        (2.6910 + 0.6924 * (r / EARTH_RADIUS_KM), 6346.6, 6356.0),
-        (2.6, 6356.0, EARTH_RADIUS_KM),
-        # Explicitly include density for r >= EARTH_RADIUS_KM:
-        (0.0, EARTH_RADIUS_KM, sympy.oo),
-    ]
+    (13.0885 - 8.8381 * (r / EARTH_RADIUS_KM) ** 2, 0, 1221.5),
+    (
+        12.5815
+        - 1.2638 * (r / EARTH_RADIUS_KM)
+        - 3.6426 * (r / EARTH_RADIUS_KM) ** 2
+        - 5.5281 * (r / EARTH_RADIUS_KM) ** 3,
+        1221.5,
+        3480.0,
+    ),
+    (
+        7.9565
+        - 6.4761 * (r / EARTH_RADIUS_KM)
+        + 5.5283 * (r / EARTH_RADIUS_KM) ** 2
+        - 3.0807 * (r / EARTH_RADIUS_KM) ** 3,
+        3480.0,
+        5701.0,
+    ),
+    (5.3197 - 1.4836 * (r / EARTH_RADIUS_KM), 5701.0, 5771.0),
+    (11.2494 - 8.0298 * (r / EARTH_RADIUS_KM), 5771.0, 5971.0),
+    (7.1089 - 3.8045 * (r / EARTH_RADIUS_KM), 5971.0, 6151.0),
+    (2.6910 + 0.6924 * (r / EARTH_RADIUS_KM), 6151.0, 6346.6),
+    (2.6910 + 0.6924 * (r / EARTH_RADIUS_KM), 6346.6, 6356.0),
+    (2.6, 6356.0, EARTH_RADIUS_KM),
+    # Explicitly include density for r >= EARTH_RADIUS_KM:
+    (0.0, EARTH_RADIUS_KM, sympy.oo),
+]
+
 
 def earth_PREM_density_sym():
     r = sympy.Symbol("r", real=True, positive=True)
@@ -45,8 +53,8 @@ def earth_PREM_density_sym():
     # Add fallback condition for r >= EARTH_RADIUS_KM (density = 0)
     pw_args.append((0, True))
 
-    dens_sym = sympy.Piecewise(*pw_args)
-    return dens_sym, r
+    dens = sympy.Piecewise(*pw_args)
+    return dens, r
 
 
 class EarthPREMPotential(SymbolicSphericalPotential):
@@ -79,7 +87,7 @@ class EarthPREMPotential(SymbolicSphericalPotential):
         amp : float or Quantity, optional
             Amplitude to be applied to the potential. Can be a Quantity with units of mass density or Gxmass density.
         R : float or Quantity, optional
-            
+
         normalize : bool or float, optional
             If True, normalize such that vc(1.,0.)=1., or, if given as a number, such that the force is this fraction of the force necessary to make vc(1.,0.)=1.
         ro : float or Quantity, optional
@@ -93,16 +101,8 @@ class EarthPREMPotential(SymbolicSphericalPotential):
         """
         SymbolicSphericalPotential.__init__(
             self,
-            dens_sym=earth_PREM_density_sym,
+            dens=earth_PREM_density_sym,
         )
-        # R = conversion.parse_length(R, ro=self._ro)
-        # self.R = R
-        # self._R2 = self.R**2.0
-        # self._R3 = self.R**3.0
-        # if normalize or (
-        #     isinstance(normalize, (int, float)) and not isinstance(normalize, bool)
-        # ):  # pragma: no cover
-        #     self.normalize(normalize)
         self.hasC = False
         self.hasC_dxdv = False
         self.hasC_dens = False
