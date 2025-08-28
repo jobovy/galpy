fc853591ecbda79851f3660322edbc26b6e3deb0
jennywagner <37711401+jennywagner@users.noreply.github.com>
2025-08-28 23:36:33 +0200

    added new test function for tol-dependencies

---
+++ tests/test_tol_integration.py
@@ -0,0 +1,84 @@
+# Test the functions in galpy/util/__init__.py
+# import sys
+import numpy
+import pytest
+
+from galpy import orbit, potential
+
+
+# Test run for two different rtol/atol values for 2d orbit integration on a Kepler pot. for all algorithms available
+# test is passed if the difference in the orbital reconstruction is not exactly zero for all sampling points and
+# if the orbital energy loss is smaller for the more precise rtol/atol orbit reconstruction
+def test_2d_tol_integration():
+    ttol_vec = [1e-12, 1e-6]
+    times = numpy.linspace(0.0, 10.0, 250)
+    integrators = [
+        "dopr54_c",
+        "odeint",
+        "dop853",
+        "dop853_c",
+        "leapfrog",
+        "leapfrog_c",
+        "rk4_c",
+        "rk6_c",
+        "symplec4_c",
+        "symplec6_c",
+        "ias15_c",
+    ]
+    pot = potential.KeplerPotential(
+        amp=1.0, normalize=True
+    )  # only use a simple normalised KeplerPotential
+    # initialise list of resulting orbits for each integrator for each rtol/atol (11 integrators, 2 tols)
+    o_lists = [[] * len(integrators) for j in range(len(ttol_vec))]
+    Delta_r = numpy.zeros([len(integrators)])
+    Delta_E = numpy.zeros([len(integrators)])
+    for cnt_tol in numpy.arange(len(ttol_vec)):
+        for cnt_int in numpy.arange(
+            len(integrators)
+        ):  # loop over all possible integration algorithms
+            # initialise a test orbit with few rounds, integrate trajectory, append to list of orbits
+            o = orbit.Orbit([1.0, 0.1, 0.8])
+            o.integrate(
+                times,
+                pot,
+                method=integrators[cnt_int],
+                rtol=ttol_vec[cnt_tol],
+                atol=ttol_vec[cnt_tol],
+            )
+            o_lists[cnt_tol].append(o)
+
+    # make test for differing reconstruction precision and energy loss along the orbits
+    for cnt_int in numpy.arange(len(integrators)):
+        Delta_r[cnt_int] = numpy.sum(
+            numpy.abs(o_lists[0][cnt_int].r(times) - o_lists[1][cnt_int].r(times))
+        )
+        Delta_E[cnt_int] = numpy.sum(
+            numpy.abs(o_lists[0][cnt_int].E(times) - o_lists[1][cnt_int].E(times))
+        )
+        # if special integrators yield same reconstructions
+        if (
+            integrators[cnt_int] == "dop853"
+            or integrators[cnt_int] == "rk6_c"
+            or integrators[cnt_int] == "ias15_c"
+        ):
+            assert Delta_r[cnt_int] == 0.0, (
+                f"{integrators[cnt_int]} orbit integration is unexpectedly sensitive to rtol/atol - position difference"
+            )
+            assert Delta_E[cnt_int] == 0.0, (
+                f"{integrators[cnt_int]} orbit integration is unexpectedly sensitive to rtol/atol - energy difference"
+            )
+        else:  # for all other integration routines check that differences are moderate as expected
+            assert Delta_r[cnt_int] > 0.0, (
+                f"{integrators[cnt_int]} orbit integration unexpectedly not sensitive to rtol/atol - position difference"
+            )
+            assert Delta_r[cnt_int] < 0.1, (
+                f"{integrators[cnt_int]} orbit integration has worse than expected reconstruction precision - position difference"
+            )
+            assert Delta_E[cnt_int] > 0.0, (
+                f"{integrators[cnt_int]} orbit integration unexpectedly not sensitive to rtol/atol - energy difference"
+            )
+            assert Delta_E[cnt_int] < 0.1, (
+                f"{integrators[cnt_int]} orbit integration has worse than expected reconstruction precision - energy difference"
+            )
+
+    return None
