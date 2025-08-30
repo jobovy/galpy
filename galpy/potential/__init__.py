50843f5400c9289e5cbffa0a3df6ac5d9d2a3fc7
Yuzhe Zhang <yuhzhang@uni-mainz.de>
2025-08-30 17:46:13 +0200

    Update galpy/potential/__init__.py
    added SymbolicSphericalPotential
    
    Co-authored-by: Jo Bovy <jo.bovy@gmail.com>

--- galpy/potential/__init__.py
+++ galpy/potential/__init__.py
@@ -11,6 +11,7 @@ from . import (
     DiskSCFPotential,
     DoubleExponentialDiskPotential,
     EarthPREMPotential,
+    SymbolicSphericalPotential,
     EllipticalDiskPotential,
     FDMDynamicalFrictionForce,
     FerrersPotential,
