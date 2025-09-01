f768e4c9cd3cb75a017fda517b9a7f3587f458e5
Jo Bovy <bovy@astro.utoronto.ca>
2025-08-31 21:07:17 -0400

    Simplify rtol/atol sensitivity test

--- tests/test_orbit.py
+++ tests/test_orbit.py
@@ -9940,58 +9940,44 @@ def test_2d_tol_integration():
     ]
     # only use the simplest normalised KeplerPotential
     pot = potential.KeplerPotential(amp=1.0, normalize=True)
-    # initialise list of resulting orbits for each integrator for each rtol/atol (10 integrators, 2 tols)
-    o_lists = [[] * len(ttol_vec) for j in range(len(integrators))]
-    Delta_r = numpy.zeros(
-        [len(integrators)]
-    )  # reconstruction differences in position summed over all time steps
-    Delta_E = numpy.zeros(
-        [len(integrators)]
-    )  # energy differences summed over all time steps
-
-    for cnt_int in numpy.arange(
-        len(integrators)
-    ):  # loop over all possible integration algorithms
+    for integrator in integrators:
+        o_list = []
         for cnt_tol in numpy.arange(len(ttol_vec)):
             # initialise a test orbit with few rounds, integrate trajectory, append to list of orbits
             o = orbit.Orbit([1.0, 0.1, 0.8])  # Orbit([R, vR, vT])
             o.integrate(
                 times,
                 pot,
-                method=integrators[cnt_int],
+                method=integrator,
                 rtol=ttol_vec[cnt_tol],
                 atol=ttol_vec[cnt_tol],
             )
-            o_lists[cnt_int].append(o)
+            o_list.append(o)
 
         # make test for differing reconstruction precision and energy loss along the orbits
-        Delta_r[cnt_int] = numpy.sum(
-            numpy.abs(o_lists[cnt_int][0].r(times) - o_lists[cnt_int][1].r(times))
-        )
-        Delta_E[cnt_int] = numpy.sum(
-            numpy.abs(o_lists[cnt_int][0].E(times) - o_lists[cnt_int][1].E(times))
-        )
+        Delta_r = numpy.sum(numpy.abs(o_list[0].r(times) - o_list[1].r(times)))
+        Delta_E = numpy.sum(numpy.abs(o_list[0].E(times) - o_list[1].E(times)))
 
         # if special integrators yield same reconstructions
-        if integrators[cnt_int] == "rk6_c":
-            assert Delta_r[cnt_int] == 0.0, (
-                f"{integrators[cnt_int]} orbit integration is unexpectedly sensitive to rtol/atol - position difference"
+        if integrator == "rk6_c":
+            assert Delta_r == 0.0, (
+                f"{integrator} orbit integration is unexpectedly sensitive to rtol/atol - position difference"
             )
-            assert Delta_E[cnt_int] == 0.0, (
-                f"{integrators[cnt_int]} orbit integration is unexpectedly sensitive to rtol/atol - energy difference"
+            assert Delta_E == 0.0, (
+                f"{integrator} orbit integration is unexpectedly sensitive to rtol/atol - energy difference"
             )
         else:  # for all other integration routines check that differences are moderate as expected
-            assert Delta_r[cnt_int] > 0.0, (
-                f"{integrators[cnt_int]} orbit integration unexpectedly not sensitive to rtol/atol - position difference"
+            assert Delta_r > 0.0, (
+                f"{integrator} orbit integration unexpectedly not sensitive to rtol/atol - position difference"
             )
-            assert Delta_r[cnt_int] < 0.1, (
-                f"{integrators[cnt_int]} orbit integration has worse than expected reconstruction precision - position difference"
+            assert Delta_r < 0.1, (
+                f"{integrator} orbit integration has worse than expected reconstruction precision - position difference"
             )
-            assert Delta_E[cnt_int] > 0.0, (
-                f"{integrators[cnt_int]} orbit integration unexpectedly not sensitive to rtol/atol - energy difference"
+            assert Delta_E > 0.0, (
+                f"{integrator} orbit integration unexpectedly not sensitive to rtol/atol - energy difference"
             )
-            assert Delta_E[cnt_int] < 0.1, (
-                f"{integrators[cnt_int]} orbit integration has worse than expected reconstruction precision - energy difference"
+            assert Delta_E < 0.1, (
+                f"{integrator} orbit integration has worse than expected reconstruction precision - energy difference"
             )
 
     return None
